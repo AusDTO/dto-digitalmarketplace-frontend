db0337927b000ac5c3c001fcc9bf52aa0baab1ea
Nathan Flew <nath@nathf.com>
2016-10-19 21:32:01 +1100

    Wrap in form + base styles

--- src/shared/sharedEntry.js
+++ src/shared/sharedEntry.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom'
 
 import VerticalList, { testData } from './VerticalList'
 import Textarea from './Textarea'
+import MultiInput from './MultiInput'
+
+const rows = [
+  { id: 0, value: 'One' },
+  { id: 1, value: 'Two' },
+  { id: 2, value: 'Three' },
+  { id: 3, value: 'Four' },
+]
 
 ReactDOM.render(
   <div>
-    <Textarea limit={10} />
+    <form>
+      <MultiInput rows={rows} />
+      <Textarea limit={10} name="shared" />
+    </form>
     <VerticalList items={testData.items} />
   </div>,
   document.getElementById('react-bundle-shared')
