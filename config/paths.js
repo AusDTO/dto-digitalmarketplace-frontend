e036d48512b483c2db8767d4ea85a0476a9f8ec0
matt-sm <matt-sm@users.noreply.github.com>
2016-10-25 18:22:43 +1100

    Seller registration - start page (#19)
    * Seller registration - start page
    
    * minor refactor

--- config/paths.js
+++ config/paths.js
@@ -43,7 +43,8 @@ module.exports = {
   entryPoints: {
     'casestudy': [resolveApp('src/bundles/CaseStudy/CaseStudyWidget')],
     'casestudy-view': [resolveApp('src/bundles/CaseStudy/CaseStudyViewWidget')],
-    'shared': [resolveApp('src/shared/sharedEntry')]
+    'shared': [resolveApp('src/shared/sharedEntry')],
+    'seller-registration-start': [resolveApp('src/bundles/SellerRegistration/StartWidget')]
   },
   entryPointMocks: {
     'casestudy-view': require(resolveApp('src/bundles/CaseStudy/components/View/View.json')),
