6118de523d963a61d0ed026f21d404fa16caca87
Nathan Flew <nath@nathf.com>
2016-11-04 07:58:33 +1100

    Tests for formPropsSelector

---
+++ src/shared/reduxModules/__tests__/formPropsSelector.test.js
@@ -0,0 +1,114 @@
+import formProps, { 
+  getCurrentForm,
+  getFormValues,
+  getFormOptions,
+  getOptions,
+  getModel
+} from '../formPropsSelector';
+
+
+let exampleState = {
+  form_options: {
+    mode: 'add'
+  },
+  options: {
+    serverRender: false
+  },
+  forms: {
+    someForm: {
+      $form: {
+        valid: false
+      }
+    }
+  },
+  someForm: {
+    foo: 'bar'
+  }
+}
+
+test('formProps will reduce state to single object', () => {
+  let expectedState = {
+    mode: 'add',
+    serverRender: false,
+    formErrors: void 0,
+    model: 'someForm',
+    form: {
+      valid: false
+    },
+    someForm: {
+      foo: 'bar'
+    }
+  };
+
+  expect(formProps(exampleState, 'someForm')).toEqual(expectedState);
+});
+
+test('formProps will still provide a simplified shape if it can\'t find the form in state', () => {
+  let expectedState = {
+    mode: 'add',
+    serverRender: false,
+    formErrors: void 0,
+    model: 'invalidFormKey',
+    form: {},
+    invalidFormKey: void 0
+  };
+
+  expect(formProps(exampleState, 'invalidFormKey')).toEqual(expectedState)
+});
+
+test('getCurrentForm return values', () => {
+  let exampleState = {
+    forms: {
+      formKey: {
+        $form: {
+          foo: 'bar'
+        }
+      }
+    }
+  };
+
+  let expectedFormState = {
+    foo: 'bar'
+  };
+
+  expect(getCurrentForm(exampleState, 'formKey')).toEqual(expectedFormState)
+  expect(getCurrentForm(void 0, 'formKey')).toEqual({})
+});
+
+test('getFormValues return value', () => {
+  expect(getFormValues(void 0, 'formKey')).toBeUndefined();
+  expect(getFormValues({ 'formKey': { foo: 'bar'} }, 'formKey')).toEqual({ foo: 'bar' });
+});
+
+test('getFormOptions return value', () => {
+  expect(getFormOptions(void 0)).toBeUndefined();
+  expect(getFormOptions({ form_options: { foo: 'bar' }})).toEqual({ foo: 'bar' });
+});
+
+test('getOptions', () => {
+  expect(getOptions(void 0)).toBeUndefined();
+  expect(getOptions({ options: { foo: 'bar' }})).toEqual({ foo: 'bar' });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/shared/reduxModules/formPropsSelector.js
+++ src/shared/reduxModules/formPropsSelector.js
@@ -1,6 +1,13 @@
 import { createSelector } from 'reselect';
 
-export const getCurrentForm = (state = {}, model) => state.forms[model].$form;
+export const getCurrentForm = (state = {}, model) => {
+  const { forms = {} } = state;
+  if (model in forms) {
+    return forms[model].$form;
+  }
+
+  return {};
+}
 export const getFormValues = (state = {}, model) => state[model];
 export const getFormOptions = (state = {}) => state.form_options;
 export const getOptions = (state = {}) => state.options;
