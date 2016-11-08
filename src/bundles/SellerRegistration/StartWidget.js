bfa74c0fef80599a8ebc7bfd32c38f8d7d931d30
Nathan Flew <nath@nathf.com>
2016-11-08 11:44:00 +1100

    Add props

--- src/bundles/SellerRegistration/StartWidget.js
+++ src/bundles/SellerRegistration/StartWidget.js
@@ -3,6 +3,6 @@ import React from 'react'
 import Start from './components/Start';
 import RegisterComponent from '../../RegisterComponent'
 
-export const StartWidget = (props) => <Start />
+export const StartWidget = (props) => <Start {...props} />
 
 export default new RegisterComponent({ 'seller-registration-start': StartWidget })
