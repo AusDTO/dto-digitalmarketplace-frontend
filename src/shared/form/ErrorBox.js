e9db7fc175affa1de39f7738d1cd67cb4ba4e9fa
Nathan Flew <nath@nathf.com>
2016-11-10 09:12:58 +1100

    Fix auto focus on ErrorBox

--- src/shared/form/ErrorBox.js
+++ src/shared/form/ErrorBox.js
@@ -5,15 +5,26 @@ import { getInvalidFields } from '../reduxModules/errorMessageSelector';
 
 class ErrorBox extends React.Component {
 
+  state = {
+    focusedOnce: false
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.refs.box && !this.state.focusedOnce) {
+      this.setState({ focusedOnce: true });
+      this.refs.box.focus();
+    }
+  }
+
   render() {
     const { invalidFields, form } = this.props;
 
     if (form.submitFailed === false || !invalidFields.length) {
-      return <span/>
+      return null;
     }
 
     return (
-      <div ref="errorBox" className="callout--warning" aria-labelledby="validation-masthead-heading" aria-role="group" tabIndex="-1" role="alert">
+      <div ref="box" className="callout--warning" aria-labelledby="validation-masthead-heading" aria-role="group" tabIndex="-1" role="alert">
         <h4 className="validation-masthead-heading">There was a problem with the details you gave</h4>
         {invalidFields && (
           <ul>
