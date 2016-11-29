18ce272abf1a2cf15a77a4007624f7d2cb70764f
Nathan Flew <nath@nathf.com>
2016-11-29 11:00:23 +1100

    Center subflow on casestudy

--- src/bundles/SellerRegistration/components/Signup/Signup.js
+++ src/bundles/SellerRegistration/components/Signup/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Match, Miss, Link } from 'react-router';
 import findIndex from 'lodash/findIndex';
+import classNames from 'classnames';
 
 import NotFound from '../../../../shared/NotFound';
 
@@ -95,10 +96,14 @@ class Signup extends React.Component {
   }
 
   render() {
-    const { validForms = {}, forms, router, steps = {} } = this.props;
+    const { validForms = {}, forms, router, steps = {}, location } = this.props;
     const formSteps = this.steps.map(step => step.formKey).filter(s => s);
     const applicationValid = formSteps.length === Object.keys(validForms).length;
     const { services = {} } = forms.domainSelectorForm;
+
+    const articleClassNames = classNames('col-xs-12 col-sm-8', {
+      'col-sm-push-2': location.pathname.match(/case-study\/(edit|view|add)/)
+    });
 
     return (
       <div className="row">
@@ -123,31 +128,28 @@ class Signup extends React.Component {
           }
           return null;
         }} />
-        <article className="col-xs-12 col-sm-8">
-          {this.steps.map(({pattern, exact, component, label}, i) => {
-            return (
-              <Match key={i} pattern={pattern} render={(routerProps) => {
-                const children = this.nextStep && <input type="hidden" name="next_step_slug" value={this.nextStep.pattern.slice(1)} />
-                const props = Object.assign({},
-                  routerProps,
-                  {
-                    applicationValid,
-                    services,
-                    router,
-                    nextRoute: this.nextStep && this.nextStep.pattern,
-                    title: label,
-                    buttonText: 'Save & Continue',
-                    actions: {
-                      submitApplication
-                    }
-                  },
-                  this.elementProps
-                );
+        <article className={articleClassNames}>
+          {this.steps.map(({pattern, exact, component, label}, i) => (
+            <Match key={i} pattern={pattern} render={(routerProps) => {
+              const children = this.nextStep && <input type="hidden" name="next_step_slug" value={this.nextStep.pattern.slice(1)} />
+              const props = Object.assign({},
+                routerProps, {
+                  applicationValid,
+                  services,
+                  router,
+                  nextRoute: this.nextStep && this.nextStep.pattern,
+                  title: label,
+                  buttonText: 'Save & Continue',
+                  actions: {
+                    submitApplication
+                  }
+                },
+                this.elementProps
+              );
 
-                return React.createElement(component, props, children);
-              }} />
-            )
-          })}
+              return React.createElement(component, props, children);
+            }} />
+          ))}
           <Miss  component={NotFound} />
         </article>
       </div>
