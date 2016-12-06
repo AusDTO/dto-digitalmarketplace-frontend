21378ca89fe6cd15b5a2d0d9246f1aaa302fdfb0
Nathan Flew <nathf@users.noreply.github.com>
2016-12-06 15:07:08 +1100

    Fix/data persist (#107)
    * Covert DomainList to class
    
    * Updated datamodel
    
    * Pass casestudylink component
    
    * Persist data
    
    * Update tests to capture new handling of data
    
    * Clean up + comments

--- src/bundles/SellerRegistration/redux/modules/application.js
+++ src/bundles/SellerRegistration/redux/modules/application.js
@@ -1,10 +1,20 @@
-import { flattenStateForms } from '../helpers';
+import { flattenStateForms, dispatchFormState, getStateForms } from '../helpers';
 
 const STEP_NEXT = 'step/next';
 const STEP_PRE = 'step/pre';
 const APP_SUBMIT = 'application/submit';
 const APP_PRE_SUBMIT = 'application/pre-submit';
 const APP_POST_SUBMIT = 'application/post-submit';
+
+const statusCheck = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+    }
+} 
 
 export default function reducer(state = {}, action = {}) {
     const { type, payload } = action;
@@ -26,6 +36,7 @@ export const nextStep = (to) => ({ type: STEP_NEXT, to });
 export const submitApplication = () => {
     return (dispatch, getState, api) => {
         dispatch(preSubmit());
+
         const state = getState();
         const { form_options = {}, steps } = state;
         let application = flattenStateForms(state);
@@ -47,8 +58,19 @@ export const submitApplication = () => {
                 'Content-Type': 'application/json'
             }
         })
-            .then(() => dispatch(submit(payload)))
-            .then(() => dispatch(postSubmit()));
+        .then(statusCheck)
+        .then((response) => response.json())
+        // Update application in the state to match the server.
+        .then((persistedModel) => {
+            dispatch(submit(persistedModel))
+            return persistedModel;
+        })
+        // Disperse the data back into the redux store's form models, keeping everything in sync. 
+        .then((model) => dispatchFormState(dispatch, getStateForms(state), model.application))
+        .then(() => dispatch(postSubmit()))
+        .catch((e) => {
+            console.error(`Error: ${e.message}`, e);
+        });
     }
 };
 
@@ -84,15 +106,9 @@ export const uploadDocument = (id, file) => {
           headers: {
             'X-CSRFToken': form_options.csrf_token,
           }
-        }).then(response => {
-          if (response.status >= 200 && response.status < 300) {
-            return response
-          } else {
-            var error = new Error(response.statusText)
-            error.response = response
-            throw error
-          }
-        }).then(r => r.text())
+        })
+        .then(statusCheck)
+        .then(r => r.text())
     }
 };
 
