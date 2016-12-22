2b7d90bf5fcd6d6c773468f01c8b92e5c45706a7
Alexander Sadleir <maxious@users.noreply.github.com>
2016-12-22 13:19:01 +1100

    remove startwidget (#128)

--- src/bundles/SellerRegistration/StartWidget.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import Start from './components/Start';
-import RegisterComponent from '../../RegisterComponent'
-
-export const StartWidget = (props) => <Start {...props} />
-
-export default new RegisterComponent({ 'seller-registration-start': StartWidget })
