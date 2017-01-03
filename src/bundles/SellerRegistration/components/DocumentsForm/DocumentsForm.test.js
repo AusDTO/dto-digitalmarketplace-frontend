d84434d4c5d9d02a2a67451c846d7b06385528ad
Nathan Flew <nath@nathf.com>
2017-01-03 16:08:59 +1100

    Test to reflect new structure

--- src/bundles/SellerRegistration/components/DocumentsForm/DocumentsForm.test.js
+++ src/bundles/SellerRegistration/components/DocumentsForm/DocumentsForm.test.js
@@ -9,13 +9,12 @@ import createStore from '../../redux/create-signup'
 
 test('DocumentsForm renders inputs when no documents are present', () => {
   const documents = {}
-  const expiry_dates = {}
 
   const store = createStore({
     form_options: {
       csrf_token: 'abc',
     },
-    documentsForm: {documents, expiry_dates}
+    documentsForm: { documents }
   });
 
   const wrapper = mount(
@@ -30,38 +29,26 @@ test('DocumentsForm renders inputs when no documents are present', () => {
 
 test('DocumentsForm renders links when documents are present', () => {
   const documents = {
-      "deed": "workers.png",
-      "financial": "ezidox_Architecture_Diagram_-_Oct_2016.pdf",
-      "liability": "financial_2.pdf",
-      "workers": "financial1.pdf"
-    }
-  const expiry_dates = {
-      "deed": {
-        "day": "25",
-        "month": "12",
-        "year": "2016"
+      deed: {
+        filename: 'workers.png',
       },
-      "financial": {
-        "day": "25",
-        "month": "12",
-        "year": "2016"
+      financial: {
+        filename: 'ezidox_Architecture_Diagram_-_Oct_2016.pdf'
       },
-      "liability": {
-        "day": "25",
-        "month": "12",
-        "year": "2016"
+      liability: {
+        filename: 'financial_2.pdf',
+        expiry: '2020-02-01'
       },
-      "workers": {
-        "day": "25",
-        "month": "12",
-        "year": "2016"
+      workers: {
+        filename: 'financial1.pdf'
       }
     }
+  
   const store = createStore({
     form_options: {
       csrf_token: 'abc',
     },
-    documentsForm: {documents, expiry_dates}
+    documentsForm: { documents }
   });
 
   const wrapper = mount(
