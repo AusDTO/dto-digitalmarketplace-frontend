6297ea8b083081a8eed87fa89ff7b8c9c6183a61
Alexander Sadleir <maxious@users.noreply.github.com>
2017-01-05 08:52:49 +1100

    Add private fields to admin application view (#136)

--- src/shared/SellerProfile/index.js
+++ src/shared/SellerProfile/index.js
@@ -1,9 +1,11 @@
 import Body from './Body';
 import LiteHeader from './LiteHeader';
 import ReviewHeader from './ReviewHeader';
+import PrivateInfo from './PrivateInfo';
 
 export {
   Body,
   LiteHeader,
-  ReviewHeader
+  ReviewHeader,
+  PrivateInfo
 }
