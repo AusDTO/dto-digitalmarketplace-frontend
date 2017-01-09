62597f6e31c99ba172cbc3eaf91ebd9e622bb683
Nathan Flew <nath@nathf.com>
2017-01-10 10:12:58 +1100

    Refactor header and add test

---
+++ src/shared/SellerProfile/ReviewHeader.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ReviewHeader from './ReviewHeader';
+
+test('Doesn\'t display any invalid/empty/falsy badges', () => {
+  
+  const props = {
+    seller_type: {
+      indigenous: true,
+      sme: true,
+      start_up: false,
+      invalid_key: true
+    }
+  }
+
+  const component = shallow(
+    <ReviewHeader {...props} />
+  );
+
+  expect(
+    component.find('.seller-profile__badges span').length
+  ).toEqual(2);
+});
