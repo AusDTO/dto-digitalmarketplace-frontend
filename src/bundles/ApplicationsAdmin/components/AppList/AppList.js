2db93339d20a3fdc3ed7a1cd7b8afe47aa8bef28
Robert Lechte <robert.lechte@digital.gov.au>
2017-01-11 11:46:33 +1100

    add reject button, change fields

--- src/bundles/ApplicationsAdmin/components/AppList/AppList.js
+++ src/bundles/ApplicationsAdmin/components/AppList/AppList.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { convertApplicationToSeller } from '../../redux/modules/applications'
+import { convertApplicationToSeller, rejectApplication } from '../../redux/modules/applications'
+import format from 'date-fns/format';
 
-const AppList = ({meta = {}, applications, onClick}) => (
+
+const AppList = ({meta = {}, applications, onRejectClick, onAcceptClick}) => (
   <div><h2>{meta.heading}</h2>
   <table style={{margin: '0 auto', width: '100%'}}><tbody>
   <tr>
+    <th>created_at</th>
     <th>name</th>
-    <th>id</th>
-    <th>email</th>
     <th>status</th>
-    <th>action</th>
+    <th>reject</th>
+    <th>accept</th>
   </tr>
   {applications.map((a, i) =>
     <tr key={a.id}>
+      <td>{format(new Date(a.created_at), 'YYYY-MM-DD HH:mm')}</td>
       <td width="45%"><a href={meta.url_preview.concat(a.id) }>{a.name || "[no name]"}
         {a.supplier_code && (<span className="badge--default">Existing Seller</span>)}</a></td>
-      <td>{a.id}</td>
-      <td>{a.email}</td>
       <td>{a.status}</td>
+        <td>{ a.status === 'submitted' &&
+          <input onClick={e => {
+                 e.preventDefault();
+                 console.log('reject');
+                 onRejectClick(a.id);
+               }} style={{margin: '0.1rem', display: 'inline-block'}} type="button" value="Reject" />
+          }</td>
       <td>{ a.status === 'submitted' &&
         <input onClick={e => {
-               e.preventDefault()
-               onClick(a.id);
-             }} style={{margin: '1rem', display: 'inline-block'}} type="button" value="Convert to seller" />
+               e.preventDefault();
+               console.log('accept');
+               onAcceptClick(a.id);
+             }} style={{margin: '0.1rem', display: 'inline-block'}} type="button" value="Convert to seller" />
         }</td>
     </tr>
   )}
@@ -40,8 +49,11 @@ const mapStateToProps = ({applications, meta}, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onClick: (id) => {
+    onAcceptClick: (id) => {
       dispatch(convertApplicationToSeller(id))
+    },
+    onRejectClick: (id) => {
+      dispatch(rejectApplication(id))
     }
   }
 }
