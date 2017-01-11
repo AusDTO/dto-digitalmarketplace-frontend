d807223648460333f95d361bfd206c942c127754
Nathan Flew <nath@nathf.com>
2017-01-11 13:25:16 +1100

    Rows stack on mobile

--- src/shared/SellerProfile/Row.js
+++ src/shared/SellerProfile/Row.js
@@ -8,10 +8,10 @@ const Row = ({ title, children, show }) => {
     return (
         <div className="row seller-profile__row">
             <hr />
-            <div className="col-sm-3">
+            <div className="col-sm-3 col-xs-12">
                 <h4 className="seller-profile__section-title">{title}</h4>
             </div>
-            <div className="col-sm-8 col-sm-push-1">
+            <div className="col-sm-8 col-sm-push-1 col-xs-12">
                 {children}
             </div>
         </div>
