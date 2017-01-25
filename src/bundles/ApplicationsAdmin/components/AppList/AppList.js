bb60d07662e98e08ed048f40969569019f69658b
djrobstep <djrobstep@users.noreply.github.com>
2017-01-26 09:08:49 +1100

    Enhancement/apps admin (#167)
    * app list changes
    
    * corrected task status

--- src/bundles/ApplicationsAdmin/components/AppList/AppList.js
+++ src/bundles/ApplicationsAdmin/components/AppList/AppList.js
@@ -6,27 +6,38 @@ import format from 'date-fns/format';
 
 const AppList = ({meta = {}, applications, onRejectClick, onAcceptClick}) => (
   <div><h2>{meta.heading}</h2>
-  <table style={{margin: '0 auto', width: '100%'}}><tbody>
-  <tr>
-    <th>created_at</th>
-    <th>name</th>
-    <th>status</th>
-    <th>reject</th>
-    <th>accept</th>
-  </tr>
+  <table className="content-table">
+
+  <thead>
+    <tr>
+      <th>created_at</th>
+      <th>name</th>
+      <th>status</th>
+      <th>jira</th>
+      <th>actions</th>
+    </tr>
+  </thead>
+
+  <tbody>
+
   {applications.map((a, i) =>
     <tr key={a.id}>
       <td>{format(new Date(a.created_at), 'YYYY-MM-DD HH:mm')}</td>
-      <td width="45%"><a href={meta.url_preview.concat(a.id) }>{a.name || "[no name]"}
+      <td><a href={meta.url_preview.concat(a.id) }>{a.name || "[no name]"}
         {a.supplier_code && (<span className="badge--default">Existing Seller</span>)}</a></td>
       <td>{a.status}</td>
+      <td>
+        {a.tasks && a.tasks.subtasks.map((t, i) =>
+          <a className={t.status} key={t.key} href={t.link}>{t.summary}</a>
+        )}
+      </td>
         <td>{ a.status === 'submitted' &&
           <button onClick={e => {
                  e.preventDefault();
                  onRejectClick(a.id);
                }} name="Reject">Reject</button>
-          }</td>
-      <td>{ a.status === 'submitted' &&
+          }
+          { a.status === 'submitted' &&
         <button onClick={e => {
                e.preventDefault();
                onAcceptClick(a.id);
