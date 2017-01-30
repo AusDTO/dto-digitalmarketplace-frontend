1ab0ca953acc04c1342f156414da6017844299ce
Nathan Flew <nath@nathf.com>
2017-01-31 10:59:43 +1100

    Add search icon

--- src/bundles/Search/components/Catalogue/Catalogue.js
+++ src/bundles/Search/components/Catalogue/Catalogue.js
@@ -6,6 +6,7 @@ import isEmpty from 'lodash/isEmpty';
 
 import Card         from '../../../../shared/Card';
 import CheckboxList from '../../../../shared/CheckboxList';
+import Icon         from '../../../../shared/Icon';
 
 import Pagination from '../Pagination';
 
@@ -26,34 +27,50 @@ export class Catalogue extends React.Component {
             <h2 styleName="heading">Seller catalogue</h2>
           </div>
           <div className="col-xs-12 col-sm-8 col-sm-push-1" styleName="autocomplete">
-            <Autocomplete 
-              value={search.keyword || ''}
-              inputProps={{name: 'keyword', id: 'keyword'}}
-              items={results.slice(0, 10)}
-              getItemValue={({ name }) => name}
-              onSelect={(value, item) => actions.updateKeyword(value)}
-              onChange={(e, value) => actions.updateKeyword(value)}
-              renderItem={(item, isHighlighted) => (
-                <div
-                  styleName={isHighlighted ? 'autocompleteItemHighlighted' : 'autocompleteItem' }
-                  key={item.abbr}
-                  id={item.abbr}
-                >{item.name}</div>
-              )}
-              renderMenu={(items, value, style) => {
-                // renderMenu can't return null, internals of Autocomplete
-                // call clone on this result. Empty span will have to do.
-                if (!value || items.length === 0) {
-                  return <span/>;
-                }
+            <form onSubmit={e => e.preventDefault()} styleName="keyword-search">
+              <article className="col-xs-10">
+                <label htmlFor="keyword" className="visually-hidden">
+                  Search by company name, role you need or the outcome you’re after
+                </label>
+                <Autocomplete
+                  value={search.keyword || ''}
+                  inputProps={{
+                    name: 'keyword',
+                    id: 'keyword',
+                    placeholder: 'Search by company name, role you need or the outcome you’re after'
+                  }}
+                  items={results.slice(0, 10)}
+                  getItemValue={({ name }) => name}
+                  onSelect={(value, item) => actions.updateKeyword(value)}
+                  onChange={(e, value) => actions.updateKeyword(value)}
+                  renderItem={(item, isHighlighted) => (
+                    <div
+                      styleName={isHighlighted ? 'autocompleteItemHighlighted' : 'autocompleteItem' }
+                      key={item.abbr}
+                      id={item.abbr}
+                    >{item.name}</div>
+                  )}
+                  renderMenu={(items, value, style) => {
+                    // renderMenu can't return null, internals of Autocomplete
+                    // call clone on this result. Empty span will have to do.
+                    if (!value || items.length === 0) {
+                      return <span/>;
+                    }
 
-                return (
-                  <div styleName="autocompleteMenu" style={{...style, position: 'absolute', left: 0, top: '53px'}}>
-                    {items}
-                  </div>
-                )
-              }}
-            />
+                    return (
+                      <div styleName="autocompleteMenu" style={{...style, position: 'absolute', left: 0, top: '53px'}}>
+                        {items}
+                      </div>
+                    )
+                  }}
+                />
+              </article>
+              <article className="col-xs-2">
+                <button type="submit" value="" style={{ border: 'none' }}>
+                  <Icon value="search" size={22} />
+                </button>
+              </article>
+            </form>
           </div>
         </article>
         <article>
