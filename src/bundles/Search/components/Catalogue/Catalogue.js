39fcdd519a02085b740055bec61e31998f45371c
Nathan Flew <nathf@users.noreply.github.com>
2017-01-31 12:23:52 +1100

    Feature/search filters (#173)
    * Add start of filters
    
    * Add search icon
    
    * Hide pagination if no results

--- src/bundles/Search/components/Catalogue/Catalogue.js
+++ src/bundles/Search/components/Catalogue/Catalogue.js
@@ -6,6 +6,7 @@ import isEmpty from 'lodash/isEmpty';
 
 import Card         from '../../../../shared/Card';
 import CheckboxList from '../../../../shared/CheckboxList';
+import Icon         from '../../../../shared/Icon';
 
 import Pagination from '../Pagination';
 
@@ -26,34 +27,50 @@ export class Catalogue extends React.Component {
             <h2 styleName="heading">Seller catalogue</h2>
           </div>
           <div className="col-xs-12 col-sm-8 col-sm-push-1" styleName="autocomplete">
-            <Autocomplete 
-              value={search.keyword || ''}
-              inputProps={{name: 'keyword', id: 'keyword'}}
-              items={results.slice(0, 10)}
-              getItemValue={({ name }) => name}
-              onSelect={(value, item) => actions.updateKeyword(value)}
-              onChange={(e, value) => actions.updateKeyword(value)}
-              renderItem={(item, isHighlighted) => (
-                <div
-                  styleName={isHighlighted ? 'autocompleteItemHighlighted' : 'autocompleteItem' }
-                  key={item.abbr}
-                  id={item.abbr}
-                >{item.name}</div>
-              )}
-              renderMenu={(items, value, style) => {
-                // renderMenu can't return null, internals of Autocomplete
-                // call clone on this result. Empty span will have to do.
-                if (!value || items.length === 0) {
-                  return <span/>;
-                }
+            <form onSubmit={e => e.preventDefault()} styleName="keyword-search">
+              <article className="col-xs-10">
+                <label htmlFor="keyword" className="visually-hidden">
+                  Search by company name, role you need or the outcome you’re after
+                </label>
+                <Autocomplete
+                  value={search.keyword || ''}
+                  inputProps={{
+                    name: 'keyword',
+                    id: 'keyword',
+                    placeholder: 'Search by company name, role you need or the outcome you’re after'
+                  }}
+                  items={results.slice(0, 10)}
+                  getItemValue={({ name }) => name}
+                  onSelect={(value, item) => actions.updateKeyword(value)}
+                  onChange={(e, value) => actions.updateKeyword(value)}
+                  renderItem={(item, isHighlighted) => (
+                    <div
+                      styleName={isHighlighted ? 'autocompleteItemHighlighted' : 'autocompleteItem' }
+                      key={item.abbr}
+                      id={item.abbr}
+                    >{item.name}</div>
+                  )}
+                  renderMenu={(items, value, style) => {
+                    // renderMenu can't return null, internals of Autocomplete
+                    // call clone on this result. Empty span will have to do.
+                    if (!value || items.length === 0) {
+                      return <span/>;
+                    }
 
-                return (
-                  <div styleName="autocompleteMenu" style={{...style, position: 'absolute', left: 0, top: '53px'}}>
-                    {items}
-                  </div>
-                )
-              }}
-            />
+                    return (
+                      <div styleName="autocompleteMenu" style={{...style, position: 'absolute', left: 0, top: '53px'}}>
+                        {items}
+                      </div>
+                    )
+                  }}
+                />
+              </article>
+              <article className="col-xs-2">
+                <button type="submit" value="" style={{ border: 'none' }}>
+                  <Icon value="search" size={22} />
+                </button>
+              </article>
+            </form>
           </div>
         </article>
         <article>
@@ -76,25 +93,33 @@ export class Catalogue extends React.Component {
             </form>
           </aside>
           <div className="col-xs-12 col-sm-8">
-            {/*TODO*/}
-            # Sellers Found
-            <hr/>
-            {search.querying ? (
-              <h4>Searching...</h4>
-            ) : isEmpty(search.results) ? (
+
+            <article styleName="filters">
+              <strong styleName="active-filter filter">
+                <span>{search.results.length}</span> Sellers found
+              </strong>
+              <hr/>
+            </article>
+
+            {isEmpty(search.results) ? (
               <h4>No results found</h4>
             ) : (
-              search.results.map((result, i) => (
-                <Card {...result} key={i} />
-              ))
+              <div>
+                {search.results.map((result, i) => (
+                  <Card {...result} key={i} />
+                ))}
+
+                <hr/>
+
+                <Pagination
+                  {...pagination}
+                  onClick={(page) => actions.updatePage(page)}
+                  onBack={(page) => actions.updatePage(page)}
+                  onNext={(page) => actions.updatePage(page)}
+                />
+              </div>
             )}
-            <hr/>
-            <Pagination 
-              {...pagination}
-              onClick={(page) => actions.updatePage(page)}
-              onBack={(page) => actions.updatePage(page)}
-              onNext={(page) => actions.updatePage(page)}
-            />
+
           </div>
         </article>
       </section>
