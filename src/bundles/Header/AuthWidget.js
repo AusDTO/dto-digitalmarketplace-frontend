f15fe189660c574280f810473cf942a5719f26ba
Alexander Sadleir <maxious@users.noreply.github.com>
2017-02-06 10:23:48 +1100

    Make registration link customisable (#188)

--- src/bundles/Header/AuthWidget.js
+++ src/bundles/Header/AuthWidget.js
@@ -2,21 +2,34 @@ import React from 'react'
 import RegisterComponent from '../../RegisterComponent'
 
 export const AuthWidget = (props) => {
-  if (props.isAuthenticated) {
-    return (
-      <ul className="inline-links--inverted">
-        <li><a href={props.dashboardUrl}>Dashboard</a></li>
-        <li><a href={props.logoutUrl}>Log out</a></li>
-      </ul>
-    )
-  }
+    if (props.isAuthenticated) {
+        return (
+            <ul className="inline-links--inverted">
+                <li><a href={props.dashboardUrl}>Dashboard</a></li>
+                <li><a href={props.logoutUrl}>Log out</a></li>
+            </ul>
+        )
+    }
 
-  return (
-    <ul className="inline-links--inverted">
-      <li><a href={props.registerUrl}>Register</a></li>
-      <li><a href={props.loginUrl}>Log in</a></li>
-    </ul>
-  )
+    return (
+        <ul className="inline-links--inverted">
+            <li><a href={props.registerUrl}>{props.registerText}</a></li>
+            <li><a href={props.loginUrl}>Log in</a></li>
+        </ul>
+    )
 }
 
-export default new RegisterComponent({ 'auth-header': AuthWidget })
+AuthWidget.propTypes = {
+    registerUrl: React.PropTypes.string,
+    registerText: React.PropTypes.string,
+    loginUrl: React.PropTypes.string,
+    dashboardUrl: React.PropTypes.string,
+    logoutUrl: React.PropTypes.string,
+    isAuthenticated: React.PropTypes.bool
+}
+
+AuthWidget.defaultProps = {
+    registerText: 'Register'
+}
+
+export default new RegisterComponent({'auth-header': AuthWidget})
