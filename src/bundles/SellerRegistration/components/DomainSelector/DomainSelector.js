ce41ea107034e165d49e101b5a4acfdabb9abef7
Alexander Sadleir <maxious@users.noreply.github.com>
2017-02-08 09:41:24 +1100

    Registration/email copy updates (#199)

--- src/bundles/SellerRegistration/components/DomainSelector/DomainSelector.js
+++ src/bundles/SellerRegistration/components/DomainSelector/DomainSelector.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Form, Control } from 'react-redux-form';
+import {connect} from 'react-redux';
+import {Form, Control} from 'react-redux-form';
 import kebabCase from 'lodash/kebabCase';
 
 import Layout         from '../../../../shared/Layout';
@@ -13,82 +13,94 @@ import domains from './domains';
 
 class DomainSelector extends BaseForm {
 
-  render() {
-    const { model, action, csrf_token, buttonText, children, onSubmit } = this.props;
+    render() {
+        const {model, supplierCode, action, csrf_token, buttonText, children, onSubmit} = this.props;
+        if (supplierCode) {
+            var header = (        <header>
+                <h1 tabIndex="-1">What services will you offer?</h1>
+                <p>The services you’re currently approved for are already ticked. Select any others you want to provide
+                    — you’ll need to provide a case study for each as evidence for <a href="/assessment-criteria"
+                                                                                      target="_blank" rel="external">assessment</a>
+                </p>
+            </header>)
+        } else {
+            var header = (        <header>
+                <h1 tabIndex="-1">What services will you offer?</h1>
+                <p>Select the digital services you want to provide. You’ll need to provide a case study for each that
+                    meets our <a href="/assessment-criteria" target="_blank" rel="external">assessment criteria</a></p>
+                <p>If you have software products to offer, you can add them later. </p>
+            </header>)
+        }
+        return (
+            <Layout>
+                {header}
+                <article role="main">
+                    <ErrorBox focusOnMount={true} model={model}/>
 
-    return (
-      <Layout>
-        <header>
-            <h1 tabIndex="-1">What services will you offer?</h1>
-            <p> Select the digital services you want to provide. You’ll need to provide a case study for each as evidence for <a href="/assessment-criteria" target="_blank" rel="external">evaluation</a></p>
-            <p>If you have any proprietary products you wish to offer you’ll have the opportunity to add them later. </p>
-        </header>
-        <article role="main">
-          <ErrorBox focusOnMount={true} model={model}/>
+                    {/* This error will never actually render */}
+                    <StatefulError
+                        model={`${model}.services`}
+                        id="services"
+                        messages={{
+                            services: 'You must select at least one service from the services below.'
+                        }}
+                    />
 
-          {/* This error will never actually render */}
-          <StatefulError
-            model={`${model}.services`}
-            id="services"
-            messages={{
-              services: 'You must select at least one service from the services below.'
-            }}
-          />
+                    <Form
+                        model={model}
+                        action={action}
+                        method="post"
+                        id="DomainSelector__create"
+                        validators={{
+                            services: (services) => {
+                                return Object
+                                    .keys(services)
+                                    .filter(s => services[s])
+                                    .length;
+                            }
+                        }}
+                        onSubmit={onSubmit}>
 
-          <Form 
-            model={model}
-            action={action}
-            method="post"
-            id="DomainSelector__create"
-            validators={{
-              services: (services) => {
-                return Object
-                  .keys(services)
-                  .filter(s => services[s])
-                  .length;
-              }
-            }}
-            onSubmit={onSubmit}>
+                        {csrf_token && (
+                            <input type="hidden" name="csrf_token" id="csrf_token" value={csrf_token}/>
+                        )}
 
-            {csrf_token && (
-              <input type="hidden" name="csrf_token" id="csrf_token" value={csrf_token}/>
-            )}
+                        <fieldset className="field">
+                            {domains.map(({key, label, description}, i) => {
+                                return (
+                                    <div key={key}>
+                                        <Control.checkbox
+                                            model={`${model}.services.${label}`}
+                                            id={kebabCase(label)}
+                                            name={kebabCase(label)}
+                                            value={label}
+                                        />
+                                        <label htmlFor={kebabCase(label)}>{label} <p>{description}</p></label>
+                                    </div>
+                                )
+                            })}
+                        </fieldset>
 
-            <fieldset className="field">
-            {domains.map(({ key, label, description }, i) => {
-              return (
-                <div key={key}>
-                  <Control.checkbox
-                    model={`${model}.services.${label}`}
-                    id={kebabCase(label)} 
-                    name={kebabCase(label)} 
-                    value={label}
-                  />
-                  <label htmlFor={kebabCase(label)}>{label} <p>{description}</p></label>
-                </div>
-              )
-            })}
-            </fieldset>
+                        {children}
 
-            {children}
-
-            <input type="submit" value={buttonText}role="button" />
-          </Form>
-        </article>
-      </Layout>
-    )
-  }
+                        <input type="submit" value={buttonText} role="button"/>
+                    </Form>
+                </article>
+            </Layout>
+        )
+    }
 }
 
 DomainSelector.defaultProps = {
-  buttonText: 'Save and continue',
-  title: 'What services will you offer? '
+    buttonText: 'Save and continue',
+    title: 'What services will you offer? '
 }
 
 const mapStateToProps = (state) => {
-  return {
-    ...formProps(state, 'domainSelectorForm')
-  }
+    return {
+        supplierCode: state.application.supplierCode,
+        ...formProps(state, 'domainSelectorForm')
+    }
 }
 
 export default connect(mapStateToProps)(DomainSelector);
