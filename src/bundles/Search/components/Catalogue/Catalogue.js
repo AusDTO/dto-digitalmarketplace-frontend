90169d1239195fd987ec2bdb866dc87e78e5ba8f
Nathan Flew <nath@nathf.com>
2017-02-10 15:11:52 +1100

    Wrap whole catalogue in form for No JS scenario

--- src/bundles/Search/components/Catalogue/Catalogue.js
+++ src/bundles/Search/components/Catalogue/Catalogue.js
@@ -26,65 +26,65 @@ export class Catalogue extends React.Component {
 
     return (
       <section>
-        <article className="row">
-          <div className="col-xs-12 col-sm-4">
-            <h2 styleName="heading">Seller catalogue</h2>
-          </div>
-          <div className="col-xs-12 col-sm-8" styleName="autocomplete">
-            <form onSubmit={e => e.preventDefault()} styleName="keyword-search">
-              <article className="col-xs-10">
-                <label htmlFor="keyword" className="visually-hidden">
-                  Search by company name, role you need or the outcome you’re after
-                </label>
-                <Autocomplete
-                  value={search.keyword || ''}
-                  inputProps={{
-                    name: 'keyword',
-                    id: 'keyword',
-                    placeholder: 'Type to search by company name, role you need or the outcome you\'re after'
-                  }}
-                  items={search.results.slice(0, 10)}
-                  getItemValue={({ name }) => name}
-                  onSelect={(value, item) => actions.updateKeyword(value)}
-                  onChange={(e, value) => actions.updateKeyword(value)}
-                  renderItem={(item, isHighlighted) => (
-                    <div
-                      styleName={isHighlighted ? 'autocompleteItemHighlighted' : 'autocompleteItem' }
-                      key={item.abbr}
-                      id={item.abbr}
-                    >{item.name}</div>
-                  )}
-                  renderMenu={(items, value, style) => {
-                    // renderMenu can't return null, internals of Autocomplete
-                    // call clone on this result. Empty span will have to do.
-                    if (!value || items.length === 0) {
-                      return <span/>;
-                    }
+        <form onSubmit={e => e.preventDefault()}>
+          <article className="row">
+            <div className="col-xs-12 col-sm-4">
+              <h2 styleName="heading">Seller catalogue</h2>
+            </div>
+            <div className="col-xs-12 col-sm-8" styleName="autocomplete">
+              <article styleName="keyword-search">
+                <article className="col-xs-10">
+                  <label htmlFor="keyword" className="visually-hidden">
+                    Search by company name, role you need or the outcome you’re after
+                  </label>
+                  <Autocomplete
+                    value={search.keyword || ''}
+                    inputProps={{
+                      name: 'keyword',
+                      id: 'keyword',
+                      placeholder: 'Type to search by company name, role you need or the outcome you\'re after'
+                    }}
+                    items={search.results.slice(0, 10)}
+                    getItemValue={({ name }) => name}
+                    onSelect={(value, item) => actions.updateKeyword(value)}
+                    onChange={(e, value) => actions.updateKeyword(value)}
+                    renderItem={(item, isHighlighted) => (
+                      <div
+                        styleName={isHighlighted ? 'autocompleteItemHighlighted' : 'autocompleteItem' }
+                        key={item.abbr}
+                        id={item.abbr}
+                      >{item.name}</div>
+                    )}
+                    renderMenu={(items, value, style) => {
+                      // renderMenu can't return null, internals of Autocomplete
+                      // call clone on this result. Empty span will have to do.
+                      if (!value || items.length === 0) {
+                        return <span/>;
+                      }
 
-                    return (
-                      <div styleName="autocompleteMenu" style={{...style, position: 'absolute', left: 0, top: '53px'}}>
-                        {items}
-                      </div>
-                    )
-                  }}
-                />
+                      return (
+                        <div styleName="autocompleteMenu" style={{...style, position: 'absolute', left: 0, top: '53px'}}>
+                          {items}
+                        </div>
+                      )
+                    }}
+                  />
+                </article>
+                <article className="col-xs-2">
+                  <button type="submit" value="" style={{ border: 'none', backgroundColor: '#18788d', color: '#fff' }}>
+                    <Icon value="search" size={22} />
+                  </button>
+                </article>
               </article>
-              <article className="col-xs-2">
-                <button type="submit" value="" style={{ border: 'none' }}>
-                  <Icon value="search" size={22} />
-                </button>
-              </article>
-            </form>
-          </div>
-        </article>
-        <article className="row">
-          <section className="col-xs-12 col-sm-4">
-            <h4 className="local-nav-heading">Filter your results</h4>
-            <LocalNav navClassName="filter-navigation" text="Filter your results">
+            </div>
+          </article>
+          <article className="row">
+            <section className="col-xs-12 col-sm-4">
+              <h4 className="local-nav-heading">Filter your results</h4>
+              <LocalNav navClassName="filter-navigation" text="Filter your results">
 
-              <a href="">Learn more about these services</a>
+                <a href="">Learn more about these services</a>
 
-              <form onSubmit={e => e.preventDefault()}>
                 <CheckboxList
                   id="role"
                   list={search.role}
@@ -98,94 +98,95 @@ export class Catalogue extends React.Component {
                   list={search.type}
                   onChange={actions.updateType}
                 />
-              </form>
-            </LocalNav>
-          </section>
-          <div className="col-xs-12 col-sm-8">
-            {isEmpty(cards) ? (
-              <article styleName={search.querying ? 'fadeOut' : 'fadeIn'}>
-                <h2>No exact matches</h2>
-                <p>Try tweaking your search criteria for more results or <a href="" onClick={(e) => {
-                  e.preventDefault();
-                  actions.resetQuery();
-                }}>clear all and start again</a>.</p>
-              </article>
-            ) : (
-              <div styleName={search.querying ? 'fadeOut' : 'fadeIn'}>
-                <article styleName="filters" className="row">
-                  <div className="row">
-                    <div className="col-xs-12 col-sm-7">
-                      <Link
-                        to={{ search: 'view=sellers' }}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          actions.updateView('sellers');
-                        }}
-                        styleName={`${search.view === 'sellers' ? 'active-filter' : ''} filter`}>
-                        <span>{pagination.total}</span> Sellers found
-                      </Link>
-                      <Link
-                        to={{ search: 'view=products' }}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          actions.updateView('products')
-                        }}
-                        styleName={`${search.view === 'products' ? 'active-filter' : ''} filter`}>
-                        <span>{pagination.total_products}</span> Products found
-                      </Link>
+                
+              </LocalNav>
+            </section>
+            <div className="col-xs-12 col-sm-8">
+              {isEmpty(cards) ? (
+                <article styleName={search.querying ? 'fadeOut' : 'fadeIn'}>
+                  <h2>No exact matches</h2>
+                  <p>Try tweaking your search criteria for more results or <Link to="/" onClick={(e) => {
+                    e.preventDefault();
+                    actions.resetQuery();
+                  }}>clear all and start again</Link>.</p>
+                </article>
+              ) : (
+                <div styleName={search.querying ? 'fadeOut' : 'fadeIn'}>
+                  <article styleName="filters" className="row">
+                    <div className="row">
+                      <div className="col-xs-12 col-sm-7">
+                        <Link
+                          to={{ search: 'view=sellers' }}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            actions.updateView('sellers');
+                          }}
+                          styleName={`${search.view === 'sellers' ? 'active-filter' : ''} filter`}>
+                          <span>{pagination.total}</span> Sellers found
+                        </Link>
+                        <Link
+                          to={{ search: 'view=products' }}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            actions.updateView('products')
+                          }}
+                          styleName={`${search.view === 'products' ? 'active-filter' : ''} filter`}>
+                          <span>{pagination.total_products}</span> Products found
+                        </Link>
+                      </div>
+
+                      <div className="col-xs-12 col-sm-5" styleName="sortBy">
+                        <fieldset>
+                          <legend>Sort by</legend>
+                          <div>
+                            <input
+                              type="radio"
+                              name="sort_by"
+                              id="sort-a-z"
+                              value="a-z"
+                              checked={search.sort_by === 'a-z'}
+                              onChange={actions.updateSort}
+                            />
+                            <label htmlFor="sort-a-z">A to Z</label>
+                          </div>
+
+                          <div>
+                            <input
+                              type="radio"
+                              name="sort_by"
+                              id="sort-latest"
+                              value="latest"
+                              checked={search.sort_by === 'latest'}
+                              onChange={actions.updateSort}
+                            />
+                            <label htmlFor="sort-latest">Newest</label>
+                          </div>
+                        </fieldset>
+                      </div>
                     </div>
+                    <hr/>
+                  </article>
 
-                    <form className="col-xs-12 col-sm-5" styleName="sortBy">
-                      <fieldset>
-                        <legend>Sort by</legend>
-                        <div>
-                          <input
-                            type="radio"
-                            name="sort_by"
-                            id="sort-a-z"
-                            value="a-z"
-                            checked={search.sort_by === 'a-z'}
-                            onChange={actions.updateSort}
-                          />
-                          <label htmlFor="sort-a-z">A to Z</label>
-                        </div>
+                  <article>
+                    {cards.map((result, i) => (
+                      <Card {...result} key={i} />
+                    ))}
+                  </article>
 
-                        <div>
-                          <input
-                            type="radio"
-                            name="sort_by"
-                            id="sort-latest"
-                            value="latest"
-                            checked={search.sort_by === 'latest'}
-                            onChange={actions.updateSort}
-                          />
-                          <label htmlFor="sort-latest">Newest</label>
-                        </div>
-                      </fieldset>
-                    </form>
-                  </div>
                   <hr/>
-                </article>
 
-                <article>
-                  {cards.map((result, i) => (
-                    <Card {...result} key={i} />
-                  ))}
-                </article>
+                  <Pagination
+                    {...pagination}
+                    onClick={actions.updatePage}
+                    onBack={actions.updatePage}
+                    onNext={actions.updatePage}
+                  />
+                </div>
+              )}
 
-                <hr/>
-
-                <Pagination
-                  {...pagination}
-                  onClick={actions.updatePage}
-                  onBack={actions.updatePage}
-                  onNext={actions.updatePage}
-                />
-              </div>
-            )}
-
-          </div>
-        </article>
+            </div>
+          </article>
+        </form>
       </section>
     )
   }
