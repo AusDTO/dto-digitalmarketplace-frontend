74125ec6e97e565be845c0a79bd66ddf459d7ee8
Alexander Sadleir <maxious@users.noreply.github.com>
2017-02-22 13:41:46 +1100

    Fix locking submit until all other steps are completed (#252)

--- src/bundles/SellerRegistration/components/Signup/Signup.js
+++ src/bundles/SellerRegistration/components/Signup/Signup.js
@@ -147,7 +147,7 @@ class Signup extends React.Component {
 
   render() {
     const { forms, location, steps = {}, actions } = this.props;
-    const applicationValid = (this.filteredSteps.length - 1) <= Object.keys(steps).length;
+    const applicationValid = (this.filteredSteps.length - 1) <= Object.values(steps).filter(s => s == "complete").length;
     let { services = {} } = forms.domainSelectorForm;
     let { name = '', abn = '' } = forms.businessDetailsForm;
     let { representative = '', email = '' } = forms.yourInfoForm;
