6b1f9739bf13ea09a715b9d762a7be16e3bfd622
matt-sm <mattysmith@gmail.com>
2017-03-16 17:20:50 +1100

    reinstate axe

--- src/RegisterComponent.js
+++ src/RegisterComponent.js
@@ -61,6 +61,11 @@ class RegisterComponent {
 
         let result = this.instance(initialState, history);
 
+        if (process.env.NODE_ENV !== 'production') {
+          var axe = require('react-axe');
+          axe(React, ReactDOM, 1000);
+        }
+
         ReactDOM.render(
           <Router history={history}>
             {result}
