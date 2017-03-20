921d5f403d9ccce01a93e973dba854be32a08f04
matt-sm <matt-sm@users.noreply.github.com>
2017-03-20 15:40:46 +1100

    Essential case studies (#287)
    * wip
    
    * Minor bug fixes
    
    * reinstate axe
    
    * switched to stateless component
    
    * destructure props

--- src/RegisterComponent.js
+++ src/RegisterComponent.js
@@ -62,8 +62,8 @@ class RegisterComponent {
         let result = this.instance(initialState, history);
 
         if (process.env.NODE_ENV !== 'production') {
-            var axe = require('react-axe');
-            axe(React, ReactDOM, 1000);
+          var axe = require('react-axe');
+          axe(React, ReactDOM, 1000);
         }
 
         ReactDOM.render(
