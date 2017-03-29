a0eebdb087536b130d9244de9a86be59e26456d3
Alexander Sadleir <maxious@users.noreply.github.com>
2017-03-30 09:03:58 +1100

    Add commit messages to slack notification of new release (#305)

--- scripts/ci-notify.sh
+++ scripts/ci-notify.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
+GITLOG=$(git log --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
+PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
+ "attachments": [ { "fallback": "A new frontend went live!",
+ "pretext": "A new frontend went live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
+ "text": "'"$GITLOG"'" }] }'
 
-curl -g -X POST --data-urlencode 'payload={"channel": "#marketplace", "username": "releasebot", "text": "A new frontend went live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'", "icon_emoji": ":lightning:"}' "$SLACK_WEBHOOK_URL"
+#echo $PAYLOAD
+curl -g -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_WEBHOOK_URL"
