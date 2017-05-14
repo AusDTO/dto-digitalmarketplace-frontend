4004eac0b6af66901529bed7a4048d82416ea31f
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-05-15 09:56:29 +1000

    fix date formatting on team brief view styling changes

--- src/bundles/BuyerDashboard/components/Dashboard/BuyerDashboard.js
+++ src/bundles/BuyerDashboard/components/Dashboard/BuyerDashboard.js
@@ -19,60 +19,60 @@ class BuyerDashboard extends Component {
   render() {
     const {team, location} = this.props;
     let pathname = location.pathname;
-    let briefView = pathname.slice(pathname.lastIndexOf('/')+1, pathname.length);
+    let briefView = pathname.slice(pathname.lastIndexOf('/') + 1, pathname.length);
     briefView = (['teambriefs', 'teamoverview'].includes(briefView) ? briefView : 'mybriefs')
 
     return (
       <section>
-          <article styleName="team-view">
-            <section styleName="team-view-heading">
-              <div styleName="team-view-teamname">
-                {team.teamName}
+        <article styleName="team-view">
+          <section styleName="team-view-heading">
+            <div styleName="team-view-teamname">
+              {team.teamName}
+            </div>
+            <div styleName="team-view-username">
+              {team.currentUserName}
+            </div>
+          </section>
+          <article styleName="filters" className="row">
+            <div className="row">
+              <div className="col-xs-12 col-sm-12">
+                <Link to="/buyers/mybriefs"
+                      onClick={() => this.onSelectView('mybriefs')}
+                      styleName={`${briefView === 'mybriefs' ? 'active-filter' : ''} filter`}>
+                  My briefs
+                </Link>
+                <Link to="/buyers/teambriefs"
+                      onClick={() => this.onSelectView('teambriefs')}
+                      styleName={`${briefView === 'teambriefs' ? 'active-filter' : ''} filter`}>
+                  Team briefs
+                </Link>
+                <Link to="/buyers/teamoverview"
+                      onClick={() => this.onSelectView('teamoverview')}
+                      styleName={`${briefView === 'teamoverview' ? 'active-filter' : ''} filter`}>
+                  Team overview
+                </Link>
               </div>
-              <div styleName="team-view-username">
-                {team.currentUserName}
-              </div>
-            </section>
-            <article styleName="filters" className="row">
-              <div className="row">
-                <div className="col-xs-12 col-sm-12">
-                  <Link to="/buyers/mybriefs"
-                        onClick={() => this.onSelectView('mybriefs')}
-                        styleName={`${briefView === 'mybriefs' ? 'active-filter' : ''} filter`}>
-                    My briefs
-                  </Link>
-                  <Link to="/buyers/teambriefs"
-                        onClick={() => this.onSelectView('teambriefs')}
-                        styleName={`${briefView === 'teambriefs' ? 'active-filter' : ''} filter`}>
-                    Team briefs
-                  </Link>
-                  <Link to="/buyers/teamoverview"
-                        onClick={() => this.onSelectView('teamoverview')}
-                        styleName={`${briefView === 'teamoverview' ? 'active-filter' : ''} filter`}>
-                    Team overview
-                  </Link>
-                </div>
-              </div>
-              <hr/>
-            </article>
-            <article>
-              {(briefView === 'mybriefs') && (
-                <DashboardBriefs
-                  teamBriefView={false}
-                  briefs={team.briefs}
-                />
-              )}
-              {(briefView === 'teambriefs') && (
-                <DashboardBriefs
-                  teamBriefView={true}
-                  briefs={team.teamBriefs}
-                />
-              )}
-              {(briefView === 'teamoverview') && (
-                <TeamOverview {...team}/>
-              )}
-            </article>
+            </div>
+            <hr/>
           </article>
+          <article>
+            {(briefView === 'mybriefs') && (
+              <DashboardBriefs
+                teamBriefView={false}
+                briefs={team.briefs}
+              />
+            )}
+            {(briefView === 'teambriefs') && (
+              <DashboardBriefs
+                teamBriefView={true}
+                briefs={team.teamBriefs}
+              />
+            )}
+            {(briefView === 'teamoverview') && (
+              <TeamOverview {...team}/>
+            )}
+          </article>
+        </article>
       </section>
     )
   }
