a3042999cf672723df9a671aaad3e552645c0ed9
Matt <matt.smith@digital.gov.au>
2017-05-26 14:05:40 +1000

    remove reduntant info from routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,11 +2,9 @@ import Bundle from '../../src/shared/Bundle';
 
 export const routes = [
   { path: '/collaborate',
-    component: Bundle,
     widgetPath: 'bundles/Collaborate/CollaborateLandingWidget.js',
   },
   { path: '/hello',
-    component: Bundle,
     widgetPath: 'bundles/HelloWorld/HelloWorldWidget.js',
   },
 ]
