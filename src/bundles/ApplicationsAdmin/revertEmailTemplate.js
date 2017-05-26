dc5a3be00d8e8c85f737ec9a7f7387163d560013
Mark Dickie <mdickie1@gmail.com>
2017-05-26 13:02:04 +1000

    Update revertEmailTemplate.js
    Apostrophes were omitted from js template text so taking out

--- src/bundles/ApplicationsAdmin/revertEmailTemplate.js
+++ src/bundles/ApplicationsAdmin/revertEmailTemplate.js
@@ -2,7 +2,7 @@ export const templateString  = (
 
   "Thank you for submitting a seller application to the Digital Marketplace."+"\n"+"\n"+
 
-    "We've just reviewed your application and it's looking good but there's a few small items to be attended to."+"\n"+"\n"+
+    "We have just reviewed your application and it is looking good but there are a few small items to be attended to."+"\n"+"\n"+
 
     "- "+"\n"+"\n"+
 
