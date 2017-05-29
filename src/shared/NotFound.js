8919b5fd39be44ec87656bb6261c95670af695a0
Matt <matt.smith@digital.gov.au>
2017-05-29 11:49:52 +1000

    revert notfound

--- src/shared/NotFound.js
+++ src/shared/NotFound.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import Status from './Status'
+import React from 'react';
 
 const NotFound = () => (
-  <Status code={404}>
-    <div className="error-page">
-      <div className="index-page grid-row">
-        <div className="column-two-thirds">
-          <header className="page-heading-smaller">
-            <h1>Page not found</h1>
-          </header>
-          <p>
-            Check you've entered the correct web address or start again on the <a href="/">Digital Marketplace homepage</a>.
-          </p>
-          <p>
-            If you can't find what you're looking for, email <a href="mailto:marketplace@digital.gov.au?subject=Marketplace%20feedback" title="Please send feedback to marketplace@digital.gov.au">marketplace@digital.gov.au</a>
-          </p>
-        </div>
+  <div className="error-page">
+    <div className="index-page grid-row">
+      <div className="column-two-thirds">
+        <header className="page-heading-smaller">
+          <h1>Page not found</h1>
+        </header>
+        <p>
+          Check you've entered the correct web address or start again on the <a href="/">Digital Marketplace homepage</a>.
+        </p>
+        <p>
+          If you can't find what you're looking for, email <a href="mailto:marketplace@digital.gov.au?subject=Marketplace%20feedback" title="Please send feedback to marketplace@digital.gov.au">marketplace@digital.gov.au</a>
+        </p>
       </div>
     </div>
-  </Status>
+  </div>
 );
-
 export default NotFound;
