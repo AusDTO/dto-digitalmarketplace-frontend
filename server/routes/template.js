dbf4dd5a6f55089cb7d49aedc59cf63324da8dbf
Matt <matt.smith@digital.gov.au>
2017-06-08 19:35:03 +1000

    add webfonts

--- server/routes/template.js
+++ server/routes/template.js
@@ -4,6 +4,14 @@ export default (content, helmet) => `
 <!doctype html>
 <html lang="en">
   <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
+      WebFont.load({
+        google: {
+          families: ['Open+Sans:400italic,700,400:latin,latin-ext']
+        }
+      });
+    </script>
     ${helmet.meta.toString()}
     ${helmet.link.toString()}
     <script>
