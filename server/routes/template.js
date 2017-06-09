4b52d678113c93bbf82006cbaa19b1fdcced269f
matt-sm <matt-sm@users.noreply.github.com>
2017-06-09 13:22:21 +1000

    add ga (#396)
    * add ga
    
    * add tracking to head
    
    * add webfonts
    
    * add autotrack

--- server/routes/template.js
+++ server/routes/template.js
@@ -1,10 +1,44 @@
+import config from 'config'
+
 export default (content, helmet) => `
 <!doctype html>
 <html lang="en">
   <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
+      WebFont.load({
+        google: {
+          families: ['Open+Sans:400italic,700,400:latin,latin-ext']
+        }
+      });
+    </script>
     ${helmet.meta.toString()}
     ${helmet.link.toString()}
-    <link rel="stylesheet" type="text/css" href="/bundle/main.css"/>
+    <script>
+        window.ga = window.ga || function () {
+                (ga.q = ga.q || []).push(arguments)
+            };
+        ga.l = +new Date;
+        ga('create', '${config.get('trackingCode')}', 'auto');
+        ga('send', 'pageview');
+
+        ga('require', 'outboundLinkTracker', {
+            shouldTrackOutboundLink: function (link, parseUrl) {
+                var href = link.getAttribute('href') || link.getAttribute('xlink:href');
+                var url = parseUrl(href);
+                var isExternal = (url.hostname != location.hostname && url.protocol.slice(0, 4) == 'http');
+
+                // treat downloads as external links
+                var exts = 'doc*|xls*|ppt*|pdf|zip|rar|exe|mp3';
+                var regExt = new RegExp(".*\\.(" + exts + ")(\\?.*)?$");
+                var isDownload = url.pathname.match(regExt);
+
+                return isDownload || isExternal;
+            }
+        });
+        ga('require', 'urlChangeTracker');
+    </script>
+    ${helmet.script.toString()}
   </head>
   <body>
     <div id="root">${content}</div>
