1e4a75f88eeed56b2e41f1fd363f4e46a31496c1
Matt <matt.smith@digital.gov.au>
2017-06-09 18:15:50 +1000

    add ui-kit 2

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Banner = () => (
+    <section class="phase-banner">
+      <div class="wrapper">
+        <p>
+          This is a beta. For support please <a href="mailto:marketplace@digital.gov.au">contact us</a>.
+        </p>
+      </div>
+    </section>
+)
+
+export default Banner
---
+++ src/components/GovAuBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const GovAuBanner = () => (
+    <section class="govau--header">
+      <div class="wrapper">
+        <div class="govau--logo">
+          <a href="//www.gov.au" title="Go to the GOV.AU homepage" class="logo">GOV.AU</a>
+        </div>
+      </div>
+    </section>
+)
+
+export default GovAuBanner
