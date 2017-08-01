043b6ea1f2b23467d09d51255b4075b8ab679373
Marco Lavielle <marcoalfonso@gmail.com>
2017-08-01 10:53:04 +1000

    Change banner color

--- app/App.js
+++ app/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer/PageFooter'
 import {Provider} from 'react-redux';
 import configureStore from './store';
 import SignupForm from './components/SignupForm/SignupForm'
+import './App.scss'
 
 const store = configureStore();
 
