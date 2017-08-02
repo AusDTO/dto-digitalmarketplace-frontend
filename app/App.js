357a6cea49ca58b995e98a1d1b7d7079ac703212
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-08-02 10:31:04 +1000

    POC routing with RR4 Only routing from ‘signup’ and its sub routes

--- app/App.js
+++ app/App.js
@@ -5,23 +5,31 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/PageFooter'
 import {Provider} from 'react-redux';
 import configureStore from './store';
-import SignupForm from './components/SignupForm/SignupForm'
+import SignupForm from './components/SignupForm/SignupForm';
+import SignupContainer from './components/SignupForm/SignupContainer';
+import { BrowserRouter as Router, withRouter, Switch, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import Routes from './routes';
 import './App.scss'
 
 const store = configureStore();
+const history = createHistory()
 
-render (
+render(
   <Provider store={store}>
-    <div id="Application">
-      <header role="banner">
-      	<Banner />
-    		<Header />
-      </header>
-      <main id="content">
-      	<SignupForm />
-      </main>
-      <Footer />
-  	</div>
+    <Router history={history}>
+      <div id="Application">
+        <header role="banner">
+          <Banner />
+          <Header />
+        </header>
+        <main id="content">
+          <Routes/>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   </Provider>,
-  document.getElementById('appReact')
+  document.getElementById("appReact")
 );
---
+++ app/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, withRouter, Switch, Route } from 'react-router-dom';
+import SignupContainer from './components/SignupForm/SignupContainer';
+
+const NotFound = () => (
+  <div>
+    Route not found
+  </div>
+)
+
+const Routes = () => (
+  <Switch>  
+    <Route path="/signup" component={SignupContainer} />
+    <Route component={NotFound}/>
+  </Switch>  
+)
+
+export default withRouter(Routes);
