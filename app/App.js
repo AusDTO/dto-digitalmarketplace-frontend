cd3538ef4b298df9c605ee26ae5ea23fa8d4805b
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-08-02 16:09:07 +1000

    Added redux devtools and redux-logger

--- app/App.js
+++ app/App.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux';
 import configureStore from './store';
 import SignupForm from './components/SignupForm/SignupForm';
 import SignupContainer from './components/SignupForm/SignupContainer';
-import { BrowserRouter as Router, withRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
 import Routes from './routes';
--- app/store/index.js
+++ app/store/index.js
@@ -1,11 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
 
 export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk)
-  );
+  const debugware = [];
+  if (process.env.NODE_ENV !== "production") {
+    debugware.push(createLogger({ collapsed: true }));
+  }
+  const createStoreWithMiddleware = compose(
+    applyMiddleware(thunk, ...debugware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )(createStore);
+
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
