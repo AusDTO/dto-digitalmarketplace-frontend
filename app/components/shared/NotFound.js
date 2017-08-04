2c3ca269b4ef813afeea811b9d6326b0572357d9
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-08-04 11:00:55 +1000

    escape single quotes remove dev tools from production store

--- app/components/shared/NotFound.js
+++ app/components/shared/NotFound.js
@@ -8,10 +8,10 @@ const NotFound = () => (
           <h1>Page not found</h1>
         </header>
         <p>
-          Check you've entered the correct web address or start again on the <a href="/">Digital Marketplace homepage</a>.
+          Check you&#39;ve entered the correct web address or start again on the <a href="/">Digital Marketplace homepage</a>.
         </p>
         <p>
-          If you can't find what you're looking for, email <a href="mailto:marketplace@digital.gov.au?subject=Marketplace%20feedback" title="Please send feedback to marketplace@digital.gov.au">marketplace@digital.gov.au</a>
+          If you can&#39;t find what you&#39;re looking for, email <a href="mailto:marketplace@digital.gov.au?subject=Marketplace%20feedback" title="Please send feedback to marketplace@digital.gov.au">marketplace@digital.gov.au</a>
         </p>
       </div>
     </div>
--- app/store/index.js
+++ app/store/index.js
@@ -4,14 +4,16 @@ import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 
 export default function configureStore(initialState) {
-  const debugware = [];
   if (process.env.NODE_ENV !== "production") {
+    const debugware = [];
     debugware.push(createLogger({ collapsed: true }));
-  }
-  const createStoreWithMiddleware = compose(
-    applyMiddleware(thunk, ...debugware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  )(createStore);
 
-  return createStoreWithMiddleware(rootReducer, initialState);
+    const createStoreWithMiddleware = compose(
+      applyMiddleware(thunk, ...debugware),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
+  } else {
+    return createStore(rootReducer, initialState, applyMiddleware(thunk));
+  }
 }
