cc9e5c8e169783df0616c7639c79ef64b18db913
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-08-23 11:36:06 +1000

    remove string literals

--- app/routes.js
+++ app/routes.js
@@ -6,29 +6,18 @@ import NotFound from './components/shared/NotFound'
 
 export const rootPath = '/2'
 
-class Routes extends Component {
-  render() {
-    const { match } = this.props
-    return (
-      <Switch>
-        <Route exact path={match.url} component={SignupContainer} />
-        <Route path={`${match.url}/signup`} component={SignupContainer} />
-        <Route path={`${match.url}/createuser/:tokenstring`} component={CreateUser} />
-        <Route component={NotFound} />
-      </Switch>
-    )
-  }
-}
+export const Routes = ({ match }) =>
+  <Switch>
+    <Route exact path={match.url} component={SignupContainer} />
+    <Route path={match.url+'/signup'} component={SignupContainer} />
+    <Route path={match.url+'/createuser/:tokenstring'} component={CreateUser} />
+    <Route component={NotFound} />
+  </Switch>
 
-class RootContainer extends Component {
-  render() {
-    return (
+const RootContainer
       <Switch>
         <Route path={rootPath} component={Routes} />
         <Route component={NotFound} />
       </Switch>
-    )
-  }
-}
 
 export default withRouter(RootContainer)
