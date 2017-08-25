59c559744887f17657d8bf39f9fcc8d7d53e62b2
Matt <matt.smith@digital.gov.au>
2017-08-25 13:53:24 +1000

    fix tests

--- app/actions/memberActions.test.js
+++ app/actions/memberActions.test.js
@@ -1,24 +1,16 @@
 import expect from 'expect'
 import {
   handleMemberInfoSuccess,
-  handleMemberInfoFailure,
   handleSignupSuccess,
-  handleSignupFailure,
   handleLoadSignupSuccess,
-  handleLoadSignupFailure,
   handleCreateUserSuccess,
-  handleCreateUserFailure
+  setErrorMessage
 } from './memberActions'
 import {
   MEMBER_INFO_FETCH_DATA_SUCCESS,
-  MEMBER_INFO_HAS_ERRORED,
   LOAD_SIGNUP_SUCCESS,
-  LOAD_SIGNUP_FAILURE,
   SIGNUP_SUCCESS,
-  SIGNUP_FAILURE,
-  SIGNUP_DUPLICATE_FAILURE,
   CREATE_USER_SUCCESS,
-  CREATE_USER_DUPLICATE_FAILURE,
   CREATE_USER_FAILURE
 } from '../constants/constants'
 
@@ -47,26 +39,6 @@ describe('Member Actions', () => {
       expect(action).toEqual(expectedAction)
       expect(action.memberInfo.userType).toEqual('buyer')
     })
-
-    it('should create a MEMBER_INFO_HAS_ERRORED action', () => {
-      // arrange
-      const memberInfoResponse = {
-        status: 500,
-        data: {
-          message: 'Something bad happened on the server and request to ping failed'
-        }
-      }
-      const expectedAction = {
-        type: MEMBER_INFO_HAS_ERRORED,
-        errorMessage: memberInfoResponse.data.message
-      }
-
-      //act
-      const action = handleMemberInfoFailure(memberInfoResponse)
-
-      //assert
-      expect(action).toEqual(expectedAction)
-    })
   })
 
   describe('Signup new user form submit', () => {
@@ -85,46 +57,6 @@ describe('Member Actions', () => {
 
       //act
       const action = handleSignupSuccess(submitSignupResponse)
-
-      //assert
-      expect(action).toEqual(expectedAction)
-    })
-
-    it('should create a SIGNUP_FAILURE action', () => {
-      // arrange
-      const submitSignupResponse = {
-        status: 400,
-        data: {
-          message: 'Error occured during signup api request'
-        }
-      }
-      const expectedAction = {
-        type: SIGNUP_FAILURE,
-        errorMessage: submitSignupResponse.data.message
-      }
-
-      //act
-      const action = handleSignupFailure(submitSignupResponse)
-
-      //assert
-      expect(action).toEqual(expectedAction)
-    })
-
-    it('should create a SIGNUP_DUPLICATE_FAILURE action', () => {
-      // arrange
-      const submitSignupResponse = {
-        status: 409,
-        data: {
-          message: 'You tried to signup for an account with a duplicate email address'
-        }
-      }
-      const expectedAction = {
-        type: SIGNUP_DUPLICATE_FAILURE,
-        errorMessage: submitSignupResponse.data.message
-      }
-
-      //act
-      const action = handleSignupFailure(submitSignupResponse)
 
       //assert
       expect(action).toEqual(expectedAction)
@@ -152,26 +84,6 @@ describe('Member Actions', () => {
       //assert
       expect(action).toEqual(expectedAction)
     })
-
-    it('should create a LOAD_SIGNUP_FAILURE action', () => {
-      // arrange
-      const loadSignupResponse = {
-        status: 400,
-        data: {
-          message: 'Error occured during complete signup api request'
-        }
-      }
-      const expectedAction = {
-        type: LOAD_SIGNUP_FAILURE,
-        errorMessage: loadSignupResponse.data.message
-      }
-
-      //act
-      const action = handleLoadSignupFailure(loadSignupResponse)
-
-      //assert
-      expect(action).toEqual(expectedAction)
-    })
   })
 
   describe('create new user form', () => {
@@ -192,46 +104,6 @@ describe('Member Actions', () => {
 
       //act
       const action = handleCreateUserSuccess(createUserResponse)
-
-      //assert
-      expect(action).toEqual(expectedAction)
-    })
-
-    it('should create a CREATE_USER_DUPLICATE_FAILURE action', () => {
-      // arrange
-      const createUserResponse = {
-        status: 409,
-        data: {
-          message: 'Duplicate user error message'
-        }
-      }
-      const expectedAction = {
-        type: CREATE_USER_DUPLICATE_FAILURE,
-        errorMessage: createUserResponse.data.message
-      }
-
-      //act
-      const action = handleCreateUserFailure(createUserResponse)
-
-      //assert
-      expect(action).toEqual(expectedAction)
-    })
-
-    it('should create a CREATE_USER_FAILURE action', () => {
-      // arrange
-      const createUserResponse = {
-        status: 400,
-        data: {
-          message: 'Other user error message'
-        }
-      }
-      const expectedAction = {
-        type: CREATE_USER_FAILURE,
-        errorMessage: createUserResponse.data.message
-      }
-
-      //act
-      const action = handleCreateUserFailure(createUserResponse)
 
       //assert
       expect(action).toEqual(expectedAction)
