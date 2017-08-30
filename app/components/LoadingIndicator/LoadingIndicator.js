236a51eeff03ff0a780c55c97784f0aef5e0c10d
Marco Lavielle <marcoalfonso@gmail.com>
2017-08-30 16:29:39 +1000

    Fix linting

--- app/components/LoadingIndicator/LoadingIndicator.js
+++ app/components/LoadingIndicator/LoadingIndicator.js
@@ -8,7 +8,7 @@ import React from 'react'
 function LoadingIndicator() {
   return (
     <div className="loading-indicator">
-      <div className="loading-inner"></div>
+      <div className="loading-inner" />
     </div>
   )
 }
