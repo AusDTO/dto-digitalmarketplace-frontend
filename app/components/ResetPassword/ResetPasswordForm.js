b889a8e7e636b444175bcf6087895ed520f36b9b
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-08-30 16:00:27 +1000

    matching password validation

--- app/components/ResetPassword/ResetPasswordForm.js
+++ app/components/ResetPassword/ResetPasswordForm.js
@@ -3,7 +3,7 @@ import { Form } from 'react-redux-form'
 import ErrorBox from '../../components/shared/form/ErrorBox'
 import Textfield from '../../components/shared/form/Textfield'
 import PageAlert from '@gov.au/page-alerts'
-import { passwordLength, passwordsMatch } from '../validators'
+import { passwordLength } from '../validators'
 import { rootPath } from '../../routes'
 
 export const ResetPasswordForm = ({ model, user, submitClicked, handleSubmit }) => {
@@ -37,7 +37,7 @@ export const ResetPasswordForm = ({ model, user, submitClicked, handleSubmit }) 
               <h4>You have successfully changed your password</h4>
               <span>
                 <p>
-                  Please <a href={`${rootPath}/login`}> login </a> to continue.
+                  Please <a href="/login"> login </a> to continue.
                 </p>
               </span>
             </PageAlert>}
@@ -45,17 +45,7 @@ export const ResetPasswordForm = ({ model, user, submitClicked, handleSubmit }) 
           <header className="page-heading page-heading-without-breadcrumb">
             <h1 className="uikit-display-5">Reset password</h1>
           </header>
-          <Form
-            model={model}
-            id="resetPassword"
-            validators={{
-              '': {
-                // Form-level validator
-                passwordsMatch: val => passwordsMatch(val)
-              }
-            }}
-            onSubmit={model => handleSubmit(model)}
-          >
+          <Form model={model} id="resetPassword" onSubmit={model => handleSubmit(model)}>
             <Textfield
               model={`${model}.password`}
               name="password"
