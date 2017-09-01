960465eafd6de0e4417339e3781cc614d4b0811f
Marco Lavielle <marcoalfonso@gmail.com>
2017-09-01 13:22:28 +1000

    Linting fix

--- app/components/LoadingIndicator/LoadingIndicator.js
+++ app/components/LoadingIndicator/LoadingIndicator.js
@@ -4,25 +4,17 @@
  */
 
 import React from 'react'
-import PropTypes from 'prop-types'
 import styles from './LoadingIndicator.scss'
 import classNames from 'classnames'
 
 function LoadingIndicator(props) {
+  const wrapper = classNames(props.isLoadingButton ? styles.buttonLoadingIndicator : '', styles.loadingIndicator)
 
-  const wrapper = classNames(
-    props.isLoadingButton ? styles.buttonLoadingIndicator : '',
-    styles.loadingIndicator
-  )
-
-  const inner = classNames(
-    props.isLoadingButton ? styles.buttonLoadingInner : '',
-    styles.loadingInner
-  )
+  const inner = classNames(props.isLoadingButton ? styles.buttonLoadingInner : '', styles.loadingInner)
 
   return (
     <div className={wrapper}>
-      <div className={inner}/>
+      <div className={inner} />
     </div>
   )
 }
