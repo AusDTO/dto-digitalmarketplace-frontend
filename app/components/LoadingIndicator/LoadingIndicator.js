5cb3652c267acaf85d9785c86833499c23d34d39
Marco Lavielle <marcoalfonso@gmail.com>
2017-09-01 13:20:18 +1000

    Refactor into css modules

--- app/components/LoadingIndicator/LoadingIndicator.js
+++ app/components/LoadingIndicator/LoadingIndicator.js
@@ -4,11 +4,25 @@
  */
 
 import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './LoadingIndicator.scss'
+import classNames from 'classnames'
 
-function LoadingIndicator() {
+function LoadingIndicator(props) {
+
+  const wrapper = classNames(
+    props.isLoadingButton ? styles.buttonLoadingIndicator : '',
+    styles.loadingIndicator
+  )
+
+  const inner = classNames(
+    props.isLoadingButton ? styles.buttonLoadingInner : '',
+    styles.loadingInner
+  )
+
   return (
-    <div className="loading-indicator">
-      <div className="loading-inner" />
+    <div className={wrapper}>
+      <div className={inner}/>
     </div>
   )
 }
