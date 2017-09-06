d2ebd95d453e8c2b500d862cbb0b69af0bdca28a
Matt <matt.smith@digital.gov.au>
2017-09-06 16:10:23 +1000

    reenable linting

--- apps/orams/index.js
+++ apps/orams/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
-const App = () =>
-  <div>
-    ORAMS
-  </div>
+const App = () => <div>ORAMS</div>
 
 ReactDOM.render(<App />, document.getElementById('root'))
