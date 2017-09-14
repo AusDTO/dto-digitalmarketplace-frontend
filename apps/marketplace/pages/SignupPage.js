720ce9d27b9e84ad585823f47acf560aa25733cb
Mark Dickie <markdickie@dtos-MacBook-Pro.local>
2017-09-14 16:40:03 +1000

    refactor SignupPage per style guide

---
+++ apps/marketplace/pages/SignupPage.js
@@ -0,0 +1,130 @@
+/* eslint-disable camelcase */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+import { required, validEmail, governmentEmail } from '../components/validators'
+import SignupForm from '../components/SignupForm/SignupForm'
+import UserOnboardingContainer from '../components/Onboarding/OnboardingContainer'
+import BaseForm from '../components/shared/form/BaseForm'
+import formProps from '../components/shared/form/formPropsSelector'
+import { handleSignupSubmit } from '../actions/memberActions'
+import { rootPath } from '../routes'
+
+export class SignupPageComponent extends BaseForm {
+  constructor(props) {
+    super(props)
+    this.state = {
+      emailValidators: {
+        required,
+        validEmail
+      },
+      emailErrorMessages: {
+        required: 'Your email is required',
+        validEmail: 'A validly formatted email is required.',
+        governmentEmail: ' Email should have a government domain.'
+      },
+      isBuyer: this.props.signupForm.user_type === 'buyer',
+      submitClicked: null
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.signupForm.user_type !== nextProps.signupForm.user_type) {
+      if (nextProps.signupForm.user_type === 'seller') {
+        this.setState({
+          emailValidators: {
+            required,
+            validEmail
+          },
+          emailErrorMessages: {
+            required: 'Your email is required',
+            validEmail: 'A validly formatted email is required.'
+          },
+          isBuyer: false
+        })
+      }
+
+      if (nextProps.signupForm.user_type === 'buyer') {
+        this.setState({
+          emailValidators: {
+            required,
+            validEmail,
+            governmentEmail
+          },
+          emailErrorMessages: {
+            required: 'Your email is required',
+            validEmail: 'A validly formatted email is required.',
+            governmentEmail: ' Email should have a government domain.'
+          },
+          isBuyer: true
+        })
+      }
+    }
+  }
+
+  handleSubmit = model => {
+    this.props.handleSignupSubmit(model)
+  }
+
+  onSubmitFailed = () => {
+    window.scrollTo(0, 0)
+  }
+
+  onSubmitClicked = () => {
+    this.setState({
+      submitClicked: new Date().valueOf()
+    })
+  }
+
+  render() {
+    const { match } = this.props
+
+    // TODO: will move UserOnboardingContainer out of here when CreateUser is refactored
+    return (
+      <div id="signup-page">
+        <Switch>
+          <Route
+            exact
+            path={match.url}
+            render={() =>
+              <SignupForm
+                {...this.props}
+                {...this.state}
+                onSubmitClicked={this.onSubmitClicked}
+                onSubmitFailed={this.onSubmitFailed}
+                handleSubmit={this.handleSubmit}
+              />}
+          />
+          <Route path={`${match.url}/success`} component={UserOnboardingContainer} />
+          <Redirect to={{ pathname: `${rootPath}/signup` }} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+SignupPageComponent.propTypes = {
+  signupSuccess: PropTypes.bool,
+  currentlySending: PropTypes.bool
+}
+
+SignupPageComponent.defaultProps = {
+  signupSuccess: null,
+  currentlySending: null
+}
+
+const mapStateToProps = state => ({
+  ...formProps(state, 'signupForm'),
+  signupSuccess: state.user.signupSuccess,
+  currentlySending: state.app.currentlySending
+})
+
+const mapDispatchToProps = dispatch => ({
+  handleSignupSubmit: model => dispatch(handleSignupSubmit(model))
+})
+
+const SignupPage = withRouter(connect(mapStateToProps, mapDispatchToProps)(SignupPageComponent))
+
+export default SignupPage
