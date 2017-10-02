da5411dc4efbf911165f86d7ae33b4b1006355bf
matt-sm <matt-sm@users.noreply.github.com>
2017-10-03 10:06:12 +1100

    show a loading indicator when ping has not returned (#533)
    * show a loading indicator when ping has not returned
    
    * fix prop types

---
+++ apps/marketplace/MarketplaceAuthenticatedRoute.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Route, Redirect, withRouter } from 'react-router-dom'
+import LoadingIndicator from 'shared/LoadingIndicator/LoadingIndicator'
+import { rootPath } from 'marketplace/routes'
+
+const PrivateRouteComponent = props => {
+  const { component: Component, loggedIn, customRedirectPath, currentlySending, ...rest } = props
+  return (
+    <Route
+      {...rest}
+      render={values => {
+        if (loggedIn) {
+          return <Component {...values} />
+        }
+
+        return currentlySending
+          ? <LoadingIndicator />
+          : <Redirect
+              to={{
+                pathname: customRedirectPath || `${rootPath}/login`,
+                state: { from: values.location }
+              }}
+            />
+      }}
+    />
+  )
+}
+
+PrivateRouteComponent.defaultProps = {
+  customRedirectPath: null,
+  currentlySending: false
+}
+
+PrivateRouteComponent.propTypes = {
+  component: PropTypes.func.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+  customRedirectPath: PropTypes.string,
+  currentlySending: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.app.loggedIn,
+  currentlySending: state.app.currentlySending
+})
+
+const PrivateRoute = withRouter(connect(mapStateToProps)(PrivateRouteComponent))
+
+export default PrivateRoute
--- apps/orams/routes.js
+++ apps/orams/routes.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { withRouter, Switch, Route } from 'react-router-dom'
+import PrivateRoute from 'orams/OramsAuthenticatedRoute'
 import HomePage from 'orams/pages/HomePage'
 import LoginPage from 'orams/pages/LoginPage'
 import LogoutPage from 'orams/pages/LogoutPage'
 import EditProfilePage from 'orams/pages/EditProfilePage'
 import NotFound from 'shared/NotFound'
-import PrivateRoute from 'shared/PrivateRoute'
 import ResetPasswordPage from 'orams/pages/ResetPasswordPage'
 
 export const rootPath = '/orams'
@@ -15,8 +15,8 @@ export const Routes = () =>
     <Route exact path={rootPath} component={HomePage} />
     <Route path={`${rootPath}/login`} component={LoginPage} />
     <Route path={`${rootPath}/logout`} component={LogoutPage} />
+    <PrivateRoute path={`${rootPath}/edit-profile`} component={EditProfilePage} />
     <Route path={`${rootPath}/reset-password`} component={ResetPasswordPage} />
-    <PrivateRoute path={`${rootPath}/edit-profile`} component={EditProfilePage} customRedirectPath="/orams/login" />
     <Route component={NotFound} />
   </Switch>
 
