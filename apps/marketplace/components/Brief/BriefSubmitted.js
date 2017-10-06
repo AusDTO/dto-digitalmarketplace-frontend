33dbd031cc9cfa4edcc9096c5c7ff4caddb26e0b
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-10-06 14:21:36 +1100

    Add screen for published brief confirmation with feedback widget

---
+++ apps/marketplace/components/Brief/BriefSubmitted.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import format from 'date-fns/format'
+import Feedback from 'marketplace/components/shared/form/Feedback'
+
+import PageAlert from '@gov.au/page-alerts'
+
+const BriefSubmitted = props =>
+  <div className="row">
+    <div className="col-sm-push-2 col-sm-8 col-xs-12">
+      <article role="main">
+        <PageAlert as="success" setFocus={props.setFocus}>
+          <h4>Your brief has been published</h4>
+
+          <p>
+            It will be open {' '}
+            {props.brief &&
+              <span>
+                until {format(new Date(props.brief.applicationsClosedAt), 'MMMM Do, YYYY [at] ha [in Canberra]')}
+              </span>}
+          </p>
+        </PageAlert>
+        <br />
+        <h1 className="uikit-display-4" style={{ fontWeight: 'bold' }}>
+          What happens next?
+        </h1>
+        <ul>
+          <li>
+            While you’re brief is live you’ll need to{' '}
+            <a href="https://marketplace1.zendesk.com/hc/en-gb/articles/115011257447-Answer-questions">
+              answer sellers questions
+            </a>.
+          </li>
+          <li>
+            Once your brief closes, you can{' '}
+            <a href="https://marketplace1.zendesk.com/hc/en-gb/articles/115011257487-Shortlist-responses">
+              shortlist seller responses
+            </a>.
+          </li>
+        </ul>
+        If you need help at any time, <a href="https://marketplace1.zendesk.com/hc/en-gb/requests/new">contact us</a>.
+        <br />
+        <br />
+        <br />
+        <br />
+        <Feedback
+          app={props.app}
+          handleSubmit={props.handleSubmit}
+          difficultyQuestion="How easy or difficult was it for you to provide a response to this brief?"
+          commentQuestion="How would you improve publishing a brief?"
+          objectAction="published"
+        />
+      </article>
+    </div>
+  </div>
+
+export default BriefSubmitted
---
+++ apps/marketplace/components/shared/form/Feedback.js
@@ -0,0 +1,139 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Control, LocalForm } from 'react-redux-form'
+
+import Textarea from 'shared/form/Textarea'
+import Icon from 'shared/Icon'
+import PageAlert from '@gov.au/page-alerts'
+
+import styles from './scss/Feedback.scss'
+
+export class Feedback extends React.Component {
+  onDifficultyChange = val => {
+    if (val.target.checked) {
+      this.props.handleSubmit({
+        difficulty: val.target.value,
+        difficultyQuestion: this.props.difficultyQuestion,
+        objectAction: this.props.objectAction
+      })
+    }
+  }
+
+  render() {
+    return (
+      <span>
+        {this.props.app && this.props.app.feedbackSuccess
+          ? <PageAlert as="success">
+              <h4>Your feedback has been sent, thank you for helping us improve the Marketplace.</h4>
+            </PageAlert>
+          : <LocalForm onSubmit={val => this.props.handleSubmit(val)}>
+              <Control.input type="hidden" model=".commentQuestion" defaultValue={this.props.commentQuestion} />
+              <Control.input type="hidden" model=".difficultyQuestion" defaultValue={this.props.difficultyQuestion} />
+              <Control.input type="hidden" model=".objectAction" defaultValue={this.props.objectAction} />
+              <fieldset className={styles.difficulty}>
+                <legend>
+                  <b>
+                    {this.props.difficultyQuestion}
+                  </b>
+                </legend>
+                <Control.radio
+                  model=".difficulty"
+                  type="radio"
+                  name="difficulty"
+                  id="easy"
+                  value="easy"
+                  controlProps={{
+                    onChange: this.onDifficultyChange
+                  }}
+                />
+                <label htmlFor="easy" className="uikit-btn uikit-btn--tertiary">
+                  <div>
+                    <Icon
+                      value="smile-o"
+                      color="#000000"
+                      size={24}
+                      style={{
+                        position: 'relative',
+                        right: '10px',
+                        top: '6px',
+                        float: 'right'
+                      }}
+                    />Easy
+                  </div>
+                </label>
+                <Control.radio
+                  model=".difficulty"
+                  type="radio"
+                  name="difficulty"
+                  id="ok"
+                  value="ok"
+                  controlProps={{
+                    onChange: this.onDifficultyChange
+                  }}
+                />
+                <label htmlFor="ok" className="uikit-btn uikit-btn--tertiary" style={{ width: '115px' }}>
+                  <div>
+                    <Icon
+                      value="meh-o"
+                      color="#000000"
+                      size={24}
+                      style={{
+                        position: 'relative',
+                        right: '25px',
+                        top: '6px',
+                        float: 'right'
+                      }}
+                    />OK
+                  </div>
+                </label>
+                <Control.radio
+                  model=".difficulty"
+                  type="radio"
+                  name="difficulty"
+                  id="difficult"
+                  value="difficult"
+                  controlProps={{
+                    onChange: this.onDifficultyChange
+                  }}
+                />
+                <label htmlFor="difficult" className="uikit-btn uikit-btn--tertiary" style={{ width: '155px' }}>
+                  <div style={{ width: '6.5em' }}>
+                    <Icon
+                      value="frown-o"
+                      color="#000000"
+                      size={24}
+                      style={{
+                        position: 'relative',
+                        right: '10px',
+                        top: '6px',
+                        float: 'right'
+                      }}
+                    />Difficult
+                  </div>
+                </label>
+              </fieldset>
+              <Textarea
+                id="comment"
+                name="comment"
+                label={this.props.commentQuestion}
+                model=".comment"
+                controlProps={{ limit: 150 }}
+              />
+              <button className="uikit-btn">Send feedback</button>
+            </LocalForm>}
+      </span>
+    )
+  }
+}
+
+Feedback.defaultProps = {}
+
+Feedback.propTypes = {
+  app: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  difficultyQuestion: PropTypes.string.isRequired,
+  commentQuestion: PropTypes.string.isRequired,
+  objectAction: PropTypes.string.isRequired
+}
+
+export default Feedback
