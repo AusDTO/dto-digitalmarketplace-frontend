3fc463f83fc13c0d55552a7510b5a40efd8b0099
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-10-13 10:51:53 +1100

    add git log to slack release message [ci skip]

--- scripts/ci-notify.sh
+++ scripts/ci-notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
-GITLOG=$(git log --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
+GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
 PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
  "attachments": [ { "fallback": "A new frontend went live!",
  "pretext": "A new frontend went live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
