0121f51e28b2a649884bd7e806dc2eecf11e4c54
Nathan Davison <nathan.davison@digital.gov.au>
2018-03-01 14:39:55 +1100

    Apply changes to the revert and reject email templates.

--- src/bundles/ApplicationsAdmin/revertEmailTemplate.js
+++ src/bundles/ApplicationsAdmin/revertEmailTemplate.js
@@ -1,17 +1,31 @@
 export const templateString  = (
 
-  "Thank you for submitting a seller application to the Digital Marketplace."+"\n"+"\n"+
-
-    "We have just reviewed your application and it is looking good but there are a few small items to be attended to."+"\n"+"\n"+
+  "Thanks for applying to join the Digital Marketplace. We’ve reviewed your application and require more detail or changes to your profile:"+"\n"+"\n"+
 
     "- "+"\n"+"\n"+
 
-    "We have reverted your application so you can sign in, amend, then resubmit your application. Please contact us if you require further clarification, or would like to discuss with our team."+"\n"+"\n"+
+    "To update your profile:"+"\n"+"\n"+
 
-    "https://marketplace.service.gov.au/sellers/application"+"\n"+"\n"+
+    "- access [Digital Marketplace](https://marketplace.service.gov.au) and sign in"+"\n"+"\n"+
+    "- update your profile as advised"+"\n"+"\n"+
+    "- resubmit your application for review"+"\n"+"\n"+
 
+    "We recommend you read our support guides on seller profiles and case studies:"+"\n"+"\n"+
 
-    "Regards"+"\n"+"\n"+
+    "- read the useful [Seller guide and FAQs](https://marketplace1.zendesk.com/hc/en-gb/categories/115001540368-Seller-guide-and-FAQs)"+"\n"+
+    "- see the digital [areas of expertise](https://marketplace1.zendesk.com/hc/en-gb/articles/115011258607) in the marketplace"+"\n"+
+    "- understand the relevant [assessment criteria](https://marketplace1.zendesk.com/hc/en-gb/articles/333757011655-Assessment-criteria) for each area of expertise you nominate"+"\n"+
+    "- create a compelling [case study](https://marketplace1.zendesk.com/hc/en-gb/articles/115011407668-Adding-case-studies) to showcase your digital expertise"+"\n"+"\n"+
 
-    "The Marketplace team"
+    "Once you’ve resubmitted your profile updates, we’ll review it again and advise you via email. "+"\n"+"\n"+
+
+    "Thanks,"+"\n"+"\n"+
+
+    "The Digital Marketplace team"+"\n"+"\n"+
+
+    "Digital Marketplace <https://marketplace.service.gov.au>"+"\n"+"\n"+
+    "Digital Transformation Agency (DTA) <https://www.dta.gov.au>"+"\n"+"\n"+
+
+    "[Terms of Use](https://marketplace.service.gov.au/terms-of-use) | [Privacy](https://marketplace.service.gov.au/privacy-policy) | [Copyright](https://marketplace.service.gov.au/copyright)"
+
 );
