c389309370314ab2b237e5eaf828ad1ea6ee05fe
Nathan <nathan.davison@digital.gov.au>
2018-03-13 11:25:38 +1100

    Change email content (#630)
    * Apply changes to the revert and reject email templates.
    
    * Fix formatting of the footer links.
    
    * Update snapshot.
    
    * Update formatting on reject notification.

--- src/bundles/ApplicationsAdmin/rejectEmailTemplate.js
+++ src/bundles/ApplicationsAdmin/rejectEmailTemplate.js
@@ -1,9 +1,5 @@
 export const templateString = (
+  
+  "Unfortunately your case study does not meet our requirements as XXXX."
 
-  "We've assessed your case study and found that it didn't meet the [**assessment criteria**](https://marketplace.service.gov.au/assessment-criteria) . \n" +
-  "To offer this service through the Marketplace in future, you’ll need to update your seller profile, placing particular focus on the case study " +
-  "you provide and ensuring it meets the [**assessment criteria**](https://marketplace.service.gov.au/assessment-criteria) we have provided. " +
-  "You can edit your case studies by navigating to your seller [**Dashboard**](https://marketplace.service.gov.au/sellers) and updating your profile. \n\n" +
-  "You may also find this [**guide to writing case studies**](https://marketplace.service.gov.au/static/media/documents/Digital%20Marketplace%20case%20study%20template.xlsx) useful. \n\n" +
-  "The Marketplace team"
 )
