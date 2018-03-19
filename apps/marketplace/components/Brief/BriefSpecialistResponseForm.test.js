dc2ef311a93ad93c0f2c84deb3ae266bd8e2fa35
Matt <matt.smith@digital.gov.au>
2018-03-19 17:06:25 +1100

    re-add original submitted form + a test

---
+++ apps/marketplace/components/Brief/BriefSpecialistResponseForm.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { Provider } from 'react-redux'
+import configureStore from 'marketplace/store'
+import BriefSpecialistResponseForm from './BriefSpecialistResponseForm'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const store = configureStore()
+test('BriefSpecialistResponseForm rendersu', () => {
+  const tree = shallow(
+    <Provider store={store}>
+      <BriefSpecialistResponseForm/>
+    </Provider>
+  )
+
+  expect(tree).toMatchSnapshot()
+})
