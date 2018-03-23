8560f2c32676ce8fa3fe95a0b5456373ceb57ef2
Nathan Davison <nathan.davison@digital.gov.au>
2018-03-23 15:09:02 +1100

    Update the dashboard link for buyers.

--- apps/marketplace/components/Header/DashBoardLink.js
+++ apps/marketplace/components/Header/DashBoardLink.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react'
 import PropTypes from 'prop-types'
+import { rootPath } from 'marketplace/routes'
 
 const DashBoardLink = props => {
   const { userType } = props
   return (
     <span>
       {userType === 'buyer'
-        ? <a href="/2/buyer-dashboard">Dashboard</a>
+        ? <a href={`${rootPath}/buyer-dashboard`}>Dashboard</a>
         : userType === 'applicant'
           ? <a href="/sellers/application">Continue application</a>
           : <a href="/sellers">Dashboard</a>}
