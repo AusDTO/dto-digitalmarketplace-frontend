cfd174127146da473bd44af67864d606312b71a1
Nathan Davison <nathan.davison@digital.gov.au>
2018-03-28 10:03:53 +1100

    Change the live brief status from 'published' to 'open'.

--- apps/marketplace/components/helpers.js
+++ apps/marketplace/components/helpers.js
@@ -3,6 +3,6 @@
 export const uniqueID = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
 
 export const statusConvert = (status = '') => {
-  const newStatus = status === 'live' ? 'published' : status
+  const newStatus = status === 'live' ? 'open' : status
   return `${newStatus.charAt(0).toUpperCase()}${newStatus.slice(1)}`
 }
