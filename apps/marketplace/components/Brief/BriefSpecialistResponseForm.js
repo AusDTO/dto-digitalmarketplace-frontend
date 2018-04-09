5fdb4317ec7149fe5fdf4591a689930cfa9afd8c
Matt <matt.smith@digital.gov.au>
2018-04-09 14:41:35 +1000

    make error message accessible

--- apps/marketplace/components/Brief/BriefSpecialistResponseForm.js
+++ apps/marketplace/components/Brief/BriefSpecialistResponseForm.js
@@ -154,7 +154,7 @@ const BriefSpecialistResponseForm = ({
                         validators={{ required }}
                         showMessagesDuringFocus
                         messages={{
-                          required: `This is an essential requirement.`
+                          required: `${requirement} is required`
                         }}
                       />
                     )}
