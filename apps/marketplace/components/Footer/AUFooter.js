d55e6b539845e15ec7895099d58338a0fcc0a168
Marco Lavielle <marcoalfonso@gmail.com>
2018-05-30 13:40:43 +1000

    Update footer copy

--- apps/marketplace/components/Footer/AUFooter.js
+++ apps/marketplace/components/Footer/AUFooter.js
@@ -34,21 +34,20 @@ const AUFooter = () => (
           <div className="au-footer__end">
             <div className="footer__content footer__legal">
               <p>
-                © Commonwealth of Australia. With the exception of the Commonwealth Coat of Arms and where otherwise
-                noted, this work is licensed under the{' '}
-                <a href="https://github.com/govau/uikit/blob/master/LICENSE" rel="external">
-                  MIT license
-                </a>
+                An initiative of the <a href="https://www.dta.gov.au/">Digital Transformation Agency</a>.{' '}
+                This program forms part of the National Innovation and Science <a href="https://innovation.gov.au/page/agenda">Agenda</a>.{' '}
+                With the exception of the Commonwealth Coat of Arms and where otherwise noted,{' '}
+                this work is licensed under the <a href="https://github.com/govau/uikit/blob/master/LICENSE" rel="external">MIT license</a>
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 col-md-pull-9 footer__logo">
           <div className="au-footer-logo-dta">
-            <div dangerouslySetInnerHTML={{ __html: logoDTA }} />
+            <a href="https://www.dta.gov.au/"><div dangerouslySetInnerHTML={{ __html: logoDTA }} /></a>
           </div>
           <div className="au-footer-logo-nisa">
-            <div dangerouslySetInnerHTML={{ __html: logoNISA }} />
+            <a href="https://innovation.gov.au/page/agenda"><div dangerouslySetInnerHTML={{ __html: logoNISA }} /></a>
           </div>
         </div>
       </div>
