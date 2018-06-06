9a98e8285d795ad33cd3b03c6c1fb7e9cc6755cb
Nathan Davison <nathan.davison@digital.gov.au>
2018-06-06 16:06:21 +1000

    Move the opportunities header into a separate component.

---
+++ apps/marketplace/components/Opportunities/OpportunitiesHeader.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import AUheading from '@gov.au/headings/lib/js/react.js'
+import { OpportunitiesFilters } from 'marketplace/components/Opportunities/OpportunitiesFilters'
+import styles from './Opportunities.scss'
+
+export const OpportunitiesHeader = props => (
+  <div className={`opportunities-page ${styles.container}`}>
+    <div className={`${styles.header} row`}>
+      <div className={`${styles.headerTitle} col-md-4 col-sm-4 col-xs-4`}>
+        <AUheading size="xl" level="1">
+          Opportunities
+        </AUheading>
+      </div>
+      <OpportunitiesFilters
+        accordionOpen={props.accordionOpen}
+        openAccordion={props.openAccordion}
+        closeAccordion={props.closeAccordion}
+        applyFilters={props.applyFilters}
+        getActiveFilterCount={props.getActiveFilterCount}
+      />
+    </div>
+  </div>
+)
+
+OpportunitiesHeader.propTypes = {
+  accordionOpen: PropTypes.bool.isRequired,
+  openAccordion: PropTypes.func.isRequired,
+  closeAccordion: PropTypes.func.isRequired,
+  applyFilters: PropTypes.func.isRequired,
+  getActiveFilterCount: PropTypes.func.isRequired
+}
+
+export default OpportunitiesHeader
--- apps/marketplace/pages/OpportunitiesPage.js
+++ apps/marketplace/pages/OpportunitiesPage.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import AUheading from '@gov.au/headings/lib/js/react.js'
 import { loadOpportunities } from 'marketplace/actions/opportunitiesActions'
 import LoadingIndicatorFullPage from 'shared/LoadingIndicatorFullPage/LoadingIndicatorFullPage'
 import Opportunities from 'marketplace/components/Opportunities/Opportunities'
-import { OpportunitiesFilters } from 'marketplace/components/Opportunities/OpportunitiesFilters'
-import styles from 'marketplace/components/Opportunities/Opportunities.scss'
+import { OpportunitiesHeader } from 'marketplace/components/Opportunities/OpportunitiesHeader'
 
 class OpportunitiesPage extends Component {
   constructor(props) {
@@ -69,31 +67,16 @@ class OpportunitiesPage extends Component {
   render() {
     const { currentlySending } = this.props
 
-    const opportunitiesHeader = (
-      <div className={`opportunities-page ${styles.container}`}>
-        <div className={`${styles.header} row`}>
-          <div className={`${styles.headerTitle} col-md-4 col-sm-4 col-xs-4`}>
-            <AUheading size="xl" level="1">
-              Opportunities
-            </AUheading>
-          </div>
-          <OpportunitiesFilters
-            accordionOpen={this.state.accordionOpen}
-            openAccordion={this.openAccordion}
-            closeAccordion={this.closeAccordion}
-            applyFilters={this.applyFilters}
-            getActiveFilterCount={this.getActiveFilterCount}
-          />
-        </div>
-      </div>
-    )
-
-    const opportunitiesBody = <Opportunities {...this.props} />
-
     return (
       <div>
-        {opportunitiesHeader}
-        {currentlySending ? <LoadingIndicatorFullPage /> : opportunitiesBody}
+        <OpportunitiesHeader
+          accordionOpen={this.state.accordionOpen}
+          openAccordion={this.openAccordion}
+          closeAccordion={this.closeAccordion}
+          applyFilters={this.applyFilters}
+          getActiveFilterCount={this.getActiveFilterCount}
+        />
+        {currentlySending ? <LoadingIndicatorFullPage /> : <Opportunities {...this.props} />}
       </div>
     )
   }
