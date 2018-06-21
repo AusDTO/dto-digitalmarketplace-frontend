0b11423523c8143d48eca014b7ce5d3072bc86c7
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2018-06-21 11:21:56 +1000

    [Footer] Prettier formatting

--- apps/marketplace/components/Footer/AUFooter.js
+++ apps/marketplace/components/Footer/AUFooter.js
@@ -34,20 +34,27 @@ const AUFooter = () => (
           <div className="au-footer__end">
             <div className="footer__content footer__legal">
               <p>
-                An initiative of the <a href="https://www.dta.gov.au/">Digital Transformation Agency</a>.{' '}
-                This program forms part of the National Innovation and Science <a href="https://innovation.gov.au/page/agenda">Agenda</a>.{' '}
-                With the exception of the Commonwealth Coat of Arms and where otherwise noted,{' '}
-                this work is licensed under the <a href="https://github.com/govau/uikit/blob/master/LICENSE" rel="external">MIT license</a>
+                An initiative of the <a href="https://www.dta.gov.au/">Digital Transformation Agency</a>. This program
+                forms part of the National Innovation and Science{' '}
+                <a href="https://innovation.gov.au/page/agenda">Agenda</a>. With the exception of the Commonwealth Coat
+                of Arms and where otherwise noted, this work is licensed under the{' '}
+                <a href="https://github.com/govau/uikit/blob/master/LICENSE" rel="external">
+                  MIT license
+                </a>
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 col-md-pull-9 footer__logo">
           <div className="au-footer-logo-dta">
-            <a href="https://www.dta.gov.au/"><div dangerouslySetInnerHTML={{ __html: logoDTA }} /></a>
+            <a href="https://www.dta.gov.au/">
+              <div dangerouslySetInnerHTML={{ __html: logoDTA }} />
+            </a>
           </div>
           <div className="au-footer-logo-nisa">
-            <a href="https://innovation.gov.au/page/agenda"><div dangerouslySetInnerHTML={{ __html: logoNISA }} /></a>
+            <a href="https://innovation.gov.au/page/agenda">
+              <div dangerouslySetInnerHTML={{ __html: logoNISA }} />
+            </a>
           </div>
         </div>
       </div>
