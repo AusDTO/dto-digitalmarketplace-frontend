563a563409c8a255b45850a8df28323b24ea6ef0
malmas18 <29763502+malmas18@users.noreply.github.com>
2018-06-28 10:58:12 +1000

    add training automation

---
+++ fixtures/tests/brief/training.js
@@ -0,0 +1,26 @@
+import { login, signOut } from '../../flows/login/buyer';
+import { create } from '../../flows/brief/training';
+import { startBrief } from '../../flows/dashboard/buyer';
+
+describe('should be able to create training brief', () => {
+    beforeEach(async () => {
+        await login();
+    });
+    afterEach(async () => {
+        await signOut();
+    });
+    it('should be able to create training brief', async () => {
+        let now = Date.now();
+        await startBrief();
+        await create({
+            title: `Digital Training ${now.valueOf()}`,
+            locations: ['Australian Capital Territory', 'Tasmania'],
+            evaluations: [
+                'Interview',
+                'References',
+                'Case study',
+                'Presentation'
+            ]
+        });
+    });
+});
