4c5f4ca312d5b4dc3bd778514289237db7bdcba9
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2018-07-05 11:15:21 +1000

    [Seller Registration] Updated syles and layout for Documents component

--- src/bundles/SellerRegistration/components/DocumentsForm/DocumentsForm.js
+++ src/bundles/SellerRegistration/components/DocumentsForm/DocumentsForm.js
@@ -23,7 +23,8 @@ import { minObjectLength, validDate } from '../../../../validators';
 import PageAlert from '@gov.au/page-alerts'
 import isPast from 'date-fns/is_past';
 
-import './DocumentsForm.css';
+import styles from '../SellerRegistration.css';
+import documents from './DocumentsForm.css';
 
 
 class DocumentsForm extends BaseForm {
@@ -137,15 +138,15 @@ class DocumentsForm extends BaseForm {
                   { this.props.expiredLiabilityInsurance || this.props.expiredWorkersCompensation ?
                     <PageAlert as="error"><p><strong>Not all your documents are up to date. Please upload the necessary documents to continue.</strong></p></PageAlert>
                   : '' }
-                  <h1 tabIndex="-1">Upload your documents</h1>
+                  <h1 styleName="styles.content-heading" tabIndex="-1">Upload your documents</h1>
 
                   <p>Your insurance documents will appear on your seller profile and your financial statement may be shared with buyers on request. So make sure they are up to date.</p>
                   <p>  Each should be no larger than 5MB and in PDF, PNG or JPEG format. If you have multiple files for a document, please scan and merge as one upload.
                   </p>
+                  <br/>
                   <div className="calloutMistake">
                     <b> Avoid common mistakes </b>
-                    <ul>
-
+                    <ul className="mistake-list">
                       <li><b>Financial statement</b> - ensure it is up to date. A letter from your accountant confirming financial viability is acceptable. We will not accept an internal letter as proof of financial viability.</li>
                       <li><b>Professional Indemnity and Public Liability Insurance</b> - check expiration dates match the uploaded documentation.</li>
                       <li><b>Workers Compensation</b> - check expiration dates match the uploaded documentation.</li>
@@ -190,17 +191,17 @@ class DocumentsForm extends BaseForm {
 
                             return (
                                 <div key={key} className="callout-no-margin">
-                                    <p styleName="question-heading">{field.label}</p>
+                                    <p styleName="documents.question-heading">{field.label}</p>
                                     <span>{field.description}</span>
 
                                     <div>
                                         {errors && <span className="validation-message">There was an error uploading the file</span>}
 
                                         {isEmpty(doc.filename) && !fieldState.uploading && !fieldState.file &&
-                                            <div id={expiry_date_field}>
+                                            <div id={expiry_date_field} styleName="documents.file-upload">
                                                 <p>
-                                                    <input type="file" id={key} name={key} styleName="hidden-input" accept=".pdf,.jpg,.png"  onChange={this.onChange.bind(this, key)} />
-                                                    <label id={`label_${key}`} htmlFor={key} styleName="custom-input"> {isEmpty(name) && "Choose file" } </label>
+                                                    <input type="file" id={key} name={key} styleName="documents.hidden-input" accept=".pdf,.jpg,.png"  onChange={this.onChange.bind(this, key)} />
+                                                    <label id={`label_${key}`} htmlFor={key} styleName="documents.custom-input"> {isEmpty(name) && "Choose file" } </label>
                                                 </p>
                                             </div>
                                         }
@@ -209,10 +210,10 @@ class DocumentsForm extends BaseForm {
                                             <div>
                                                 <ul className="bordered-list">
                                                     <li className="bordered-list__item row">
-                                                        <div className="col-xs-9" styleName="overflow-hidden">
+                                                        <div className="col-xs-9" styleName="documents.overflow-hidden">
                                                             <a href={url} target="_blank" rel="external">{doc.filename}</a>
                                                         </div>
-                                                        <div className="col-xs-3" styleName="text-right">
+                                                        <div className="col-xs-3" styleName="documents.text-right">
                                                             <a href="3" onClick={this.onReset.bind(this, key)}>Delete</a>
                                                         </div>
                                                     </li>
@@ -252,11 +253,11 @@ class DocumentsForm extends BaseForm {
                                     {fieldState.uploading && <p>Uploading...</p>}
 
                                     {fieldState.file &&
-                                        <div styleName="upload-container">
-                                            <p styleName="filler">&nbsp;</p>
-                                            <p id={`span_${key}`} ref={`span_${key}`}>{!isEmpty(name) && name }</p>
-                                            <button type="submit" styleName="sumbit-container" onClick={this.onUpload.bind(this, key)}>Upload</button>
-                                            <p styleName="custom-input">Choose file</p>
+                                        <div styleName="documents.upload-container">
+                                            <p styleName="documents.filler">&nbsp;</p>
+                                            <p id={`span_${key}`} ref={`span_${key}`} styleName="documents.file-name">{!isEmpty(name) && name }</p>
+                                            <button type="submit" styleName="documents.submit-container" onClick={this.onUpload.bind(this, key)}>Upload</button>
+                                            <p styleName="documents.custom-input">Choose file</p>
                                         </div>
                                     }
                                 </div>
