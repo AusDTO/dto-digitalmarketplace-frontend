9c6ce79c9c35b37d636cf0125ca3804c185e2137
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2018-07-16 14:13:22 +1000

    [Onboarding] Added Design System class to H1 tag

--- apps/marketplace/components/Onboarding/SellerOnboarding.js
+++ apps/marketplace/components/Onboarding/SellerOnboarding.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SellerOnboarding = () => (
   <div className="row">
     <div className="col-sm-push-2 col-sm-8 col-xs-12">
-      <h1 title="Welcome to the Marketplace">Welcome to the Marketplace</h1>
+      <h1 className="au-display-xl" title="Welcome to the Marketplace">Welcome to the Marketplace</h1>
       <p>What do you want to do next?</p>
       <h2 className="au-display-lg">List my digital offering on the Marketplace</h2>
       <span>
