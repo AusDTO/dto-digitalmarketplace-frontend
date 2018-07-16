4a03bcb6763e780c09f97413f78f17a70fd9e9bf
Nathan Davison <nathan.davison@digital.gov.au>
2018-07-16 12:00:27 +1000

    Update the Create a new brief page to the new design.

--- apps/marketplace/components/BriefChoice/BriefChoice.js
+++ apps/marketplace/components/BriefChoice/BriefChoice.js
@@ -1,79 +1,104 @@
-import React, { Component } from 'react'
-import { AUradio } from '@gov.au/control-input/lib/js/react.js'
-import { rootPath } from 'marketplace/routes'
+import React from 'react'
+import AUheading from '@gov.au/headings/lib/js/react.js'
 import styles from './BriefChoice.scss'
 
-export class BriefChoice extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      briefType: ''
-    }
-  }
-
-  handleChange(type) {
-    this.setState({
-      briefType: type
-    })
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className="row">
-          <div className="col-xs-12">
-            <h1 className="au-display-xl">Start a New Brief</h1>
-            <p>Create a new opportunity for a:</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className={`${styles.choice} col-xs-12`}>
-            <AUradio
-              id="choice-digital-specialist"
-              label="Digital Specialist"
-              name="brief_choice"
-              value="digital-professionals"
-              onChange={() => {
-                this.handleChange('digital-professionals')
-              }}
-            />
-            <p>Get seller responses to your criteria plus candidate resumes for shortlisting.</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className={`${styles.choice} col-xs-12`}>
-            <AUradio
-              id="choice-digital-outcome"
-              label="Digital Outcome"
-              name="brief_choice"
-              value="digital-outcome"
-              onChange={() => {
-                this.handleChange('digital-outcome')
-              }}
-            />
-            <p>
-              Get a summary of seller responses to your criteria, in spreadsheet format, for shortlisting. Once your
-              shortlist is complete you can request sellers to provide full proposals for you to evaluate.
-            </p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <a href={`/buyers/frameworks/digital-marketplace/requirements/${this.state.briefType}`}>
-              <button className="au-btn" disabled={!this.state.briefType && 'disabled'}>
-                Continue
-              </button>
-            </a>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <a href={`${rootPath}/buyer-dashboard`}>Return to dashboard</a>
-          </div>
-        </div>
+const BriefChoice = () => (
+  <div className={styles.container}>
+    <div className={`row ${styles.headingRow}`}>
+      <div className="col-xs-12">
+        <AUheading level="1" size="xl">
+          Create a new brief
+        </AUheading>
       </div>
-    )
-  }
-}
+    </div>
+    <div className="row">
+      <div className="col-xs-12 col-md-3">
+        <span />
+        <AUheading level="2" size="md">
+          Digital Outcome
+        </AUheading>
+        <p>Acquire a team to help you work towards a digital project or goal. For example, a website.</p>
+        <ul className={styles.tickList}>
+          <li>Receive written proposals from sellers.</li>
+          <li>Request case studies, work history, references and/or presentations.</li>
+        </ul>
+        <p>
+          <strong>Receive responses within:</strong>
+          <br />1 - 2 weeks
+        </p>
+        <p className={styles.hideDesktop}>
+          <a href="/buyers/frameworks/digital-marketplace/requirements/digital-outcome">
+            <button className="au-btn">Get started</button>
+          </a>
+        </p>
+      </div>
+      <div className="col-xs-12 col-md-1">
+        <div className={styles.separator} />
+      </div>
+      <div className="col-xs-12 col-md-3">
+        <span />
+        <AUheading level="2" size="md">
+          Training
+        </AUheading>
+        <p>Build and embed skills to support digital services. For example, coaching in agile practices.</p>
+        <ul className={styles.tickList}>
+          <li>Receive written proposals from sellers.</li>
+          <li>Request trainer résumés, case studies, references and/or presentations.</li>
+        </ul>
+        <p>
+          <strong>Receive responses within:</strong>
+          <br />1 - 2 weeks
+        </p>
+        <p className={styles.hideDesktop}>
+          <a href="/buyers/frameworks/digital-marketplace/requirements/training">
+            <button className="au-btn">Get started</button>
+          </a>
+        </p>
+      </div>
+      <div className="col-xs-12 col-md-1">
+        <div className={styles.separator} />
+      </div>
+      <div className="col-xs-12 col-md-3">
+        <span />
+        <AUheading level="2" size="md">
+          Digital Specialist
+        </AUheading>
+        <p>Find a person with specialised digital skills. For example, a developer.</p>
+        <ul className={styles.tickList}>
+          <li>Receive seller résumés.</li>
+          <li>Request references, interviews, scenarios and/or presentations.</li>
+        </ul>
+        <p>
+          <strong>Receive responses within:</strong>
+          <br />1 - 2 weeks
+        </p>
+        <p className={styles.hideDesktop}>
+          <a href="/buyers/frameworks/digital-marketplace/requirements/digital-professionals">
+            <button className="au-btn">Get started</button>
+          </a>
+        </p>
+      </div>
+    </div>
+    <div className={`row ${styles.hideMobile}`}>
+      <div className="col-md-3">
+        <a href="/buyers/frameworks/digital-marketplace/requirements/digital-outcome">
+          <button className="au-btn">Get started</button>
+        </a>
+      </div>
+      <div className="col-md-1" />
+      <div className="col-md-3">
+        <a href="/buyers/frameworks/digital-marketplace/requirements/training">
+          <button className="au-btn">Get started</button>
+        </a>
+      </div>
+      <div className="col-md-1" />
+      <div className="col-md-3">
+        <a href="/buyers/frameworks/digital-marketplace/requirements/digital-professionals">
+          <button className="au-btn">Get started</button>
+        </a>
+      </div>
+    </div>
+  </div>
+)
 
 export default BriefChoice
