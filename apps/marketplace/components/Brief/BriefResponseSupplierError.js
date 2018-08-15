972e4b2b0978a7069aa3406437ac4840122f3e53
malmas18 <29763502+malmas18@users.noreply.github.com>
2018-08-15 16:17:03 +1000

    update supplier error page to new designs

--- apps/marketplace/components/Brief/BriefResponseSupplierError.js
+++ apps/marketplace/components/Brief/BriefResponseSupplierError.js
@@ -5,27 +5,31 @@ import AUpageAlert from '@gov.au/page-alerts/lib/js/react.js'
 
 const BriefResponseSupplierError = props => (
   <div className="row">
-    <DocumentTitle title="Seller Profile Error - Digital Marketplace">
+    <DocumentTitle title="Before you can apply - Digital Marketplace">
       <div className="col-sm-push-2 col-sm-8 col-xs-12">
         <h2 class="au-header__heading">
-          Seller profile error
+          Before you can apply
         </h2>
+        <br />
         <article role="main">
-          <p>Please fix the following problems with your profile before responding to a brief</p>
-          <AUpageAlert as="error"
-                       setFocus={props.setFocus}>
-            <h4 className="au-display-sm">
-              The following problems were detect with your seller profile
-            </h4>
-            <ul>
-              {Array.isArray(props.app.errorMessage) && props.app.errorMessage.map((err, i) => (
-                <li key={`errorMessage${i}`}>{err.message}</li>)
-              )}
-          </ul>
-          </AUpageAlert>
-          <a link="/sellers/edit" class="au-btn">
-            Update your profile
-          </a>
+          <p>
+            <AUpageAlert as="error"
+                        setFocus={props.setFocus}>
+              <h4 className="au-display-sm">
+                Please update your profile with:
+              </h4>
+              <ul>
+                {Array.isArray(props.app.errorMessage) && props.app.errorMessage.map((err, i) => (
+                  <li key={`errorMessage${i}`}>{err.message}</li>)
+                )}
+              </ul>
+            </AUpageAlert>
+          </p>
+          <p>
+            <a link="/sellers/edit" class="au-btn">
+              Update your profile
+            </a>
+          </p>
         </article>
       </div>
     </DocumentTitle>
