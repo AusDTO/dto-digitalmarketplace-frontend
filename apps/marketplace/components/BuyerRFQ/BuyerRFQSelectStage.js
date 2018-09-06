c6224bd39c921bff80760dc89e12e1367eee7198
Nathan Davison <nathan.davison@digital.gov.au>
2018-09-06 15:41:32 +1000

    Initial version of the Buyer RFQ sellect seller stage.

--- apps/marketplace/components/BuyerRFQ/BuyerRFQSelectStage.js
+++ apps/marketplace/components/BuyerRFQ/BuyerRFQSelectStage.js
@@ -1,5 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
+import AUheading from '@gov.au/headings/lib/js/react.js'
+import SellerSelect from 'marketplace/components/SellerSelect/SellerSelect'
+import styles from './BuyerRFQSelectStage.scss'
 
-const BuyerRFQSelectStage = () => <div>Select stage</div>
+export class BuyerRFQSelectStage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sellers: {}
+    }
+
+    this.handleSellerSelect = this.handleSellerSelect.bind(this)
+  }
+
+  handleSellerSelect(seller) {
+    this.setState(curState => {
+      const newState = { ...curState }
+      newState.sellers[seller.code] = seller
+      return newState
+    })
+  }
+
+  removeSeller(sellerCode) {
+    this.setState(curState => {
+      const newState = { ...curState }
+      delete newState.sellers[sellerCode]
+      return newState
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <AUheading level="1" size="xl">
+          Select sellers
+        </AUheading>
+        {Object.keys(this.state.sellers).length > 0 && (
+          <ol className={styles.selectedSellers}>
+            {Object.keys(this.state.sellers).map(sellerCode => (
+              <li key={sellerCode}>
+                {this.state.sellers[sellerCode].name}
+                <a
+                  href="#remove"
+                  onClick={e => {
+                    e.preventDefault()
+                    this.removeSeller(sellerCode)
+                  }}
+                >
+                  Remove
+                </a>
+              </li>
+            ))}
+          </ol>
+        )}
+        <SellerSelect showSelected={false} showSearchButton={false} onSellerSelect={this.handleSellerSelect} />
+      </div>
+    )
+  }
+}
 
 export default BuyerRFQSelectStage
