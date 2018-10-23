2226e8f7c48eec0ee7b6728cb2ece5d5b76bd6d2
Nathan Davison <nathan.davison@digital.gov.au>
2018-10-23 11:07:46 +1100

    Flesh out the status logic for the RFX overview steps.

--- apps/marketplace/components/BuyerRFQ/BuyerRFQOverview.js
+++ apps/marketplace/components/BuyerRFQ/BuyerRFQOverview.js
@@ -1,51 +1,127 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import isValid from 'date-fns/is_valid'
 import isFuture from 'date-fns/is_future'
+import subDays from 'date-fns/sub_days'
 import { rootPath } from 'marketplace/routes'
 import styles from './BuyerRFQOverview.scss'
+
+const Badge = props => <span className={styles.badge}>{props.status}</span>
+
+const answerSellerQuestionsRender = (brief, isPublished, isPastQuestionsDeadline) => {
+  if (!isPublished) {
+    return (
+      <span>
+        <span>2. Answer seller questions</span>
+      </span>
+    )
+  } else if (isPublished && !isPastQuestionsDeadline) {
+    return (
+      <span>
+        <a
+          href={`/buyers/frameworks/digital-marketplace/requirements/rfx/${
+            brief.id
+          }/supplier-questions/answer-question`}
+        >
+          2. Answer seller questions
+        </a>
+        <Badge status="To do" />
+      </span>
+    )
+  } else if (isPublished && isPastQuestionsDeadline) {
+    return (
+      <span>
+        <span>2. Answer seller questions</span>
+        <Badge status="Done" />
+      </span>
+    )
+  }
+  return null
+}
+
+const downloadResponsesRender = (brief, isPublished, isClosed) => {
+  if (isPublished && isClosed) {
+    return (
+      <span>
+        <a href={`${rootPath}/brief/${brief.id}/download-responses`}>3. Download responses</a>
+        <Badge status="Ready for download" />
+      </span>
+    )
+  }
+
+  return (
+    <span>
+      <span>3. Download responses</span>
+    </span>
+  )
+}
 
 const BuyerRFQOverview = props => {
   const { brief } = props
 
   if (brief && brief.id && brief.dates) {
-    const isPublished = !!brief.dates.published_date
-    const isOpenToQuestions = brief.dates.questions_close && isFuture(new Date(brief.dates.questions_close))
-    const isClosed = brief.dates.closing_time && !isFuture(new Date(brief.dates.closing_time))
+    const isPublished = brief.dates.published_date && isValid(new Date(brief.dates.published_date))
+    const isPastQuestionsDeadline =
+      brief.dates.closing_time &&
+      isValid(new Date(brief.dates.closing_time)) &&
+      new Date().toLocaleString('en-US', { timeZone: 'UTC' }) >
+        subDays(new Date(brief.dates.closing_time), 1).toLocaleString('en-US', { timeZone: 'UTC' })
+    const isClosed =
+      brief.dates.closing_time &&
+      isValid(new Date(brief.dates.closing_time)) &&
+      !isFuture(new Date(brief.dates.closing_time))
 
     return (
       <div>
         <ul className={styles.overviewList}>
           <li>
             {isPublished ? (
-              <span>1. Send request to sellers</span>
+              <span>
+                <span>1. Send request to sellers</span>
+                <Badge status="Done" />
+              </span>
             ) : (
-              <a href={`${rootPath}/buyer-rfq/${brief.id}/introduction`}>1. Send request to sellers</a>
+              <span>
+                <a href={`${rootPath}/buyer-rfq/${brief.id}/introduction`}>1. Send request to sellers</a>
+                <Badge status="To do" />
+              </span>
             )}
           </li>
+          <li>{answerSellerQuestionsRender(brief, isPublished, isPastQuestionsDeadline)}</li>
+          <li>{downloadResponsesRender(brief, isPublished, isClosed)}</li>
           <li>
-            {isPublished && isOpenToQuestions ? (
-              <a
-                href={`/buyers/frameworks/digital-marketplace/requirements/rfx/${
-                  brief.id
-                }/supplier-questions/answer-question`}
-              >
-                2. Answer seller questions
-              </a>
+            {isPublished && isClosed ? (
+              <span>
+                <span>4. Evaluate responses</span>
+                <Badge status="To do" />
+              </span>
             ) : (
-              <span>2. Answer seller questions</span>
+              <span>
+                <span>4. Evaluate responses</span>
+              </span>
             )}
           </li>
           <li>
             {isPublished && isClosed ? (
-              <a href={`${rootPath}/brief/${brief.id}/download-responses`}>3. Download responses</a>
+              <span>
+                <span>5. Award a contract</span>
+                <Badge status="To do" />
+              </span>
             ) : (
-              <span>3. Download responses</span>
+              <span>5. Award a contract</span>
             )}
           </li>
-          <li>4. Evaluate responses</li>
-          <li>5. Award a contract</li>
-          <li>6. Debrief sellers</li>
+          <li>
+            {isPublished && isClosed ? (
+              <span>
+                <span>6. Debrief sellers</span>
+                <Badge status="To do" />
+              </span>
+            ) : (
+              <span>6. Debrief sellers</span>
+            )}
+          </li>
         </ul>
       </div>
     )
