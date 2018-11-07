06f294cf93c5bc6c282185b343d56e9a5a957a53
Nathan Davison <nathan.davison@digital.gov.au>
2018-11-08 10:25:48 +1100

    Update outcome choice UI.

--- apps/marketplace/components/BriefOutcomeChoice/BriefOutcomeChoice.js
+++ apps/marketplace/components/BriefOutcomeChoice/BriefOutcomeChoice.js
@@ -14,10 +14,10 @@ const BriefOutcomeChoice = () => (
     <div className={`row ${styles.container}`}>
       <div className="col-xs-12 col-md-5">
         <div className={styles.card}>
-          <AUheading level="2" size="md">
+          <AUheading level="2" size="lg">
             Ask the market
           </AUheading>
-          <p>
+          <p className={styles.largerText}>
             EOI/RFI style suited to exploratory procurements. Uncover new expertise or trial products and emerging
             technologies.
           </p>
@@ -30,22 +30,24 @@ const BriefOutcomeChoice = () => (
           <AUheading level="3" size="sm">
             Get responses within
           </AUheading>
-          <ul className={styles.flexGrow}>
+          <ul className={styles.flexList}>
             <li>Choose 5 to 10 business days</li>
           </ul>
           <p className={styles.buttons}>
             <a href="/buyers/frameworks/digital-marketplace/requirements/digital-outcome" className="au-btn">
-              Start
+              Get started
             </a>
           </p>
         </div>
       </div>
       <div className="col-xs-12 col-md-5 col-md-offset-1">
         <div className={styles.card}>
-          <AUheading level="2" size="md">
+          <AUheading level="2" size="lg">
             Get proposals/quotes
           </AUheading>
-          <p>Quickly seek quotes based on researched user needs and prioritised technical requirements.</p>
+          <p className={styles.largerText}>
+            Quickly seek quotes based on researched user needs and prioritised technical requirements.
+          </p>
           <AUheading level="3" size="sm">
             What you get
           </AUheading>
@@ -54,14 +56,14 @@ const BriefOutcomeChoice = () => (
             <li>Pre-populated evaluation spreadsheet.</li>
           </ul>
           <AUheading level="3" size="sm">
-            Get responses within
+            Get response format within
           </AUheading>
-          <ul className={styles.flexGrow}>
+          <ul className={styles.flexList}>
             <li>Set your own timeframes (2 days minimum)</li>
           </ul>
           <p className={styles.buttons}>
             <a href={`${rootPath}/buyer-rfq/create`} className="au-btn">
-              Start
+              Get started
             </a>
           </p>
         </div>
