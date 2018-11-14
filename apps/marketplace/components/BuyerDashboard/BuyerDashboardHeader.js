c94ae948a0aa02a79ffbec6ee6490c8305ba21b7
Nathan Davison <nathan.davison@digital.gov.au>
2018-11-14 15:14:56 +1100

    Update the buyer dashboard to latest UI.

--- apps/marketplace/components/BuyerDashboard/BuyerDashboardHeader.js
+++ apps/marketplace/components/BuyerDashboard/BuyerDashboardHeader.js
@@ -1,45 +1,83 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withRouter, NavLink } from 'react-router-dom'
+import AUaccordion from '@gov.au/accordion/lib/js/react.js'
 import { rootPath } from 'marketplace/routes'
 import styles from './BuyerDashboard.scss'
 
-export const BuyerDashboardHeader = props => (
-  <div className={`${styles.header} row`}>
-    <div className="col-sm-12">
-      <small className={styles.organisation}>{props.organisation}</small>
-      <div className="row">
-        <div className="col-xs-12">
-          <h1 className="au-display-xl">Dashboard</h1>
+class BuyerDashboardHeader extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      createNewIsOpen: false
+    }
+
+    this.handleCreateNewToggle = this.handleCreateNewToggle.bind(this)
+  }
+
+  handleCreateNewToggle() {
+    this.setState(curState => {
+      const newState = { ...curState }
+      newState.createNewIsOpen = !curState.createNewIsOpen
+      return newState
+    })
+  }
+
+  render() {
+    return (
+      <div className={`${styles.header} row`}>
+        <div className="col-sm-12">
+          <small className={styles.organisation}>{this.props.organisation}</small>
+          <div className="row">
+            <div className="col-xs-12">
+              <h1 className="au-display-xl">Dashboard</h1>
+            </div>
+          </div>
+          <div className={`${styles.menuRow} row`}>
+            <div className="col-xs-12 col-sm-8 col-md-9">
+              <ul className={styles.menu}>
+                <li>
+                  <NavLink id="my-briefs-link" to="/" activeClassName={styles.active} exact>
+                    My briefs
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink id="team-briefs-link" to="/team-briefs" activeClassName={styles.active}>
+                    Team briefs
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink id="team-overview-link" to="/team-overview" activeClassName={styles.active}>
+                    Team overview
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className={`${styles.alignRight} col-xs-12 col-sm-4 col-md-3 buyer-dashboard-actions`}>
+              <AUaccordion
+                header="Create new request"
+                open={this.state.createNewIsOpen}
+                onOpen={this.handleCreateNewToggle}
+                onClose={this.handleCreateNewToggle}
+                className={styles.createNew}
+              >
+                <ul>
+                  <li>
+                    <a href="/buyers/frameworks/digital-marketplace/requirements/digital-professionals">Specialist</a>
+                  </li>
+                  <li>
+                    <a href={`${rootPath}/outcome-choice`}>Outcome</a>
+                  </li>
+                  <li>
+                    <a href="/buyers/frameworks/digital-marketplace/requirements/training">Training</a>
+                  </li>
+                </ul>
+              </AUaccordion>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={`${styles.menuRow} row`}>
-        <div className="col-xs-12 col-sm-8 col-md-9">
-          <ul className={styles.menu}>
-            <li>
-              <NavLink id="my-briefs-link" to="/" activeClassName={styles.active} exact>
-                My briefs
-              </NavLink>
-            </li>
-            <li>
-              <NavLink id="team-briefs-link" to="/team-briefs" activeClassName={styles.active}>
-                Team briefs
-              </NavLink>
-            </li>
-            <li>
-              <NavLink id="team-overview-link" to="/team-overview" activeClassName={styles.active}>
-                Team overview
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className={`${styles.alignRight} col-xs-12 col-sm-4 col-md-3`}>
-          <a className={`${styles.firstButton} au-btn`} href={`${rootPath}/create-brief`}>
-            Start a new brief
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default withRouter(BuyerDashboardHeader)
