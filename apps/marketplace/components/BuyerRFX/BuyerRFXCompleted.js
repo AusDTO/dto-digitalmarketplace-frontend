915df6f11c539e22839d49ca0120b4f69ac2be42
Nathan Davison <nathan.davison@digital.gov.au>
2018-11-30 15:34:17 +1100

    Change answer seller questions link on RFX complete.

--- apps/marketplace/components/BuyerRFX/BuyerRFXCompleted.js
+++ apps/marketplace/components/BuyerRFX/BuyerRFXCompleted.js
@@ -20,11 +20,7 @@ const BuyerRFXCompleted = props => (
     <ul>
       <li>
         While your opportunity is live, you&apos;ll need to{' '}
-        <a
-          href={`/buyers/frameworks/digital-marketplace/requirements/rfx/${
-            props.briefId
-          }/supplier-questions/answer-question`}
-        >
+        <a href="https://marketplace1.zendesk.com/hc/en-gb/articles/115011257447-Answer-questions" rel="external">
           answer seller questions
         </a>.
       </li>
