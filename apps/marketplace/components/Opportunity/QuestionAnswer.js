7c8bc50234b9997e2937704d3bf4ee32fdd707e3
Nathan Davison <nathan.davison@digital.gov.au>
2018-12-06 15:31:19 +1100

    Adjust QA section in published RFX.

--- apps/marketplace/components/Opportunity/QuestionAnswer.js
+++ apps/marketplace/components/Opportunity/QuestionAnswer.js
@@ -7,17 +7,21 @@ import styles from './QuestionAnswer.scss'
 
 const QuestionAnswer = props => (
   <div className={styles.container}>
-    <div className="row">
-      <div className="col-xs-12">
-        <AUheading level="2" size="lg">
-          Question and answer
-        </AUheading>
+    <AUheading level="2" size="lg">
+      Question and answer
+    </AUheading>
+    <div className={`${styles.deadline} row`}>
+      <div className="col-xs-12 col-sm-5">
+        <strong>Deadline for asking questions</strong>
+      </div>
+      <div className="col-xs-12 col-sm-7">
+        {format(parse(props.questionsClosingDate), 'dddd D MMMM YYYY')} at 6PM (in Canberra)
       </div>
     </div>
     {props.questions.map(qa => (
       <div className="row" key={qa.question}>
-        <div className="col-xs-12 col-sm-4">{qa.question}</div>
-        <div className="col-xs-12 col-sm-8">{qa.answer}</div>
+        <div className="col-xs-12 col-sm-5">{qa.question}</div>
+        <div className="col-xs-12 col-sm-7">{qa.answer}</div>
       </div>
     ))}
     {props.questions.length === 0 && <p>No questions have been asked or answered yet.</p>}
@@ -25,13 +29,6 @@ const QuestionAnswer = props => (
       !props.clarificationQuestionsAreClosed && (
         <p>
           <a href={`/sellers/opportunities/${props.briefId}/ask-a-question`}>Ask a question</a>
-        </p>
-      )}
-    {props.showAskQuestionInfo &&
-      props.clarificationQuestionsAreClosed && (
-        <p>
-          The deadline for asking questions about this opportunity was{' '}
-          {format(parse(props.questionsClosingDate), 'dddd D MMMM YYYY')}.
         </p>
       )}
   </div>
