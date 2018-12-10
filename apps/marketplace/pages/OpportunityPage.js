80e3108481e35331fc13e27a6916facc17e583bc
malmas18 <29763502+malmas18@users.noreply.github.com>
2018-12-10 15:09:59 +1100

    formatting

--- apps/marketplace/pages/OpportunityPage.js
+++ apps/marketplace/pages/OpportunityPage.js
@@ -57,7 +57,11 @@ class OpportunityPage extends Component {
     }
 
     // only RFX can be displayed using this opportunity view
-    if (this.props.brief && this.props.brief.lot && (this.props.brief.lot !== 'rfx' && this.props.brief.lot !== 'atm')) {
+    if (
+      this.props.brief &&
+      this.props.brief.lot &&
+      (this.props.brief.lot !== 'rfx' && this.props.brief.lot !== 'atm')
+    ) {
       window.location = `/digital-marketplace/opportunities/${this.props.brief.id}`
       return null
     }
