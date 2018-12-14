580874937ca9c224453d26bec7d2176ba6c70474
Sam <29763502+malmas18@users.noreply.github.com>
2018-12-14 11:59:26 +1100

    start adding search

--- src/bundles/CaseStudy/components/AssessmentList/AssessmentList.js
+++ src/bundles/CaseStudy/components/AssessmentList/AssessmentList.js
@@ -35,62 +35,67 @@ class AssessmentList extends React.Component {
     } = this.props
 
     return (
+      <div>
+        <input id="keyword" type="text" size="30" placeholder="id or name"/>
+        <hr/>
+      { 
         casestudies.map((cs, i) =>
-          <div>
-            <div className="row">
-              <div className="col-md-2"><b>supplier</b></div>
-              <div className="col-md-4"><b>title</b></div>
-              <div className="col-md-2"><b>domain</b></div>
-              <div className="col-md-2"><b>assessments</b></div>
-              <div className="col-md-2"><b>case study status</b></div>
-            </div>
-            <span key={cs.id}>
-              <div className="row">
-                <div className="col-md-2">{cs.name}(#{cs.supplier_code})</div>
-                <div className="col-md-4"><a href={`casestudy-assessment/${cs.id}`}>{cs.data.title}(#{cs.id})</a></div>
-                <div className="col-md-2">{cs.data.service}</div>
-                <div className="col-md-2">{cs.assessment_count}</div>
-                <div className="col-md-2">
-                  {cs.status}
-                  <br/>
-                  <a href="#" onClick={e => {
-                    e.preventDefault();
-                    onApproveClick(cs.id);
-                  }} name="approve">approve</a>
-                  {' '}
-                  <a href="#" onClick={e => {
-                    e.preventDefault();
-                    onRejectClick(cs.id);
-                  }} name="reject">reject</a>
-                </div>
-              </div>
-              <div className="row">
-                <div>
-                  <div className="col-md-2"><b>assessors</b></div>
-                  <div className="col-md-4"><b>comment</b></div>
-                  <div className="col-md-4"><b>criterias met</b></div>
-                  <div className="col-md-2"><b>result</b></div>
-                </div>
-              </div>
-              {cs.assessment_count > 0 ? cs.assessment_results.map(ar =>
-                <div key={ar.id} className="row">
-                  <div className="col-md-2">{ar.username}</div>
-                  <div className="col-md-4">{ar.comment}</div>
-                  <div className="col-md-4">
-                  {ar.criterias_met.map(cm =>
-                    <div key={`${ar.id}-${cm.domain_criteria_id}`} styleName="tooltip">
-                      {`${cm.domain_criteria_id}`}
-                      <div styleName="tooltiptext">{cm.domain_criteria}</div>
-                    </div>
-                  )}
-                  </div>
-                  <div className="col-md-2">{ar.status}</div>
-                </div>
-              ) : <i>no assessments</i>}
-              <hr/>
-            </span>
+        <div>
+          <div className="row">
+            <div className="col-md-2"><b>supplier</b></div>
+            <div className="col-md-4"><b>title</b></div>
+            <div className="col-md-2"><b>domain</b></div>
+            <div className="col-md-2"><b>assessments</b></div>
+            <div className="col-md-2"><b>case study status</b></div>
           </div>
-        )
+          <span key={cs.id}>
+            <div className="row">
+              <div className="col-md-2">{cs.name}(#{cs.supplier_code})</div>
+              <div className="col-md-4"><a href={`casestudy-assessment/${cs.id}`}>{cs.data.title}(#{cs.id})</a></div>
+              <div className="col-md-2">{cs.data.service}</div>
+              <div className="col-md-2">{cs.assessment_count}</div>
+              <div className="col-md-2">
+                {cs.status}
+                <br/>
+                <a href="#" onClick={e => {
+                  e.preventDefault();
+                  onApproveClick(cs.id);
+                }} name="approve">approve</a>
+                {' '}
+                <a href="#" onClick={e => {
+                  e.preventDefault();
+                  onRejectClick(cs.id);
+                }} name="reject">reject</a>
+              </div>
+            </div>
+            <div className="row">
+              <div>
+                <div className="col-md-2"><b>assessors</b></div>
+                <div className="col-md-4"><b>comment</b></div>
+                <div className="col-md-4"><b>criterias met</b></div>
+                <div className="col-md-2"><b>result</b></div>
+              </div>
+            </div>
+            {cs.assessment_count > 0 ? cs.assessment_results.map(ar =>
+              <div key={ar.id} className="row">
+                <div className="col-md-2">{ar.username}</div>
+                <div className="col-md-4">{ar.comment}</div>
+                <div className="col-md-4">
+                {ar.criterias_met.map(cm =>
+                  <div key={`${ar.id}-${cm.domain_criteria_id}`} styleName="tooltip">
+                    {`${cm.domain_criteria_id}`}
+                    <div styleName="tooltiptext">{cm.domain_criteria}</div>
+                  </div>
+                )}
+                </div>
+                <div className="col-md-2">{ar.status}</div>
+              </div>
+            ) : <i>no assessments</i>}
+            <hr/>
+          </span>
+        </div>
+      )}
+      </div>
     )
   }
 }
