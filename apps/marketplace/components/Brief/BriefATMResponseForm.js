a5eece4ed4e46cd38337e980d77ca51a80b0bd28
Nathan Davison <nathan.davison@digital.gov.au>
2019-01-18 16:13:40 +1100

    Flesh out the ATM brief response form.

--- apps/marketplace/components/Brief/BriefATMResponseForm.js
+++ apps/marketplace/components/Brief/BriefATMResponseForm.js
@@ -6,12 +6,33 @@ import format from 'date-fns/format'
 import DocumentTitle from 'react-document-title'
 import AUpageAlert from '@gov.au/page-alerts/lib/js/react.js'
 import AUheading from '@gov.au/headings/lib/js/react.js'
-import { requiredFile } from 'marketplace/components/validators'
+import { requiredFile, required, validEmail, validPhoneNumber } from 'marketplace/components/validators'
 import ErrorBox from 'shared/form/ErrorBox'
 import FilesInput from 'shared/form/FilesInput'
+import Textfield from 'shared/form/Textfield'
+import Textarea from 'shared/form/Textarea'
 import LoadingButton from 'marketplace/components/LoadingButton/LoadingButton'
 import dmapi from 'marketplace/services/apiClient'
-import styles from './BriefATMResponseForm.scss'
+
+const briefRequiresCriteriaResponses = brief => {
+  let requiresCriteriaResponses = false
+  if (brief.evaluationType.includes('500 word responses to your criteria') && brief.evaluationCriteria.length > 0) {
+    requiresCriteriaResponses = true
+  }
+  return requiresCriteriaResponses
+}
+
+const briefRequiresDocumentUpload = brief => {
+  let mustUpload = false
+  if (
+    brief.evaluationType.includes('Case study') ||
+    brief.evaluationType.includes('References') ||
+    brief.evaluationType.includes('Résumés')
+  ) {
+    mustUpload = true
+  }
+  return mustUpload
+}
 
 const BriefATMResponseForm = ({
   model,
@@ -30,7 +51,7 @@ const BriefATMResponseForm = ({
     <DocumentTitle title="Brief Response - Digital Marketplace">
       <div className="col-sm-push-2 col-sm-8 col-xs-12">
         <article role="main">
-          {briefResponseSuccess && <Redirect to={`${match.url}/rfx/respond/submitted`} />}
+          {briefResponseSuccess && <Redirect to={`${match.url}/atm/respond/submitted`} />}
           {!briefResponseSuccess && (
             <ErrorBox
               title="There was a problem submitting your response"
@@ -42,62 +63,124 @@ const BriefATMResponseForm = ({
           <AUheading level="1" size="xl">
             Apply for &apos;{brief.title}&apos;
           </AUheading>
-          <AUheading level="2" size="lg">
-            Upload documentation
-          </AUheading>
           {app.supplierCode ? (
             <Form model={model} id="briefResponse" onSubmit={data => handleSubmit(data)}>
-              {brief.evaluationType.includes('Response template') && (
-                <div>
-                  <span />
-                  <AUheading level="3" size="sm">
-                    Response template
-                  </AUheading>
-                  <p>Attachment must be .DOC, .XLS, .PPT or .PDF format and a maximum of 20MB</p>
-                  <FilesInput
-                    fieldLabel="Upload response"
-                    name="attachedDocumentURL"
-                    model={`${model}.attachedDocumentURL.0`}
-                    formFields={1}
-                    url={`/brief/${brief.id}/respond/documents/${app.supplierCode}`}
-                    api={dmapi}
-                    fileId={0}
-                    validators={{
-                      requiredFile
-                    }}
-                    messages={{
-                      requiredFile: 'Choose a file for your response'
-                    }}
-                    uploading={uploading}
-                  />
-                </div>
+              <Textfield
+                model={`${model}.availability`}
+                name="availability"
+                id="availability"
+                htmlFor="availability"
+                label="When can you start?"
+                maxLength={100}
+                description={
+                  <span>
+                    The buyer needs this project to start no later than <b>{brief.startDate}</b>
+                  </span>
+                }
+                validators={{
+                  required
+                }}
+                messages={{
+                  required: 'Enter a date for when you can start the project'
+                }}
+              />
+              {briefRequiresCriteriaResponses(brief) && (
+                <AUheading level="2" size="lg">
+                  Evaluation criteria
+                </AUheading>
               )}
-              {brief.evaluationType.includes('Written proposal') && (
-                <div>
-                  <AUheading level="3" size="sm">
-                    Written proposal
-                  </AUheading>
-                  <p>Including:</p>
-                  <ul className={styles.proposalList}>{brief.proposalType.map(type => <li key={type}>{type}</li>)}</ul>
-                  <p>Attachment must be .DOC, .XLS, .PPT or .PDF format and a maximum of 20MB</p>
-                  <FilesInput
-                    fieldLabel="Upload written proposal"
-                    name="attachedDocumentURL"
-                    model={`${model}.attachedDocumentURL.1`}
-                    formFields={1}
-                    url={`/brief/${brief.id}/respond/documents/${app.supplierCode}`}
-                    api={dmapi}
-                    fileId={1}
-                    validators={{
-                      requiredFile
-                    }}
+              {briefRequiresCriteriaResponses(brief) && <p>Demonstrate how you meet the buyer&apos;s needs.</p>}
+              {briefRequiresCriteriaResponses(brief) &&
+                brief.evaluationCriteria.map((evaluationCriteria, i) => (
+                  <Textarea
+                    key={evaluationCriteria.criteria}
+                    model={`${model}.criteria[${evaluationCriteria.criteria}]`}
+                    name={`criteria.${evaluationCriteria.criteria}`}
+                    id={`criteria.${i}`}
+                    controlProps={{ limit: 500 }}
+                    label={evaluationCriteria.criteria}
+                    validators={{ required }}
+                    showMessagesDuringFocus
                     messages={{
-                      requiredFile: 'Choose a file for your written proposal'
+                      required: `The "${evaluationCriteria.criteria}" criteria response is required`
                     }}
-                    uploading={uploading}
                   />
-                </div>
+                ))}
+              {briefRequiresDocumentUpload(brief) && (
+                <AUheading level="2" size="lg">
+                  Upload documentation
+                </AUheading>
               )}
+              {briefRequiresDocumentUpload(brief) && (
+                <ul>
+                  <li>Documents must be in .DOC, .XLS, .PPT, or .PDF format and no more than 50MB.</li>
+                  <li>
+                    You need to provide:
+                    <ul>
+                      {brief.evaluationType.map(item => (
+                        <span key={item}>
+                          {['Case study', 'References', 'Résumés'].includes(item) && <li>{item}</li>}
+                        </span>
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
+              )}
+              {briefRequiresDocumentUpload(brief) && (
+                <FilesInput
+                  label=""
+                  fieldLabel="Upload document"
+                  name="attachedDocumentURL"
+                  model={`${model}.attachedDocumentURL.0`}
+                  formFields={1}
+                  url={`/brief/${brief.id}/respond/documents/${app.supplierCode}`}
+                  api={dmapi}
+                  fileId={0}
+                  validators={{
+                    requiredFile
+                  }}
+                  messages={{
+                    requiredFile: 'You must upload your written proposal'
+                  }}
+                  uploading={uploading}
+                />
+              )}
+              <Textfield
+                model={`${model}.respondToEmailAddress`}
+                name="respondToEmailAddress"
+                id="respondToEmailAddress"
+                htmlFor="respondToEmailAddress"
+                label="Email"
+                description="All communication about your application will be sent to this email."
+                defaultValue={app.emailAddress}
+                maxLength={100}
+                showMaxLength
+                validators={{
+                  required,
+                  validEmail
+                }}
+                messages={{
+                  required: 'You must add an email',
+                  validEmail: 'You must add a valid email'
+                }}
+              />
+              <Textfield
+                model={`${model}.respondToPhone`}
+                name="respondToPhone"
+                id="respondToPhone"
+                htmlFor="respondToPhone"
+                label="Phone number"
+                maxLength={100}
+                showMaxLength
+                validators={{
+                  required,
+                  validPhoneNumber
+                }}
+                messages={{
+                  required: 'You must add a valid phone number',
+                  validPhoneNumber: 'You must add a valid phone number'
+                }}
+              />
               <AUheading level="2" size="lg">
                 Once you submit this application
               </AUheading>
