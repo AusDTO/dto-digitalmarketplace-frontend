a6a166714382b33984931b7dad8f6c01e477812a
Nathan Davison <nathan.davison@digital.gov.au>
2019-01-22 08:35:33 +1100

    Update ATM introduction stage.

--- apps/marketplace/components/BuyerATM/BuyerATMIntroductionStage.js
+++ apps/marketplace/components/BuyerATM/BuyerATMIntroductionStage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Form } from 'react-redux-form'
 import AUheadings from '@gov.au/headings/lib/js/react.js'
 import { AUcallout } from '@gov.au/callout/lib/js/react.js'
+import { rootPath } from 'marketplace/routes'
 import styles from './BuyerATMIntroductionStage.scss'
 
 const BuyerATMIntroductionStage = props => (
@@ -12,8 +13,9 @@ const BuyerATMIntroductionStage = props => (
     </AUheadings>
     <AUcallout description="" className={styles.noticeBar}>
       <strong>
-        This is an EOI/RFI process. In this stage, sellers can only submit up to 500 words to each criteria you provide.
-        If you need proposals or quotes now, go to <a href="#request proposals">request proposals[TODO]</a>.
+        This approach is for expressions of interest or requests for information. Sellers submit up to 500 words to each
+        criteria you provide. If you need proposals, use{' '}
+        <a href={`${rootPath}/outcome-choice`}>seek proposals and quotes</a>.
       </strong>
     </AUcallout>
     <AUheadings level="2" size="lg">
@@ -21,19 +23,34 @@ const BuyerATMIntroductionStage = props => (
     </AUheadings>
     <ul>
       <li>
-        You can use the <a href="#template">list of questions template[TODO]</a> to develop your request offline before
+        You can <a href="#template">download the list of questions XLSX 84KB</a> to prepare your request offline before
         publishing.
       </li>
-      <li>Have your evaluation criteria and closing dates ready.</li>
-      <li>Any documents you upload must be in DOC, XLS, PPT or PDF format.</li>
+      <li>
+        If you are inviting responses from specific sellers, check they are on the{' '}
+        <a href="/search/sellers" target="_blank">
+          Digital Marketplace panel
+        </a>
+        . You can{' '}
+        <a
+          href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000669656-Getting-a-new-seller-on-the-panel"
+          rel="external noopener noreferrer"
+          target="_blank"
+        >
+          encourage a seller to sign up
+        </a>{' '}
+        if they have yet to join.
+      </li>
     </ul>
     <AUheadings level="2" size="lg">
       Getting help
     </AUheadings>
     <p>
-      <a href="#guide">View step by step guide[TODO]</a>
+      <a href="#guide">View support article</a>
       <br />
-      <a href="#chat">Contact us[TODO]</a>
+      <a href="/contact-us" target="_blank">
+        Contact us
+      </a>
     </p>
     <p>All fields are mandatory unless marked optional.</p>
     {props.formButtons}
