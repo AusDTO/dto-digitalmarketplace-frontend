b6f68bd0a500924fa3189918851ddeee7d4d359c
Nathan Davison <nathan.davison@digital.gov.au>
2019-03-07 11:16:52 +1100

    Redo the getBriefLastQuestionDate test to use a static reference date rather than the current today value.

--- apps/marketplace/components/helpers.js
+++ apps/marketplace/components/helpers.js
@@ -39,8 +39,7 @@ export const nextWeekDay = date => {
   return newDate
 }
 
-export const getBriefLastQuestionDate = closingDate => {
-  const today = new Date()
+export const getBriefLastQuestionDate = (closingDate, today = new Date()) => {
   let lastQuestionDate = new Date()
   if (closingDate < addDays(today, 3)) {
     lastQuestionDate = nextWeekDay(subDays(closingDate, 1))
--- apps/marketplace/components/helpers.test.js
+++ apps/marketplace/components/helpers.test.js
@@ -2,29 +2,29 @@ import addDays from 'date-fns/add_days'
 import format from 'date-fns/format'
 import { getBriefLastQuestionDate, nextWeekDay } from './helpers'
 
-const now = new Date()
+const referenceDate = new Date('2019-03-04')
 
 describe('getBriefLastQuestionDate', () => {
   test('When closing date is over a week away', () => {
-    const closingDate = addDays(now, 9)
-    const lastQuestionDate = format(getBriefLastQuestionDate(closingDate), 'YYYY-MM-DD')
-    expect(lastQuestionDate).toEqual(format(nextWeekDay(addDays(now, 5)), 'YYYY-MM-DD'))
+    const closingDate = addDays(referenceDate, 9)
+    const lastQuestionDate = format(getBriefLastQuestionDate(closingDate, referenceDate), 'YYYY-MM-DD')
+    expect(lastQuestionDate).toEqual(format(nextWeekDay(addDays(referenceDate, 5)), 'YYYY-MM-DD'))
   })
 
   test('When closing date is between 2 and 5 days away', () => {
-    const closingDate = nextWeekDay(addDays(now, 3))
-    const lastQuestionDate = format(getBriefLastQuestionDate(closingDate), 'YYYY-MM-DD')
-    expect(lastQuestionDate).toEqual(format(nextWeekDay(addDays(now, 2)), 'YYYY-MM-DD'))
+    const closingDate = nextWeekDay(addDays(referenceDate, 3))
+    const lastQuestionDate = format(getBriefLastQuestionDate(closingDate, referenceDate), 'YYYY-MM-DD')
+    expect(lastQuestionDate).toEqual(format(nextWeekDay(addDays(referenceDate, 2)), 'YYYY-MM-DD'))
   })
 
   test('When closing date is 2 days away', () => {
-    const closingDate = addDays(now, 2)
-    const lastQuestionDate = format(getBriefLastQuestionDate(closingDate), 'YYYY-MM-DD')
+    const closingDate = addDays(referenceDate, 2)
+    const lastQuestionDate = format(getBriefLastQuestionDate(closingDate, referenceDate), 'YYYY-MM-DD')
     if (
-      format(now, 'YYYY-MM-DD') !== format(lastQuestionDate, 'YYYY-MM-DD') &&
+      format(referenceDate, 'YYYY-MM-DD') !== format(lastQuestionDate, 'YYYY-MM-DD') &&
       format(closingDate, 'YYY-MM-DD') > format(lastQuestionDate, 'YYYY-MM-DD')
     ) {
-      expect(lastQuestionDate).toEqual(format(nextWeekDay(addDays(now, 1)), 'YYYY-MM-DD'))
+      expect(lastQuestionDate).toEqual(format(nextWeekDay(addDays(referenceDate, 1)), 'YYYY-MM-DD'))
     }
   })
 })
