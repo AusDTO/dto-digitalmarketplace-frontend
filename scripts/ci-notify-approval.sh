b68c7a0dcd976a1506ed2543ad3d84d1d9bcc8a5
Sam <29763502+malmas18@users.noreply.github.com>
2019-03-20 15:47:36 +1100

    test to support channel

--- scripts/ci-notify-approval.sh
+++ scripts/ci-notify-approval.sh
@@ -2,19 +2,13 @@
 LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
 GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
 PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
- "attachments": [ { "fallback": "Frontend is about to go live!",
- "pretext": "Frontend is about to go live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
+ "attachments": [ { "fallback": ":rotating_light: Frontend is about to go live!",
+ "pretext": ":rotating_light: Frontend is about to go live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
  "text": "'"$GITLOG"'" }] }'
 
 echo $PAYLOAD
-curl -g -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_WEBHOOK_URL"
+curl -g -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_MARKETPLACE_SUPPORT_WEBHOOK_URL"
 
 ENVIRONMENT=production
 LOCAL_USERNAME=`whoami`
 REVISION=`git log -n 1 --pretty=format:"%H"`
-
-curl https://api.rollbar.com/api/1/deploy/ \
-  -F access_token=$ROLLBAR_TOKEN \
-  -F environment=$ENVIRONMENT \
-  -F revision=$REVISION \
-  -F local_username=$LOCAL_USERNAME
