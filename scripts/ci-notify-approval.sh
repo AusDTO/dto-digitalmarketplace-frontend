2f30217ad2795822f483270f7ce8e8943d900042
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-03-20 20:19:05 +1100

    Apply suggestions from code review
    [skip ci]

--- scripts/ci-notify-approval.sh
+++ scripts/ci-notify-approval.sh
@@ -2,8 +2,8 @@
 LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
 GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
 PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
- "attachments": [ { "fallback": ":rotating_light: Frontend is about to go live!",
- "pretext": ":rotating_light: Frontend is about to go live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
+ "attachments": [ { "fallback": ":rotating_light: Frontend is ready to go live!",
+ "pretext": ":rotating_light: Frontend is ready to go live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
  "text": "'"$GITLOG"'" }] }'
 
 echo $PAYLOAD
