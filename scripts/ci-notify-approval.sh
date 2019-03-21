ae4d9d1b032905b82a927d20f2905de432248ba1
Sam <29763502+malmas18@users.noreply.github.com>
2019-03-21 12:41:55 +1100

    fix " in commit message

--- scripts/ci-notify-approval.sh
+++ scripts/ci-notify-approval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LASTTAG=$(git describe --abbrev=0 --tags HEAD~1)
-GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
+GITLOG=$(git log --no-merges --format="%cd %s" --date=short $LASTTAG...HEAD | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g' | sed 's/\"/\\"/g')
 PAYLOAD='{"channel": "#marketplace", "icon_emoji": ":lightning:","username": "releasebot",
  "attachments": [ { "fallback": ":rotating_light: Frontend is ready to go live!",
  "pretext": ":rotating_light: Frontend is ready to go live! '"$CIRCLE_REPOSITORY_URL"'/releases/tag/'"$CIRCLE_TAG"'",
