01e2042f827e00c7bb0738c2b02b70a13c59ca1f
Sam <29763502+malmas18@users.noreply.github.com>
2019-04-11 12:05:18 +1000

    styles

--- apps/marketplace/components/Brief/BriefResponseSupplierError.js
+++ apps/marketplace/components/Brief/BriefResponseSupplierError.js
@@ -4,7 +4,6 @@ import DocumentTitle from 'react-document-title'
 
 import AUpageAlert from '@gov.au/page-alerts/lib/js/react.js'
 
-
 export class BriefResponseSupplierError extends Component {
   formatMessage = (item, row) => {
     const { message, links } = item
@@ -36,40 +35,38 @@ export class BriefResponseSupplierError extends Component {
   }
 
   render() {
-    const {
-      app
-    } = this.props
+    const { app } = this.props
 
-    return <div className="row">
-      <DocumentTitle title="Before you can apply - Digital Marketplace">
-        <div className="col-sm-push-2 col-sm-8 col-xs-12">
-          <h2 className="au-header__heading">Before you can apply</h2>
-          <br />
-          <article role="main">
-            <AUpageAlert as="error">
-              <h4 className="au-display-sm">Please update your profile with:</h4>
-              <ul>
-                {Array.isArray(app.errorMessage) &&
-                  app.errorMessage.map((err, i) => {
-                    return <li key={i}>{this.formatMessage(err, i)}</li>
-                  })}
-              </ul>
-            </AUpageAlert>
+    return (
+      <div className="row">
+        <DocumentTitle title="Before you can apply - Digital Marketplace">
+          <div className="col-sm-push-2 col-sm-8 col-xs-12">
+            <h2 className="au-header__heading">Before you can apply</h2>
             <br />
-            <a href="/sellers/edit" className="au-btn">
-              Update your profile
-          </a>
-          </article>
-        </div>
-      </DocumentTitle>
-    </div>
+            <article role="main">
+              <AUpageAlert as="error">
+                <h4 className="au-display-sm">Please update your profile with:</h4>
+                <ul>
+                  {Array.isArray(app.errorMessage) &&
+                    app.errorMessage.map((err, i) => {
+                      return <li key={i}>{this.formatMessage(err, i)}</li>
+                    })}
+                </ul>
+              </AUpageAlert>
+              <br />
+              <a href="/sellers/edit" className="au-btn">
+                Update your profile
+              </a>
+            </article>
+          </div>
+        </DocumentTitle>
+      </div>
+    )
   }
 }
-
 
 const mapStateToProps = state => ({
   app: state.app
 })
-
 
 export default connect(mapStateToProps)(BriefResponseSupplierError)
