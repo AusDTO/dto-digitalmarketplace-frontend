9eab1c905690dc1e061c37b651a4f6c49078227c
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-01 09:44:59 +1000

    [Header] Add SellerHeaderActions component

--- apps/marketplace/components/Header/HeaderActions.js
+++ apps/marketplace/components/Header/HeaderActions.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import BuyerHeaderActions from './BuyerHeaderActions'
+import SellerHeaderActions from './SellerHeaderActions'
 
 const HeaderActions = props => {
   const { userType } = props
-  return <div className="au-marketplace-header-actions">{userType === 'buyer' && <BuyerHeaderActions />}</div>
+  return (
+    <div className="au-marketplace-header-actions">
+      {userType === 'buyer' && <BuyerHeaderActions />}
+      {userType === 'seller' && <SellerHeaderActions />}
+    </div>
+  )
 }
 
 export default HeaderActions
---
+++ apps/marketplace/components/Header/SellerHeaderActions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SellerHeaderActions = () => (
+  <ul data-reactroot="" id="main-navigation" className="au-marketplace-header-inline-links">
+    <li>
+      <a href="/sellers">Dashboard</a>
+    </li>
+    <li>
+      <a href="/logout">Sign out</a>
+    </li>
+  </ul>
+)
+
+export default SellerHeaderActions
