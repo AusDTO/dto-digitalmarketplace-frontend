ee66d8bfcdeda9f075764b6eec4896e972df55ef
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-01 09:51:42 +1000

    [Header] Add ApplicantHeaderActions component

--- apps/marketplace/components/Header/HeaderActions.js
+++ apps/marketplace/components/Header/HeaderActions.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import ApplicantHeaderActions from './ApplicantHeaderActions'
 import BuyerHeaderActions from './BuyerHeaderActions'
 import SellerHeaderActions from './SellerHeaderActions'
 
@@ -8,6 +9,7 @@ const HeaderActions = props => {
   const { userType } = props
   return (
     <div className="au-marketplace-header-actions">
+      {userType === 'applicant' && <ApplicantHeaderActions />}
       {userType === 'buyer' && <BuyerHeaderActions />}
       {userType === 'seller' && <SellerHeaderActions />}
     </div>
