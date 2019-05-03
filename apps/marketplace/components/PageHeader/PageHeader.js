d4423d7e4928a30d416856c8adab66f76a5131b5
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-03 15:47:17 +1000

    [Teams] Render page header actions

--- apps/marketplace/components/PageHeader/PageHeader.js
+++ apps/marketplace/components/PageHeader/PageHeader.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import styles from './PageHeader.scss'
 
 class PageHeader extends React.PureComponent {
   render() {
-    const { organisation, title } = this.props
+    const { actions, organisation, title } = this.props
 
     return (
-      <div className="row">
-        <div className="col-sm-12">
+      <div className={`${styles.pageHeader} row`}>
+        <div className="col-lg-6 col-md-9 col-sm-12">
           <small>{organisation}</small>
-          <div className="row">
-            <div className="col-xs-12">
-              <h1 className="au-display-xl">{title}</h1>
-            </div>
-          </div>
+          <h1 className={`au-display-xl ${styles.title}`}>{title}</h1>
+        </div>
+        <div className="col-lg-6 col-md-3 col-sm-12">
+          <div className={styles.actions}>{actions.map(action => action)}</div>
         </div>
       </div>
     )
