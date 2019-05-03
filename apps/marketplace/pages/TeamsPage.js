6dd132cb95744d4df9c4296765912a2780fae4fe
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-03 15:45:58 +1000

    [Teams] Pass create team button to page header component

--- apps/marketplace/pages/TeamsPage.js
+++ apps/marketplace/pages/TeamsPage.js
@@ -2,12 +2,20 @@ import React from 'react'
 
 import { BrowserRouter } from 'react-router-dom'
 import { rootPath } from 'marketplace/routes'
+
+import AUbutton from '@gov.au/buttons'
 import PageHeader from '../components/PageHeader/PageHeader'
+
+const createTeamButton = (
+  <AUbutton as="secondary" href="#">
+    Create a team
+  </AUbutton>
+)
 
 const TeamsPage = props => (
   <BrowserRouter basename={`${rootPath}/teams`}>
     <div>
-      <PageHeader organisation={props.organisation} title="Teams and people" />
+      <PageHeader actions={[createTeamButton]} organisation={props.organisation} title="Teams and people" />
     </div>
   </BrowserRouter>
 )
