f646c48b8bb4cfe72fedda7f65c460e28732660a
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-06 09:41:37 +1000

    [Teams] Add PageNavigation component to page

--- apps/marketplace/pages/TeamsPage.js
+++ apps/marketplace/pages/TeamsPage.js
@@ -5,6 +5,7 @@ import { rootPath } from 'marketplace/routes'
 
 import AUbutton from '@gov.au/buttons'
 import PageHeader from '../components/PageHeader/PageHeader'
+import PageNavigation from '../components/PageNavigation/PageNavigation'
 
 const createTeamButton = (
   <AUbutton as="secondary" href="#" key="Create a team">
@@ -16,6 +17,7 @@ const TeamsPage = props => (
   <BrowserRouter basename={`${rootPath}/teams`}>
     <div>
       <PageHeader actions={[createTeamButton]} organisation={props.organisation} title="Teams and people" />
+      <PageNavigation />
     </div>
   </BrowserRouter>
 )
