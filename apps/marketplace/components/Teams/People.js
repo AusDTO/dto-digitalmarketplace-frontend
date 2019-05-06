06f9bc1eed428d81f774dc832e162d10e021241e
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-06 14:23:15 +1000

    [Teams] Add initial Teams and People components

---
+++ apps/marketplace/components/Teams/People.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const People = () => <div>People</div>
+
+export default People
---
+++ apps/marketplace/components/Teams/Teams.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Teams = () => <div>Teams</div>
+
+export default Teams
