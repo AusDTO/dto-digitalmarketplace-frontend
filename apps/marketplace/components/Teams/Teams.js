f843eeabc232d35a41bd54e61083a5229aa338d0
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-06 14:35:08 +1000

    [Teams] Add paragraph to display when no teams exist

--- apps/marketplace/components/Teams/Teams.js
+++ apps/marketplace/components/Teams/Teams.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const Teams = () => <div>Teams</div>
+const Teams = () => (
+  <div>
+    <p>Easily track and manage access to opportunities created within your organisation.</p>
+  </div>
+)
 
 export default Teams
