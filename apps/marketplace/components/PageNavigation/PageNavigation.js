741923dba384aef2dab03d87c9a316988b0c3e17
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-06 10:22:50 +1000

    [Teams] Create li elements for each nav link

--- apps/marketplace/components/PageNavigation/PageNavigation.js
+++ apps/marketplace/components/PageNavigation/PageNavigation.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const PageNavigation = () => <div className="row" />
+const PageNavigation = props => (
+  <div className="row">
+    <div className="col-sm-12">
+      <nav>
+        <ul>{props.links.map(link => <li>{link}</li>)}</ul>
+      </nav>
+    </div>
+  </div>
+)
 
 export default PageNavigation
