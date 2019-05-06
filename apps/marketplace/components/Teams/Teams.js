15433868166ecadbdad82b5dd83697667f85d1de
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-06 14:37:45 +1000

    [Teams] Add create team button to display when no teams exist

--- apps/marketplace/components/Teams/Teams.js
+++ apps/marketplace/components/Teams/Teams.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import AUbutton from '@gov.au/buttons'
+
 const Teams = () => (
   <div>
     <p>Easily track and manage access to opportunities created within your organisation.</p>
+    <AUbutton as="secondary" href="#">
+      Create a team
+    </AUbutton>
   </div>
 )
 
