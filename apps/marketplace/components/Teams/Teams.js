f18f6742a5d0edcb77cd26ec98d906148d528bd2
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-06 15:11:23 +1000

    [Teams] Center content when no teams exist

--- apps/marketplace/components/Teams/Teams.js
+++ apps/marketplace/components/Teams/Teams.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 import AUbutton from '@gov.au/buttons'
 
+import styles from './Teams.scss'
+
 const Teams = () => (
-  <div>
-    <p>Easily track and manage access to opportunities created within your organisation.</p>
-    <AUbutton as="secondary" href="#">
-      Create a team
-    </AUbutton>
-    <a href="">How teams work</a>
+  <div className={styles.teams}>
+    <div className={styles.teamsInitialView}>
+      <p>Easily track and manage access to opportunities created within your organisation.</p>
+      <AUbutton as="secondary" href="#">
+        Create a team
+      </AUbutton>
+      <a href="">How teams work</a>
+    </div>
   </div>
 )
 
