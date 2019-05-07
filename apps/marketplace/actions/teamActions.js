cdbc76aca6eedd305c7603b2039c0f471036ef37
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-07 15:12:51 +1000

    [Teams] Add action to load buyer team members

---
+++ apps/marketplace/actions/teamActions.js
@@ -0,0 +1,20 @@
+import { BUYER_TEAM_MEMBERS_SUCCESS, USER_ORGANISATION } from '../constants/constants'
+import { GENERAL_ERROR } from '../constants/messageConstants'
+import dmapi from '../services/apiClient'
+import { sendingRequest, setErrorMessage } from './appActions'
+
+export const handleTeamActionSuccess = (data, type) => ({ data, type })
+
+export const loadBuyerTeamMembers = (endpoint = '/dashboard/team/overview') => dispatch => {
+  dispatch(sendingRequest(true))
+  dmapi({ url: endpoint }).then(response => {
+    if (!response || response.error) {
+      dispatch(setErrorMessage(GENERAL_ERROR))
+    } else {
+      response.data.loadedAt = new Date().valueOf()
+      dispatch(handleTeamActionSuccess(response.data.organisation, USER_ORGANISATION))
+      dispatch(handleTeamActionSuccess(response.data, BUYER_TEAM_MEMBERS_SUCCESS))
+    }
+    dispatch(sendingRequest(false))
+  })
+}
