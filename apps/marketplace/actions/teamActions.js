45c4ef413f9ca62ef5e3c5c46312ec9028518be5
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-08 09:34:33 +1000

    [Teams] Clear error messages if successful load of buyer team members

--- apps/marketplace/actions/teamActions.js
+++ apps/marketplace/actions/teamActions.js
@@ -1,7 +1,7 @@
 import { BUYER_TEAM_MEMBERS_SUCCESS, USER_ORGANISATION } from '../constants/constants'
 import { GENERAL_ERROR } from '../constants/messageConstants'
 import dmapi from '../services/apiClient'
-import { sendingRequest, setErrorMessage } from './appActions'
+import { clearErrorMessages, sendingRequest, setErrorMessage } from './appActions'
 
 export const handleTeamActionSuccess = (data, type) => ({ data, type })
 
@@ -12,6 +12,7 @@ export const loadBuyerTeamMembers = (endpoint = '/dashboard/team/overview') => d
       dispatch(setErrorMessage(GENERAL_ERROR))
     } else {
       response.data.loadedAt = new Date().valueOf()
+      dispatch(clearErrorMessages())
       dispatch(handleTeamActionSuccess(response.data.organisation, USER_ORGANISATION))
       dispatch(handleTeamActionSuccess(response.data, BUYER_TEAM_MEMBERS_SUCCESS))
     }
