411f4982dd5029ae3322997292721c388115338b
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-07 14:43:03 +1000

    [Teams] Handle user organisation in team reducer

--- apps/marketplace/reducers/teamReducers.js
+++ apps/marketplace/reducers/teamReducers.js
@@ -1,7 +1,16 @@
-const defaultUserState = {}
+import { USER_ORGANISATION } from '../constants/constants'
+
+const defaultUserState = {
+  organisation: ''
+}
 
 const teamReducer = (state = defaultUserState, action) => {
   switch (action.type) {
+    case USER_ORGANISATION:
+      return {
+        ...state,
+        organisation: action.data
+      }
     default:
       return state
   }
