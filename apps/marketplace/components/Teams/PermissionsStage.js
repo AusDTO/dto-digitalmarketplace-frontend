3cce616b3df667c089890c75b910569da73ab221
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-10 08:50:21 +1000

    [Teams] Add paragraph asking what team members can do

--- apps/marketplace/components/Teams/PermissionsStage.js
+++ apps/marketplace/components/Teams/PermissionsStage.js
@@ -5,12 +5,15 @@ import { Form } from 'react-redux-form'
 import AUheadings from '@gov.au/headings'
 import formProps from 'shared/form/formPropsSelector'
 
+import commonStyles from './TeamStages.scss'
+
 const PermissionsStage = props => (
   <Form model={props.model}>
     <AUheadings level="1" size="xl">
       Permissions
     </AUheadings>
     <p>Team members automatically see opportunities created by anyone in the team.</p>
+    <p className={commonStyles.bold}>What can each team member do?</p>
   </Form>
 )
 
