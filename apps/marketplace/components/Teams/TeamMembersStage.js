014a7fee9a896587fa72cb5a2bcc086c20203b3c
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-09 16:15:28 +1000

    [Teams] Add description for team members stage

--- apps/marketplace/components/Teams/TeamMembersStage.js
+++ apps/marketplace/components/Teams/TeamMembersStage.js
@@ -5,11 +5,19 @@ import { Form } from 'react-redux-form'
 import AUheadings from '@gov.au/headings'
 import formProps from 'shared/form/formPropsSelector'
 
+import commonStyles from './TeamStages.scss'
+
 const TeamMembersStage = props => (
   <Form model={props.model}>
     <AUheadings level="1" size="xl">
       Team members
     </AUheadings>
+    <div className={commonStyles.stageContentContainer}>
+      <p>
+        Members must already have a Digital Marketplace account in their name that ends in{' '}
+        <span className={commonStyles.bold}>@humanservices.gov.au</span>
+      </p>
+    </div>
   </Form>
 )
 
