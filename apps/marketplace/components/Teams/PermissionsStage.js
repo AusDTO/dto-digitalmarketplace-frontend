5b1f617c8bc0f6eeeb66a222971b1c745a574025
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-10 11:59:54 +1000

    [Teams] Add checkbox to select all permissions

--- apps/marketplace/components/Teams/PermissionsStage.js
+++ apps/marketplace/components/Teams/PermissionsStage.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux'
 import { Form } from 'react-redux-form'
 
 import AUheading from '@gov.au/headings'
+import { AUcheckbox } from '@gov.au/control-input'
 import formProps from 'shared/form/formPropsSelector'
 
+import styles from './PermissionsStage.scss'
 import commonStyles from './TeamStages.scss'
 
 const PermissionsStage = props => (
@@ -14,6 +16,14 @@ const PermissionsStage = props => (
     </AUheading>
     <p>Team members automatically see opportunities created by anyone in the team.</p>
     <p className={commonStyles.bold}>What can each team member do?</p>
+    <div className={styles.allPermissionsContainer}>
+      <AUcheckbox
+        className={styles.allPermissionsCheckbox}
+        id="all-permissions-checkbox"
+        label="Every member can create drafts, publish opportunities, answer seller questions, download responses, create work orders, download reporting data"
+        name="permissions"
+      />
+    </div>
   </Form>
 )
 
