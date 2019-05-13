461ec4acaf03fe25f275878b7ba381ac20c022e7
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-13 15:11:12 +1000

    [Teams] Apply common styles to next steps list on review stage

--- apps/marketplace/components/Teams/ReviewStage.js
+++ apps/marketplace/components/Teams/ReviewStage.js
@@ -7,6 +7,7 @@ import formProps from 'shared/form/formPropsSelector'
 import TeamTable from './TeamTable'
 
 import styles from './ReviewStage.scss'
+import commonStyles from './TeamStages.scss'
 
 const ReviewStage = props => (
   <Form model={props.model}>
@@ -26,11 +27,13 @@ const ReviewStage = props => (
     <AUheading level="2" size="lg">
       What happens next
     </AUheading>
-    <ul className={styles.nextStepsList}>
-      <li>We will email each member to sign in with the email you have provided.</li>
-      <li>Members will see any current opportunities created by other team members.</li>
-      <li>Members will see all new opportunities created by other team members.</li>
-    </ul>
+    <div className={commonStyles.stageContentContainer}>
+      <ul className={commonStyles.stageList}>
+        <li>We will email each member to sign in with the email you have provided.</li>
+        <li>Members will see any current opportunities created by other team members.</li>
+        <li>Members will see all new opportunities created by other team members.</li>
+      </ul>
+    </div>
   </Form>
 )
 
