63af0e08f245ea0f021e7b74263d2b3af1dbdef5
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-17 11:16:26 +1000

    [Teams] Update create team button hrefs

--- apps/marketplace/components/Teams/TeamsOverview.js
+++ apps/marketplace/components/Teams/TeamsOverview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import AUbutton from '@gov.au/buttons'
+import { rootPath } from 'marketplace/routes'
 
 import styles from './TeamsOverview.scss'
 
@@ -8,7 +9,7 @@ const TeamsOverview = () => (
   <div className={styles.teams}>
     <div className={styles.initial}>
       <p>Easily track and manage access to opportunities created within your organisation.</p>
-      <AUbutton as="secondary" href="#">
+      <AUbutton as="secondary" href={`${rootPath}/teams/create/about`}>
         Create a team
       </AUbutton>
       <a href="">How teams work</a>
