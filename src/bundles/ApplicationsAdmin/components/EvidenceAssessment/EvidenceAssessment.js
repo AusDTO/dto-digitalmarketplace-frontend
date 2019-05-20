58dfaf164cc74afd9e9b1f9daf672459f2be16fd
Nathan Davison <nathan.davison@digital.gov.au>
2019-05-20 14:22:29 +1000

    Flesh out the evidence assessment page for assessing a specific submission.

--- src/bundles/ApplicationsAdmin/components/EvidenceAssessment/EvidenceAssessment.js
+++ src/bundles/ApplicationsAdmin/components/EvidenceAssessment/EvidenceAssessment.js
@@ -1,47 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { AUradio } from '@gov.au/control-input/lib/js/react.js'
-
+import { AUradio } from '@gov.au/control-input'
+import AUselect from '@gov.au/select'
+import AUtextInput from '@gov.au/text-inputs'
+import { approveEvidence, rejectEvidence } from '../../redux/modules/evidence'
+import format from 'date-fns/format'
 import './EvidenceAssessment.css'
 
 class EvidenceAssessment extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      hasFailingCriteria: false
+      hasFailingCriteria: false,
+      wasApproved: false,
+      wasRejected: false
     }
 
     const criteria = {}
     Object.keys(this.props.evidence.evidence.criteriaResponses).map(criteriaId => {
       criteria[criteriaId] = {
-        demonstrates: undefined
+        demonstrates: undefined,
+        reason: '',
+        feedback: ''
       }
     })
     this.state.criteria = criteria
+
+    this.handleAssessmentApprove = this.handleAssessmentApprove.bind(this)
+    this.handleAssessmentReject = this.handleAssessmentReject.bind(this)
   }
 
-  getReviewChoice(criteriaId) {
-    return (
-      <div>
-        <AUradio
-          label="Demonstrates"
-          name={`criteria-review-${criteriaId}`}
-          id={`criteria-review-${criteriaId}-yes`}
-          checked={this.state.criteria[criteriaId] === true}
-          onChange={e => console.log(e)}
-        />
-        <AUradio
-          label="Does not demonstrate"
-          name={`criteria-review-${criteriaId}`}
-          id={`criteria-review-${criteriaId}-no`}
-          checked={this.state.criteria[criteriaId] === false}
-          onChange={e => console.log(e)}
-        />
-        {this.state.criteria[criteriaId] === false && (
-          <p>xxx</p>
-        )}
-      </div>
-    )
+  hasReviewedAllCriteria() {
+    return Object.keys(this.state.criteria).every(id => {
+      return this.state.criteria[id].demonstrates === true || (this.state.criteria[id].demonstrates === false && this.state.criteria[id].reason)
+    })
   }
 
   getCriteriaName(criteriaId) {
@@ -49,38 +41,195 @@ class EvidenceAssessment extends React.Component {
     return match.name ? match.name : ''
   }
 
+  getReasonOptions() {
+    const reasonsText = [
+      'Not clear what the seller specifically did',
+      'Evidence does not contain enough detail to assess',
+      'Evidence does not relate to the criteria',
+      'Evidence only partially demonstrates the criteria',
+      'The situation cited does not reflect a common government need'
+    ]
+    const reasons = [
+      {
+        value: '',
+        text: 'Select a reason...'
+      }
+    ]
+    reasonsText.map(reason => reasons.push({ value: reason, text: reason }))
+    return reasons
+  }
+
+  handleCriteriaReviewClick(e, criteriaId) {
+    e.persist()
+    this.setState(curState => {
+      const value = e.target.value === "yes" ? true : false
+      const newState = { ...curState }
+      newState.criteria[criteriaId].demonstrates = value
+      const currentFailed = Object.keys(curState.criteria).filter(id => curState.criteria[id].demonstrates === false)
+      if (currentFailed && currentFailed.length > 0) {
+        newState.hasFailingCriteria = true
+      } else {
+        newState.hasFailingCriteria = false
+      }
+      return newState
+    })
+  }
+
+  handleFeedbackChange(e, type, criteriaId) {
+    e.persist()
+    this.setState(curState => {
+      const feedback = e.target.value
+      const newState = { ...curState }
+      newState.criteria[criteriaId][type] = feedback
+      return newState
+    })
+  }
+
+  handleAssessmentReject() {
+    const evidenceId = this.props.match.params.id
+    const failed_criteria = {}
+    Object.keys(this.state.criteria).map(criteriaId => {
+      if (this.state.criteria[criteriaId].demonstrates === false && this.state.criteria[criteriaId].reason) {
+        failed_criteria[criteriaId] = {
+          reason: this.state.criteria[criteriaId].reason,
+          feedback: this.state.criteria[criteriaId].feedback
+        }
+      }
+    })
+    this.props.rejectEvidence(evidenceId, failed_criteria).then(res => {
+      this.setState({
+        wasApproved: false,
+        wasRejected: true
+      })
+    })
+  }
+
+  handleAssessmentApprove() {
+    const evidenceId = this.props.match.params.id
+    this.props.approveEvidence(evidenceId).then(res => {
+      this.setState({
+        wasApproved: true,
+        wasRejected: false
+      })
+    })
+  }
+
   render() {
     const { evidence } = this.props
 
-    console.log(this.state)
+    if (evidence && evidence.status !== 'submitted') {
+      return (
+        <div>
+          <h1 className="au-display-xl">Error loading evidence assessment</h1>
+          <p>
+            This evidence submission ({evidence.domain.name} for {evidence.supplier.name}){' '}
+            {evidence.status === 'assessed' && <span>was already approved on {format(evidence.updated_at, 'DD/MM/YYYY')}.</span>}
+            {evidence.status === 'rejected' && <span>was already rejected on {format(evidence.updated_at, 'DD/MM/YYYY')}.</span>}
+            {evidence.status === 'draft' && <span>is not yet submitted.</span>}
+          </p>
+          <p><a href="/admin/evidence-assessments">Return to evidence assessment list</a></p>
+        </div>
+      )
+    }
 
-    return (
-      <div>
-        <span styleName="supplierInfo">
-          {evidence.supplier.name} and {evidence.supplier.code}
-        </span>
-        <h1 className="au-display-xl">{evidence.domain.name} assessment</h1>
-        <h2 className="au-display-md">{evidence.evidence.client}</h2>
-        <p>
-          {evidence.evidence.from.month}-{evidence.evidence.from.year} to {evidence.evidence.to.month}-{evidence.evidence.to.year}
-        </p>
-        <p>{evidence.evidence.background}</p>
-        {Object.keys(evidence.evidence.criteriaResponses).map(criteriaId => (
-          <React.Fragment key={criteriaId}>
-            <p>
-              <strong>{this.getCriteriaName(criteriaId)}</strong>
-            </p>
-            <p>{evidence.evidence.criteriaResponses[criteriaId]}</p>
-            <p>
-              {this.getReviewChoice(criteriaId)}
-            </p>
-          </React.Fragment>
-        ))}
-        <p>
+    if (this.state.wasApproved) {
+      return (
+        <div>
+          <h1 className="au-display-xl">Assessment approved</h1>
+          <p>{evidence.domain.name} for <strong>{evidence.supplier.name} ({evidence.supplier.code})</strong> was approved.</p>
+          <p><a href="/admin/evidence-assessments">Return to evidence assessment list</a></p>
+        </div>
+      )
+    }
 
-        </p>
-      </div>
-    )
+    if (this.state.wasRejected) {
+      return (
+        <div>
+          <h1 className="au-display-xl">Assessment rejected</h1>
+          <p>{evidence.domain.name} for <strong>{evidence.supplier.name} ({evidence.supplier.code})</strong> was rejected.</p>
+          <p><a href="/admin/evidence-assessments">Return to evidence assessment list</a></p>
+        </div>
+      )
+    }
+
+    if (evidence && evidence.supplier) {
+      return (
+        <div>
+          <span styleName="supplierInfo">
+            {evidence.supplier.name} and {evidence.supplier.code}
+          </span>
+          <h1 className="au-display-xl">{evidence.domain.name} assessment</h1>
+          <h2 className="au-display-md">{evidence.evidence.client}</h2>
+          <p>
+            {evidence.evidence.from.month}-{evidence.evidence.from.year} to {evidence.evidence.to.month}-{evidence.evidence.to.year}
+          </p>
+          <p>{evidence.evidence.background}</p>
+          {Object.keys(evidence.evidence.criteriaResponses).map(criteriaId => (
+            <React.Fragment key={criteriaId}>
+              <p>
+                <strong>{this.getCriteriaName(criteriaId)}</strong>
+              </p>
+              <p>{evidence.evidence.criteriaResponses[criteriaId]}</p>
+              <p>
+                <span styleName="criteriaReview">
+                  <AUradio
+                    label="Demonstrates"
+                    name={`criteria-review-${criteriaId}`}
+                    id={`criteria-review-${criteriaId}-yes`}
+                    value="yes"
+                    checked={this.state.criteria[criteriaId].demonstrates === true}
+                    onChange={e => this.handleCriteriaReviewClick(e, criteriaId)}
+                    block
+                  />
+                  <AUradio
+                    label="Does not demonstrate"
+                    name={`criteria-review-${criteriaId}`}
+                    value="no"
+                    id={`criteria-review-${criteriaId}-no`}
+                    checked={this.state.criteria[criteriaId].demonstrates === false}
+                    onChange={e => this.handleCriteriaReviewClick(e, criteriaId)}
+                    block
+                  />
+                  {this.state.criteria[criteriaId].demonstrates === false && (
+                    <span styleName="feedback">
+                      <AUselect
+                        id={`criteria-review-${criteriaId}-reason`}
+                        name={`criteria-review-${criteriaId}-reason`}
+                        options={this.getReasonOptions()}
+                        value={this.state.criteria[criteriaId].reason}
+                        onChange={e => this.handleFeedbackChange(e, 'reason', criteriaId)}
+                        block
+                      />
+                      <AUtextInput
+                        as="textarea"
+                        block
+                        placeholder="Feedback shown to seller (optional)"
+                        value={this.state.criteria[criteriaId].feedback}
+                        onChange={e => this.handleFeedbackChange(e, 'feedback', criteriaId)}
+                      />
+                    </span>
+                  )}
+                </span>
+              </p>
+            </React.Fragment>
+          ))}
+          <p>
+            {this.hasReviewedAllCriteria() && this.state.hasFailingCriteria && (
+              <button name="reject" styleName="actionButton rejectButton" onClick={this.handleAssessmentReject}>
+                Reject assessment
+              </button>
+            )}
+            {this.hasReviewedAllCriteria() && !this.state.hasFailingCriteria && (
+              <button name="reject" styleName="actionButton approveButton" onClick={this.handleAssessmentApprove}>
+                Approve assessment
+              </button>
+            )}
+          </p>
+        </div>
+      )
+    }
+
+    return <div />
   }
 }
 
@@ -88,6 +237,11 @@ const mapStateToProps = ({ evidence, meta }) => {
   return { evidence, meta }
 }
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => {
+  return {
+    approveEvidence: id => dispatch(approveEvidence(id)),
+    rejectEvidence: (id, feedback) => dispatch(rejectEvidence(id, feedback))
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(EvidenceAssessment)
--- src/bundles/ApplicationsAdmin/redux/modules/evidence.js
+++ src/bundles/ApplicationsAdmin/redux/modules/evidence.js
@@ -2,18 +2,18 @@ export const APPROVED_EVIDENCE = 'APPROVED_EVIDENCE';
 export const REJECTED_EVIDENCE = 'REJECTED_EVIDENCE';
 
 export default function reducer(state = {}, action = {}) {
-  const { type, evidence } = action;
+  const { type } = action;
   switch (type) {
     case APPROVED_EVIDENCE:
-      return [...state, evidence]
+      return state
     case REJECTED_EVIDENCE:
-      return [...state, evidence]
+      return state
     default:
       return state;
   }
 }
 
-export const approvedEvidence = (id) => ({ type: APPROVED_EVIDENCE, id });
+export const approvedEvidence = () => ({ type: APPROVED_EVIDENCE });
 
 export const approveEvidence = (id) => {
   return (dispatch, getState, api) => {
@@ -27,28 +27,24 @@ export const approveEvidence = (id) => {
       }
     })
     .then((response) => response.json())
-    .then((json) => {
-      dispatch(approvedEvidence(id))
-    })
+    .then(() => dispatch(approvedEvidence()))
   }
 };
 
-export const rejectedEvidence = (id, message) => ({ type: REJECTED_EVIDENCE, id, message });
+export const rejectedEvidence = () => ({ type: APPROVED_EVIDENCE });
 
-export const rejectEvidence = (id, message) => {
-    return (dispatch, getState, api) => {
-        const state = getState();
-        return api(state.meta.url_reject, {
-            method: 'POST',
-            body: JSON.stringify({application_id: id, message: message}),
-            headers: {
-                // Flask expects the token as a header.
-                'X-CSRFToken': state.form_options.csrf_token
-            }
-        })
-            .then((response) => response.json())
-            .then((json) => {
-                dispatch(rejectedEvidence(id))
-            })
-    }
+export const rejectEvidence = (id, failed_criteria) => {
+  return (dispatch, getState, api) => {
+    const state = getState();
+    return api(state.meta.url_reject, {
+      method: 'POST',
+      body: JSON.stringify({id, failed_criteria}),
+      headers: {
+        // Flask expects the token as a header.
+        'X-CSRFToken': state.form_options.csrf_token
+      }
+    })
+    .then((response) => response.json())
+    .then(() => dispatch(rejectedEvidence()))
+  }
 };
