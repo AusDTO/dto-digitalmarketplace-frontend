78bd5e0175524800097472f22de06f2718aeb8e8
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-05-29 14:51:39 +1000

    fix week day calculation problem (#773)
    * fix week day calculation problem

--- apps/marketplace/components/helpers.js
+++ apps/marketplace/components/helpers.js
@@ -31,23 +31,22 @@ export const getResponsesFileSizeAndType = (bytes, lot) => {
   return result
 }
 
-export const nextWeekDay = date => {
+const getPreviousWeekDay = date => {
   let newDate = date
   while (isWeekend(newDate)) {
-    newDate = addDays(newDate, 1)
+    newDate = subDays(newDate, 1)
   }
   return newDate
 }
 
 export const getBriefLastQuestionDate = (closingDate, today = new Date()) => {
-  let lastQuestionDate = new Date()
+  let lastQuestionDate = getPreviousWeekDay(subDays(closingDate, 1))
   if (closingDate <= addDays(today, 3)) {
-    lastQuestionDate = nextWeekDay(subDays(closingDate, 1))
     if (today > lastQuestionDate) {
       lastQuestionDate = today
     }
   } else {
-    lastQuestionDate = nextWeekDay(subDays(closingDate, 2))
+    lastQuestionDate = getPreviousWeekDay(subDays(lastQuestionDate, 1))
   }
   if (lastQuestionDate > closingDate) {
     lastQuestionDate = closingDate
