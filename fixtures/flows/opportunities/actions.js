6ae18efbd3d35f94ec5873bbcfad35949804a05b
Sam <29763502+malmas18@users.noreply.github.com>
2019-05-30 16:38:14 +1000

    remove double export

--- fixtures/flows/opportunities/actions.js
+++ fixtures/flows/opportunities/actions.js
@@ -42,15 +42,6 @@ export const applyForRfx = async () => {
   await util.clickLink('Apply for opportunity')
 }
 
-export const applyForSpecialist = async number => {
-  if (number === 0) {
-    await util.clickLink('Apply Now')
-  } else {
-    await util.clickLink('Edit application')
-    await util.clickLink('Add another specialist')
-  }
-}
-
 export const viewSpecialistApplication = async title => {
   await util.clickLink('View your application')
   await util.matchText('h1', `Thanks for your application. You've now applied for ‘${title}’`)
