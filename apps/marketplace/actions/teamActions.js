5d18aa17c1aa4d14cf0aa7df11b9c73d059e5d8a
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-31 13:40:14 +1000

    [Teams] Add action to create a team

--- apps/marketplace/actions/teamActions.js
+++ apps/marketplace/actions/teamActions.js
@@ -1,7 +1,33 @@
-import { SAVE_TEAM_SUCCESS } from '../constants/constants'
+import { CREATE_TEAM_SUCCESS, SAVE_TEAM_SUCCESS } from '../constants/constants'
 import { GENERAL_ERROR } from '../constants/messageConstants'
 import dmapi from '../services/apiClient'
 import { sendingRequest, setErrorMessage } from './appActions'
+
+export const handleCreateTeamSuccess = response => ({
+  type: CREATE_TEAM_SUCCESS,
+  team: response.data
+})
+
+export const createTeam = () => (dispatch, getState) => {
+  dispatch(sendingRequest(true))
+  return dmapi({
+    url: '/team',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': getState().app.csrfToken
+    }
+  }).then(response => {
+    if (!response || response.error) {
+      const errorMessage = response.data.message ? response.data.message : GENERAL_ERROR
+      dispatch(setErrorMessage(errorMessage))
+    } else {
+      dispatch(handleCreateTeamSuccess(response))
+    }
+    dispatch(sendingRequest(false))
+    return response
+  })
+}
 
 export const handleSaveTeamSuccess = response => ({
   type: SAVE_TEAM_SUCCESS,
