14363bbaa3aaa0c913baa382e061e13fecbb1dfd
Nathan Davison <nathan.davison@digital.gov.au>
2019-05-31 14:10:08 +1000

    Changes to assessment rejection message.

--- src/bundles/ApplicationsAdmin/rejectEmailTemplate.js
+++ src/bundles/ApplicationsAdmin/rejectEmailTemplate.js
@@ -1,31 +1,23 @@
 export const templateString = (
   
-  `Hi {{ supplier_name }} team,
+  `Hi,
 
-We have completed an assessment you requested under {{ domain_name }} [to demonstrate the assessment criteria](https://marketplace1.zendesk.com/hc/en-gb/articles/333757011655-Assessment-criteria).
+We have completed an assessment for {{ domain_name }}. Unfortunately the case study did not demonstrate the criteria for the following reason/s:
 
-**Assessment result: Not approved**
+\#
 
-This means you are not currently eligible to respond to opportunities in this category.
+If you wish to respond to opportunities in {{ domain_name }}:
 
-Note to Ops team drafter: Delete the dot points that do not apply to this assessment feedback.
+  1. Incorporate the feedback and [read our guide](https://marketplace1.zendesk.com/hc/en-gb/articles/115011407668-Writing-a-case-study) on how to best demonstrate your expertise.
+  1. Select the [assessment criteria](https://marketplace1.zendesk.com/hc/en-gb/articles/333757011655-Assessment-criteria) that best demonstrates your expertise in {{ domain_name }}.
+  1. Write about what you did for each criteria. Make sure you demonstrate at least the minimum number of required criteria for the category.
+  1. Resubmit your case study for assessment.
 
-The case study did not meet the requirements for the following reason/s:
+If you have any questions, [contact the Marketplace](https://marketplace.service.gov.au/contact-us).
 
-* The case study provided information on what was done but requires more detail on how it was done. This made it difficult to relate service delivery back to the criteria for this area of expertise.
-* The case study did not clearly establish what role the seller had in the outcomes delivered and described in the case study.
-* An assessment of the case study and the pricing provided does not indicate value for money to buyers seeking similar services under the Digital Marketplace panel.
-
-We recommend to check the overall quality of your case study prior to resubmitting your case study. Please note: Your case study is open to view when registered government buyers search for suitable sellers for their opportunities.
-
-If you wish to provide services under {{ domain_name }} through the Marketplace in the future, you must **submit a new or updated case study** for assessment.
-
-Please read our [seller guide](https://marketplace1.zendesk.com/hc/en-gb) and consider the [assessment criteria](https://marketplace1.zendesk.com/hc/en-gb/articles/333757011655-Assessment-criteria) for the category before updating your case study.
-
-If you have any questions or need guidance, [contact the Marketplace](https://marketplace.service.gov.au/contact-us).
-
-Thank you
-
-The Digital Marketplace team`
+Thank you,  
+The Digital Marketplace team  
+Digital Transformation Agency  
+[marketplace.service.gov.au](https://marketplace.service.gov.au) | [dta.gov.au](https://dta.gov.au)`
 
 )
