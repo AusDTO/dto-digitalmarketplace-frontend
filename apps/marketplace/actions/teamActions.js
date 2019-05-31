8d2e87fe3cad6f87e94a7ef330c442829e260af5
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-05-31 16:26:03 +1000

    [Teams] Add action to get team data

--- apps/marketplace/actions/teamActions.js
+++ apps/marketplace/actions/teamActions.js
@@ -1,4 +1,4 @@
-import { CREATE_TEAM_SUCCESS, SAVE_TEAM_SUCCESS } from '../constants/constants'
+import { CREATE_TEAM_SUCCESS, GET_TEAM_SUCCESS, SAVE_TEAM_SUCCESS } from '../constants/constants'
 import { GENERAL_ERROR } from '../constants/messageConstants'
 import dmapi from '../services/apiClient'
 import { sendingRequest, setErrorMessage } from './appActions'
@@ -23,6 +23,32 @@ export const createTeam = () => (dispatch, getState) => {
       dispatch(setErrorMessage(errorMessage))
     } else {
       dispatch(handleCreateTeamSuccess(response))
+    }
+    dispatch(sendingRequest(false))
+    return response
+  })
+}
+
+export const handleGetTeamSuccess = response => ({
+  type: GET_TEAM_SUCCESS,
+  team: response.data
+})
+
+export const getTeam = teamId => (dispatch, getState) => {
+  dispatch(sendingRequest(true))
+  return dmapi({
+    url: `/team/${teamId}`,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': getState().app.csrfToken
+    }
+  }).then(response => {
+    if (!response || response.error) {
+      const errorMessage = response.data.message ? response.data.message : GENERAL_ERROR
+      dispatch(setErrorMessage(errorMessage))
+    } else {
+      dispatch(handleGetTeamSuccess(response))
     }
     dispatch(sendingRequest(false))
     return response
