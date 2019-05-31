dad9b519e6b77f279bf5fb3fccf5a55b02ef9385
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-05-31 20:39:31 +1000

    Update specialist.js

--- fixtures/tests/briefResponse/specialist.js
+++ fixtures/tests/briefResponse/specialist.js
@@ -4,13 +4,13 @@ import respond from '../../flows/briefResponse/specialist'
 import create from '../../flows/brief/specialist'
 import startBrief from '../../flows/dashboard/buyer'
 
-describe('create and respond to new specialist brief', () => {
+describe('create and respond to specialist opportunity', () => {
   // in order to get the right brief we are going for 'today's date'.
   const today = Date.now()
   const title = `Specialist ${today.valueOf()}`
   let brief = null
 
-  it('should be able to create new Specialist', async () => {
+  it('should be able to create specialist opportunity', async () => {
     await buyerLogin()
     await startBrief()
     brief = await create({
@@ -19,7 +19,7 @@ describe('create and respond to new specialist brief', () => {
       categoryId: process.env.SELLER_CATEGORY
     })
   })
-  it('should be able to respond to new specialist brief', async () => {
+  it('should be able to respond to specialist opportunity', async () => {
     await sellerLogin()
     for (let i = 0; i < parseInt(brief.numberOfSuppliers, 10); i += 1) {
       // eslint-disable-next-line no-await-in-loop
