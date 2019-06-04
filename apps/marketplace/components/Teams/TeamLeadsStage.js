4e83ae6d4b34a073b7f35071073b2d915ca8d9c6
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-04 12:06:36 +1000

    [Teams] Show domain in team lead description

--- apps/marketplace/components/Teams/TeamLeadsStage.js
+++ apps/marketplace/components/Teams/TeamLeadsStage.js
@@ -8,41 +8,45 @@ import Textfield from 'shared/form/Textfield'
 
 import commonStyles from './TeamStages.scss'
 
-const teamLeadNameDescription = (
+const TeamLeadNameDescription = props => (
   <span>
     Team leads must already have a Digital Marketplace account in their name that ends in{' '}
-    <span className={commonStyles.bold}>@humanservices.gov.au</span>
+    <span className={commonStyles.bold}>@{props.domain}</span>
   </span>
 )
 
-const TeamLeadsStage = props => (
-  <Form model={props.model} onSubmit={props.onSubmit} onSubmitFailed={props.onSubmitFailed}>
-    <AUheading level="1" size="xl">
-      Team leads
-    </AUheading>
-    <div className={commonStyles.stageContentContainer}>
-      <p className={commonStyles.bold}>Team leads can:</p>
-      <ul className={commonStyles.stageList}>
-        <li>Add and remove members</li>
-        <li>Specify what each member can do</li>
-        <li>Create other team leads</li>
-      </ul>
-    </div>
-    <Textfield
-      defaultValue={props[props.model].teamLeadName}
-      description={teamLeadNameDescription}
-      htmlFor="teamLeadName"
-      id="teamLeadName"
-      label="Team lead name"
-      maxLength={100}
-      model={`${props.model}.teamLeadName`}
-      name="teamLeadName"
-      placeholder=""
-      validators={{}}
-    />
-    {props.formButtons}
-  </Form>
-)
+const TeamLeadsStage = props => {
+  const teamLeadNameDescription = <TeamLeadNameDescription domain={props[props.model].domain} />
+
+  return (
+    <Form model={props.model} onSubmit={props.onSubmit} onSubmitFailed={props.onSubmitFailed}>
+      <AUheading level="1" size="xl">
+        Team leads
+      </AUheading>
+      <div className={commonStyles.stageContentContainer}>
+        <p className={commonStyles.bold}>Team leads can:</p>
+        <ul className={commonStyles.stageList}>
+          <li>Add and remove members</li>
+          <li>Specify what each member can do</li>
+          <li>Create other team leads</li>
+        </ul>
+      </div>
+      <Textfield
+        defaultValue={props[props.model].teamLeadName}
+        description={teamLeadNameDescription}
+        htmlFor="teamLeadName"
+        id="teamLeadName"
+        label="Team lead name"
+        maxLength={100}
+        model={`${props.model}.teamLeadName`}
+        name="teamLeadName"
+        placeholder=""
+        validators={{}}
+      />
+      {props.formButtons}
+    </Form>
+  )
+}
 
 TeamLeadsStage.defaultProps = {
   onSubmit: () => {},
