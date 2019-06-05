cbc333510e740305cdd0cb535ff8b193018f4a9f
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-05 11:27:09 +1000

    [Teams] Replace Textfield with Design System text input

--- apps/marketplace/components/ItemSelect/ItemSelectInput.js
+++ apps/marketplace/components/ItemSelect/ItemSelectInput.js
@@ -1,19 +1,14 @@
 import React from 'react'
 
-import Textfield from 'shared/form/Textfield'
+import AUtextInput from '@gov.au/text-inputs/lib/js/react.js'
 
 const ItemSelectInput = props => (
-  <Textfield
-    defaultValue={props.defaultValue}
-    description={props.description}
-    htmlFor={props.id}
+  <AUtextInput
+    className={props.className}
     id={props.id}
-    label={props.label}
-    maxLength={props.maxLength}
-    model={props.model}
-    name={props.name}
+    onChange={props.handleSearchChange}
     placeholder={props.placeholder}
-    validators={props.validators}
+    value={props.inputValue}
   />
 )
 
