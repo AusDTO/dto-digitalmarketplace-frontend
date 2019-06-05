c05239704ca956a53a0ba5e61c795f69163e0d94
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-05 12:00:43 +1000

    [Teams] Destructure props for ItemSelect and ItemSelectInput components

--- apps/marketplace/components/ItemSelect/ItemSelect.js
+++ apps/marketplace/components/ItemSelect/ItemSelect.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 import ItemSelectInput from './ItemSelectInput'
 
-const ItemSelect = props => (
-  <ItemSelectInput {...props} />
-)
+const ItemSelect = props => {
+  const { className, description, handleSearchChange, id, inputValue, label, placeholder } = props
+
+  return (
+    <ItemSelectInput
+      className={className}
+      description={description}
+      handleSearchChange={handleSearchChange}
+      id={id}
+      inputValue={inputValue}
+      label={label}
+      placeholder={placeholder}
+    />
+  )
+}
 
 export default ItemSelect
--- apps/marketplace/components/ItemSelect/ItemSelectInput.js
+++ apps/marketplace/components/ItemSelect/ItemSelectInput.js
@@ -5,21 +5,25 @@ import AUtextInput from '@gov.au/text-inputs/lib/js/react.js'
 // eslint-disable-next-line no-unused-vars
 import styles from './ItemSelectInput.scss'
 
-const ItemSelectInput = props => (
-  <div>
-    <label htmlFor={props.id}>{props.label}</label>
+const ItemSelectInput = props => {
+  const { className, description, handleSearchChange, id, inputValue, label, placeholder } = props
+
+  return (
     <div>
-      <span>{props.description}</span>
+      <label htmlFor={id}>{label}</label>
+      <div>
+        <span>{description}</span>
+      </div>
+      <AUtextInput
+        block
+        className={className}
+        id={id}
+        onChange={handleSearchChange}
+        placeholder={placeholder}
+        value={inputValue}
+      />
     </div>
-    <AUtextInput
-      block
-      className={props.className}
-      id={props.id}
-      onChange={props.handleSearchChange}
-      placeholder={props.placeholder}
-      value={props.inputValue}
-    />
-  </div>
-)
+  )
+}
 
 export default ItemSelectInput
