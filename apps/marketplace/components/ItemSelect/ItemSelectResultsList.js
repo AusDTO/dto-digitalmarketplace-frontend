ab1d0346f48f899a834849eb402c6f3fdad01556
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-06 20:30:28 +1000

    [Teams] Set max height for item select results

--- apps/marketplace/components/ItemSelect/ItemSelectResultsList.js
+++ apps/marketplace/components/ItemSelect/ItemSelectResultsList.js
@@ -3,10 +3,18 @@ import React from 'react'
 import styles from './ItemSelectResultsList.scss'
 
 const ItemSelectResultsList = props => {
-  const { emptyResultsMessage, keywords, resultIsEmpty, resultListItems } = props
+  const { emptyResultsMessage, items, keywords, resultListItems } = props
+
+  const numberOfItems = items.length
+  const resultIsEmpty = numberOfItems < 1
 
   return (
-    <ul className={styles.itemSelectList}>
+    <ul
+      className={`
+        ${styles.itemSelectList}
+        ${!resultIsEmpty ? styles.hasResults : ''}
+      `}
+    >
       {resultIsEmpty && keywords && emptyResultsMessage}
       {!resultIsEmpty && resultListItems}
     </ul>
