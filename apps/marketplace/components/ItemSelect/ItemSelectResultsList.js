4f73559b43a76e2852f37118e8a9122abd93ca39
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-06 15:50:29 +1000

    [Teams] Add padding and border to the results of the item select

--- apps/marketplace/components/ItemSelect/ItemSelectResultsList.js
+++ apps/marketplace/components/ItemSelect/ItemSelectResultsList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import styles from './ItemSelectResultsList.scss'
+
 const ItemSelectResultsList = props => {
   const { emptyResultsMessage, keywords, resultIsEmpty, resultListItems } = props
 
   return (
-    <ul>
+    <ul className={styles.itemSelectList}>
       {resultIsEmpty && keywords && emptyResultsMessage}
       {!resultIsEmpty && resultListItems}
     </ul>
