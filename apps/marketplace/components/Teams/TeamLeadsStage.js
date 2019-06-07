d292c7c18aaa6ee48a9a22c2f62868c15d3bf70e
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-07 12:02:34 +1000

    [Teams] Add an ItemSelect component to find team members

--- apps/marketplace/components/Teams/TeamLeadsStage.js
+++ apps/marketplace/components/Teams/TeamLeadsStage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Form } from 'react-redux-form'
 
 import AUheading from '@gov.au/headings'
+import { findTeamMember } from 'marketplace/actions/teamActions'
 import formProps from 'shared/form/formPropsSelector'
 import ItemSelect from '../ItemSelect/ItemSelect'
 
@@ -15,14 +16,67 @@ const TeamLeadNameDescription = props => (
   </span>
 )
 
+const EmptyResultsMessage = () => <li>User cannot be found.</li>
+
+const TeamMemberListItems = props =>
+  props.items.map(item => (
+    <li key={item.id}>
+      <a href={`#${item}`}>
+        {item.name} ({item.email})
+      </a>
+    </li>
+  ))
+
 export class TeamLeadsStage extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      inputValue: '',
+      timeoutId: null,
+      users: []
+    }
+
+    this.handleSearchChange = this.handleSearchChange.bind(this)
+  }
+
+  handleSearchChange(e) {
+    const keywords = e.target.value
+    let timeoutHandle = null
+
+    this.setState({
+      inputValue: keywords
+    })
+
+    if (timeoutHandle) {
+      clearTimeout(timeoutHandle)
+    }
+
+    timeoutHandle = setTimeout(() => {
+      if (keywords && keywords.length >= this.props.minimumSearchChars) {
+        this.props
+          .findTeamMember(keywords)
+          .then(data => {
+            const noResults = !data.users.length > 0
+            this.setState({
+              users: data.users,
+              noResults
+            })
+          })
+          .catch(() => {})
+      } else {
+        this.setState({
+          users: []
+        })
+      }
+    }, 500)
   }
 
   render() {
-    const { formButtons, model, onSubmit, onSubmitFailed } = this.props
+    const { formButtons, minimumSearchChars, model, onSubmit, onSubmitFailed } = this.props
     const teamLeadNameDescription = <TeamLeadNameDescription domain={this.props[model].domain} />
+    const emptyResultsMessage = <EmptyResultsMessage />
+    const teamMemberListItems = <TeamMemberListItems items={this.state.users} />
 
     return (
       <Form model={model} onSubmit={onSubmit} onSubmitFailed={onSubmitFailed}>
@@ -40,13 +94,20 @@ export class TeamLeadsStage extends Component {
         <ItemSelect
           defaultValue={this.props[model].teamLeadName}
           description={teamLeadNameDescription}
+          emptyResultsMessage={emptyResultsMessage}
+          handleSearchChange={this.handleSearchChange}
           htmlFor="teamLeadName"
           id="teamLeadName"
+          inputValue={this.state.inputValue}
+          items={this.state.users}
           label="Team lead name"
           maxLength={100}
+          minimumSearchChars={minimumSearchChars}
           model={`${model}.teamLeadName`}
           name="teamLeadName"
           placeholder=""
+          resultIsEmpty={this.state.users.length < 1}
+          resultListItems={teamMemberListItems}
           showSearchButton={false}
           validators={{}}
         />
@@ -57,6 +118,7 @@ export class TeamLeadsStage extends Component {
 }
 
 TeamLeadsStage.defaultProps = {
+  minimumSearchChars: 2,
   onSubmit: () => {},
   onSubmitFailed: () => {}
 }
@@ -65,4 +127,8 @@ const mapStateToProps = (state, props) => ({
   ...formProps(state, props.model)
 })
 
-export default connect(mapStateToProps)(TeamLeadsStage)
+const mapDispatchToProps = dispatch => ({
+  findTeamMember: keyword => dispatch(findTeamMember(keyword))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeamLeadsStage)
