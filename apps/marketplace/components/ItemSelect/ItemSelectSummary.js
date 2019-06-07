88ec1056a1eff53aae7d5fb5ac77b62e33fd4cc3
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-07 15:51:43 +1000

    [Teams] Add skeleton ItemSelectSummary component

---
+++ apps/marketplace/components/ItemSelect/ItemSelectSummary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import ItemSelect from './ItemSelect';
+
+const ItemSelectSummary = () => (
+  <div>
+    ItemSelectSummary
+  </div>
+)
+
+export default ItemSelectSummary
