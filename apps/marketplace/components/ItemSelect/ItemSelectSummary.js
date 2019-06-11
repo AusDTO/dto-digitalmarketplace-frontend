8dc2e65344283d34e66442bcbd3f07a18d52bf00
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-11 13:12:06 +1000

    [Teams] Add margin to top of item select summary

--- apps/marketplace/components/ItemSelect/ItemSelectSummary.js
+++ apps/marketplace/components/ItemSelect/ItemSelectSummary.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import AUheading from '@gov.au/headings/lib/js/react.js'
 
+import styles from './ItemSelectSummary.scss'
+
 const ItemSelectSummary = props => {
   const { selectedItems, summaryHeading } = props
   const sortedKeys = Object.keys(selectedItems).sort((a, b) => (selectedItems[a] > selectedItems[b] ? 1 : -1))
 
   return (
-    <div>
+    <div className={styles.itemSelectSummary}>
       <AUheading level="2" size="sm">
         {summaryHeading}
       </AUheading>
