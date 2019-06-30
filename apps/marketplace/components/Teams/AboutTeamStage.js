56e5dae3b7119a45b3cd4cf42a7ca81b3f3252d4
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-30 15:41:39 +1000

    [Teams] Destructure props for About stage

--- apps/marketplace/components/Teams/AboutTeamStage.js
+++ apps/marketplace/components/Teams/AboutTeamStage.js
@@ -6,38 +6,42 @@ import AUheading from '@gov.au/headings'
 import formProps from 'shared/form/formPropsSelector'
 import Textfield from 'shared/form/Textfield'
 
-const AboutTeamStage = props => (
-  <Form model={props.model} onSubmit={props.onSubmit} onSubmitFailed={props.onSubmitFailed}>
-    <AUheading level="1" size="xl">
-      Name and email
-    </AUheading>
-    <Textfield
-      defaultValue={props[props.model].name}
-      description=""
-      htmlFor="name"
-      id="name"
-      label="Team name"
-      maxLength={100}
-      model={`${props.model}.name`}
-      name="name"
-      placeholder=""
-      validators={{}}
-    />
-    <Textfield
-      defaultValue={props[props.model].emailAddress}
-      description="Receive emails about opportunities created by any team member"
-      htmlFor="emailAddress"
-      id="emailAddress"
-      label="Team email address (optional)"
-      maxLength={100}
-      model={`${props.model}.emailAddress`}
-      name="emailAddress"
-      placeholder=""
-      validators={{}}
-    />
-    {props.formButtons}
-  </Form>
-)
+const AboutTeamStage = props => {
+  const { formButtons, model, onSubmit, onSubmitFailed } = props
+
+  return (
+    <Form model={model} onSubmit={onSubmit} onSubmitFailed={onSubmitFailed}>
+      <AUheading level="1" size="xl">
+        Name and email
+      </AUheading>
+      <Textfield
+        defaultValue={props[model].name}
+        description=""
+        htmlFor="name"
+        id="name"
+        label="Team name"
+        maxLength={100}
+        model={`${model}.name`}
+        name="name"
+        placeholder=""
+        validators={{}}
+      />
+      <Textfield
+        defaultValue={props[model].emailAddress}
+        description="Receive emails about opportunities created by any team member"
+        htmlFor="emailAddress"
+        id="emailAddress"
+        label="Team email address (optional)"
+        maxLength={100}
+        model={`${model}.emailAddress`}
+        name="emailAddress"
+        placeholder=""
+        validators={{}}
+      />
+      {formButtons}
+    </Form>
+  )
+}
 
 AboutTeamStage.defaultProps = {
   onSubmit: () => {},
