1f87cb9568410b608c66044bfa372e14f3d33a81
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-30 16:54:08 +1000

    [Teams] Destructure props for Review stage

--- apps/marketplace/components/Teams/ReviewStage.js
+++ apps/marketplace/components/Teams/ReviewStage.js
@@ -8,28 +8,32 @@ import TeamTable from './TeamTable'
 
 import commonStyles from './TeamStages.scss'
 
-const ReviewStage = props => (
-  <Form model={props.model} onSubmit={props.onSubmit} onSubmitFailed={props.onSubmitFailed}>
-    <AUheading level="1" size="xl">
-      Review
-    </AUheading>
-    <AUheading level="2" size="lg">
-      Golden State Warriors
-    </AUheading>
-    <TeamTable teamLeads={props[props.model].teamLeads} teamMembers={props[props.model].teamMembers} />
-    <AUheading level="2" size="lg">
-      What happens next
-    </AUheading>
-    <div className={commonStyles.stageContentContainer}>
-      <ul className={commonStyles.stageList}>
-        <li>We will email each member to sign in with the email you have provided.</li>
-        <li>Members will see any current opportunities created by other team members.</li>
-        <li>Members will see all new opportunities created by other team members.</li>
-      </ul>
-    </div>
-    {props.formButtons}
-  </Form>
-)
+const ReviewStage = props => {
+  const { formButtons, model, onSubmit, onSubmitFailed } = props
+
+  return (
+    <Form model={model} onSubmit={onSubmit} onSubmitFailed={onSubmitFailed}>
+      <AUheading level="1" size="xl">
+        Review
+      </AUheading>
+      <AUheading level="2" size="lg">
+        Golden State Warriors
+      </AUheading>
+      <TeamTable teamLeads={props[model].teamLeads} teamMembers={props[model].teamMembers} />
+      <AUheading level="2" size="lg">
+        What happens next
+      </AUheading>
+      <div className={commonStyles.stageContentContainer}>
+        <ul className={commonStyles.stageList}>
+          <li>We will email each member to sign in with the email you have provided.</li>
+          <li>Members will see any current opportunities created by other team members.</li>
+          <li>Members will see all new opportunities created by other team members.</li>
+        </ul>
+      </div>
+      {formButtons}
+    </Form>
+  )
+}
 
 ReviewStage.defaultProps = {
   onSubmit: () => {},
