73e48d8eb0e975a3a814e03d5f4fff59dda0fbe7
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-30 14:19:18 +1000

    [Teams] Sort team members by name in the TeamTable component

--- apps/marketplace/components/Teams/TeamTable.js
+++ apps/marketplace/components/Teams/TeamTable.js
@@ -3,23 +3,31 @@ import React from 'react'
 import commonStyles from './TeamStages.scss'
 import styles from './ReviewStage.scss'
 
-const TeamTable = props => (
-  <table className={`${styles.teamTable} ${commonStyles.stageTable}`}>
-    <thead>
-      <tr>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.teamMembers.map(teamMember => (
-        <tr key={teamMember.email}>
-          <td>{teamMember.name}</td>
-          <td>{teamMember.email}</td>
+const TeamTable = props => {
+  const { teamLeads, teamMembers } = props
+
+  const sortedTeamMemberKeys = Object.keys(teamMembers).sort(
+    (a, b) => (teamMembers[a].name > teamMembers[b].name ? 1 : -1)
+  )
+
+  return (
+    <table className={`${styles.teamTable} ${commonStyles.stageTable}`}>
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-)
+      </thead>
+      <tbody>
+        {sortedTeamMemberKeys.map(userId => (
+          <tr key={userId}>
+            <td>{teamMembers[userId].name}</td>
+            <td>{teamMembers[userId].emailAddress}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default TeamTable
