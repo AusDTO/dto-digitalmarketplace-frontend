69e8388f4f3452c646039aed3bd4185de444bbb8
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-07-10 19:18:12 +1000

    [Alerts] Focus cancel button on mount and update

--- apps/marketplace/components/Alerts/MarketplaceAlert.js
+++ apps/marketplace/components/Alerts/MarketplaceAlert.js
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import AUpageAlert from '@gov.au/page-alerts/lib/js/react.js'
 
 import styles from './MarketplaceAlert.scss'
 
-const MarketplaceAlert = props => {
-  const { actions, content, type } = props
+class MarketplaceAlert extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <AUpageAlert as={type}>
-      {content}
-      <div className={styles.actionsContainer}>{actions}</div>
-    </AUpageAlert>
-  )
+    this.actionRef = React.createRef()
+    this.alertRef = React.createRef()
+
+    this.focusAlert = this.focusAlert.bind(this)
+  }
+
+  componentDidMount = () => {
+    this.focusAlert()
+  }
+
+  componentDidUpdate = () => {
+    this.focusAlert()
+  }
+
+  focusAlert = () => {
+    window.scrollTo(0, this.alertRef.offsetTop)
+    this.actionRef.focus()
+  }
+
+  render() {
+    const { cancelButtonText, confirmButtonText, content, handleCancelClick, handleConfirmClick, type } = this.props
+
+    return (
+      <div
+        ref={ref => {
+          this.alertRef = ref
+        }}
+      >
+        <AUpageAlert as={type}>
+          {content}
+          <div className={styles.actionsContainer}>
+            <button className="au-btn" onClick={handleConfirmClick}>
+              {confirmButtonText}
+            </button>
+            <button
+              className="au-btn au-btn--secondary"
+              onClick={handleCancelClick}
+              ref={ref => {
+                this.actionRef = ref
+              }}
+            >
+              {cancelButtonText}
+            </button>
+          </div>
+        </AUpageAlert>
+      </div>
+    )
+  }
 }
 
 export default MarketplaceAlert
