cc907168112b05337fe30f95c1be0489f6dd5d03
Sam <29763502+malmas18@users.noreply.github.com>
2019-07-15 10:41:58 +1000

    hide when zero

--- apps/marketplace/components/Header/SellerHeaderActions.js
+++ apps/marketplace/components/Header/SellerHeaderActions.js
@@ -7,7 +7,7 @@ const SellerHeaderActions = props => {
     <ul data-reactroot="" id="main-navigation" className="au-marketplace-header-inline-links">
       <li>
         <a href="/2/seller-dashboard">Dashboard</a>
-        {notificationCount && notificationCount > 0 && <div className="notification">{notificationCount}</div>}
+        {notificationCount && notificationCount > 0 ? <div className="notification">{notificationCount}</div> : ''}
       </li>
       <li>
         <a href="/logout">Sign out</a>
