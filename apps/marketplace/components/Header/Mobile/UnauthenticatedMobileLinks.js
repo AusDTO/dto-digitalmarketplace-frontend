e195e9f0e80c94ed462a11cc31422d23beee1108
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-07-30 08:40:41 +1000

    [Header] Switch order of Sign in and Sign up links

--- apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
+++ apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
@@ -5,10 +5,10 @@ import CommonMobileLinks from './CommonMobileLinks'
 const UnauthenticatedMobileLinks = () => (
   <React.Fragment>
     <div className="au-marketplace-header_mobile-link">
-      <a href="/2/signup">Sign up</a>
+      <a href="/login">Sign in</a>
     </div>
     <div className="au-marketplace-header_mobile-link">
-      <a href="/login">Sign in</a>
+      <a href="/2/signup">Sign up</a>
     </div>
     <CommonMobileLinks />
   </React.Fragment>
