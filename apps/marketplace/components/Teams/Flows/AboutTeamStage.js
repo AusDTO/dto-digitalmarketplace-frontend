7ffccd09a8ee756f64bdcd42e786402014c6a54c
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-07-31 12:49:28 +1000

    [Teams] Prefix email domain with @

--- apps/marketplace/components/Teams/Flows/AboutTeamStage.js
+++ apps/marketplace/components/Teams/Flows/AboutTeamStage.js
@@ -14,7 +14,7 @@ const AboutTeamStage = props => {
   const userDomain = currentUserEmailAddress.split('@')[1]
 
   const governmentEmail = team => validGovernmentEmail(team.emailAddress, currentUserEmailAddress)
-  const governmentEmailMessage = `You must use an email address ending in ${userDomain}`
+  const governmentEmailMessage = `You must use an email address ending in @${userDomain}`
 
   const requiredName = team => required(team.name)
   const requiredNameMessage = 'A team name is required'
