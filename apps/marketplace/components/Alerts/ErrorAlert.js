f928bf55a029666a6fb3ef778e448493c8b43a71
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-09-03 15:45:36 +1000

    [Alerts] Focus page alert when form errors exist

--- apps/marketplace/components/Alerts/ErrorAlert.js
+++ apps/marketplace/components/Alerts/ErrorAlert.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Errors } from 'react-redux-form'
-import AUpageAlert from '@gov.au/page-alerts/lib/js/react.js'
-import AUheadings from '@gov.au/headings/lib/js/react.js'
-import styles from './ErrorAlert.scss'
+
+import PageAlertError from './PageAlertError'
 
 const ErrorAlert = props => (
   <Errors
@@ -11,20 +10,7 @@ const ErrorAlert = props => (
     show={field => field.touched && !field.focus}
     messages={props.messages}
     component="li"
-    wrapper={({ children }) => {
-      if (!children.length) {
-        return null
-      }
-
-      return (
-        <AUpageAlert as="error" className={styles.container}>
-          <AUheadings level="2" size="lg">
-            {props.title}
-          </AUheadings>
-          <ul>{children}</ul>
-        </AUpageAlert>
-      )
-    }}
+    wrapper={PageAlertError}
   />
 )
 
@@ -33,7 +19,6 @@ ErrorAlert.defaultProps = {
 }
 
 ErrorAlert.propTypes = {
-  title: PropTypes.string.isRequired,
   model: PropTypes.string.isRequired,
   messages: PropTypes.object
 }
