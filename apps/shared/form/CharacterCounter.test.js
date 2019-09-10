0e825894c13adb0fa4f024f2de552c071c4fae08
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-09-11 09:28:28 +1000

    [CharacterCounter] Test component renders limit when value is null

--- apps/shared/form/CharacterCounter.test.js
+++ apps/shared/form/CharacterCounter.test.js
@@ -6,6 +6,11 @@ import CharacterCounter from './CharacterCounter'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('CharacterCounter', () => {
+  test('shows the limit when value is null', () => {
+    const wrapper = mount(<CharacterCounter limit={10} value={null} />)
+    expect(wrapper.text()).toEqual('10 characters left')
+  })
+
   test('shows the limit when value is empty', () => {
     const wrapper = mount(<CharacterCounter limit={10} value={''} />)
     expect(wrapper.text()).toEqual('10 characters left')
