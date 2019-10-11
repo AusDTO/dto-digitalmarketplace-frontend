06c4d4df8db0ebaae11e3c1cbd4b5a5e1f9a1336
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-10-11 16:18:48 +1100

    linting

--- apps/marketplace/pages/InsightPage.js
+++ apps/marketplace/pages/InsightPage.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import DocumentTitle from 'react-document-title'
 import AUheading from '@gov.au/headings/lib/js/react.js'
-import 'chart.js/dist/Chart.bundle.min.js'
-import 'chart.js/dist/Chart.min.css'
+import Chart from 'chart.js'
+import 'chartjs-plugin-labels'
 import { loadInsights } from 'marketplace/actions/insightActions'
 import LoadingIndicatorFullPage from 'shared/LoadingIndicatorFullPage/LoadingIndicatorFullPage'
 import WhoIsBuying from '../components/Insights/WhoIsBuying'
@@ -18,8 +18,6 @@ import NumberOfSellersPerCategory from '../components/Insights/NumberOfSellersPe
 import ResponsesPerOpportunity from '../components/Insights/ResponsesPerOpportunity'
 import TopSellers from '../components/Insights/TopSellers'
 import AustenderFigures from '../components/Insights/AustenderFigures'
-import Chart from 'chart.js'
-import chartjsPluginLabels from 'chartjs-plugin-labels'
 
 import styles from '../main.scss'
 
@@ -128,8 +126,9 @@ class InsightPage extends Component {
 
           <div className={`row ${styles['margin-bottom-5x']}`}>
             <div className="col-xs-12">
-              * Contract information is sourced from AusTender. It excludes contracts awarded by entities that don't
-              report through AusTender and contracts under $10,000. Contracts may take up to 42 days to be published.
+              * Contract information is sourced from AusTender. It excludes contracts awarded by entities that
+              don&apos;t report through AusTender and contracts under $10,000. Contracts may take up to 42 days to be
+              published.
             </div>
           </div>
         </div>
