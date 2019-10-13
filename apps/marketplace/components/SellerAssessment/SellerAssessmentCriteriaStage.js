3132d149365093d2ddff3fd32fd4883fad8b36e9
Reshma Abraham <reshmamabraham@gmail.com>
2019-10-14 10:14:10 +1100

    run prettier

--- apps/marketplace/components/SellerAssessment/SellerAssessmentCriteriaStage.js
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentCriteriaStage.js
@@ -111,9 +111,7 @@ class SellerAssessmentCriteriaStage extends Component {
           }}
         />
         <p>
-          <strong>
-            For this assessment, you must submit at least {criteriaNeeded} criteria.
-          </strong>
+          <strong>For this assessment, you must submit at least {criteriaNeeded} criteria.</strong>
         </p>
         <div className={styles.criteria}>
           {domain.criteria.map(criteria => (
