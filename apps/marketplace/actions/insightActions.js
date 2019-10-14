401300f58e876a489d0c68f8fc8dabe97a7c15c2
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-10-14 16:18:53 +1100

    update url

--- apps/marketplace/actions/insightActions.js
+++ apps/marketplace/actions/insightActions.js
@@ -1,5 +1,5 @@
 import dmapi from '../services/apiClient'
 
-export const loadInsights = () => () => dmapi({ url: `/insights` })
+export const loadInsights = () => () => dmapi({ url: `/insight` })
 
 export default loadInsights
