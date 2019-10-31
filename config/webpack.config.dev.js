7dec5b0331e8eddbe0ecae2fbdb35a2400e68475
Reshma Abraham <reshmamabraham@gmail.com>
2019-10-31 11:48:56 +1100

    added the right regex

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -73,7 +73,7 @@ var loaders = [
       // When you `import` an asset, you get its (virtual) filename.
       // In production, they would get copied to the `build` folder.
       {
-        test: /external-link--aqua\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+        test: /external-link--aqua\.svg|\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
         loader: 'file',
         query: {
           name: 'static/media/[name].[ext]'
