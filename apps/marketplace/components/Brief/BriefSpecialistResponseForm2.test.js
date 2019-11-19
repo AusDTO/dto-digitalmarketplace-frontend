1931ee9384ee0bb653bbc043dbf0ccd2dd972886
Nathan Davison <nathan.davison@digital.gov.au>
2019-11-19 16:17:26 +1100

    Linting

--- apps/marketplace/components/Brief/BriefSpecialistResponseForm2.test.js
+++ apps/marketplace/components/Brief/BriefSpecialistResponseForm2.test.js
@@ -142,7 +142,7 @@ describe('BriefSpecialistResponseForm2', () => {
           specialistNumber={1}
           app={{ supplierCode: 1 }}
           brief={{ numberOfSuppliers: '3' }}
-          briefResponseStatus='submitted'
+          briefResponseStatus="submitted"
           briefResponseForm={{
             attachedDocumentURL: [],
             availability: '',
@@ -199,7 +199,7 @@ describe('BriefSpecialistResponseForm2', () => {
           specialistNumber={3}
           app={{ supplierCode: 1 }}
           brief={{ numberOfSuppliers: '3' }}
-          briefResponseStatus='submitted'
+          briefResponseStatus="submitted"
           briefResponseForm={{
             attachedDocumentURL: [],
             availability: '',
