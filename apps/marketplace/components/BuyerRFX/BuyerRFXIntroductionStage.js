07ece40343f04db471b2491c9ca1120c2f20cf79
Reshma Abraham <reshmamabraham@gmail.com>
2019-11-21 08:51:44 +1100

    ran prettier

--- apps/marketplace/components/BuyerRFX/BuyerRFXIntroductionStage.js
+++ apps/marketplace/components/BuyerRFX/BuyerRFXIntroductionStage.js
@@ -21,7 +21,7 @@ const BuyerRFXIntroductionStage = props => (
       </li>
       <li>
         Any{' '}
-         <a href="/search/sellers" rel="noopener noreferrer" target="_blank">
+        <a href="/search/sellers" rel="noopener noreferrer" target="_blank">
           sellers
         </a>{' '}
         you invite must be approved in the{' '}
