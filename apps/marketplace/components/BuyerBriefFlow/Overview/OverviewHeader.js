7c60079575424a07a3a50913e91b8a03f68f200d
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-12-05 10:20:16 +1100

    [Overview] Add default props and prop types for the header

--- apps/marketplace/components/BuyerBriefFlow/Overview/OverviewHeader.js
+++ apps/marketplace/components/BuyerBriefFlow/Overview/OverviewHeader.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import AUheading from '@gov.au/headings/lib/js/react.js'
 
@@ -53,6 +54,33 @@ const OverviewHeader = props => {
       </div>
     </div>
   )
+}
+
+OverviewHeader.defaultProps = {
+  brief: {},
+  canCloseOpportunity: null,
+  flowName: '',
+  handleDeleteClick: () => {},
+  isClosed: null,
+  isPublished: null,
+  isPartOfTeam: null,
+  isTeamLead: null,
+  teams: []
+}
+
+OverviewHeader.propTypes = {
+  brief: PropTypes.shape({
+    internalReference: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  }).isRequired,
+  canCloseOpportunity: PropTypes.bool.isRequired,
+  flowName: PropTypes.string.isRequired,
+  handleDeleteClick: PropTypes.func.isRequired,
+  isClosed: PropTypes.bool.isRequired,
+  isPublished: PropTypes.bool,
+  isPartOfTeam: PropTypes.bool.isRequired,
+  isTeamLead: PropTypes.bool.isRequired,
+  teams: PropTypes.array.isRequired
 }
 
 export default OverviewHeader
