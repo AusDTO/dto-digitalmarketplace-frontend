5db97ac7d6c9d04a4d4c7070d8e7304e40e6ec88
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-12-12 10:16:41 +1100

    [EditOpportunity] Wrap heading and contact us in a div

--- apps/marketplace/components/Brief/EditOpportunity.js
+++ apps/marketplace/components/Brief/EditOpportunity.js
@@ -6,12 +6,14 @@ import styles from '../../main.scss'
 
 const EditOpportunity = props => (
   <div>
-    <AUheading level="1" size="xl">
-      Edit live opportunity
-    </AUheading>
-    <p className={styles.fontSizeMd}>
-      If you&apos;re having issues making the changes you need, <a href="/contact-us">contact us</a>.
-    </p>
+    <div className="row">
+      <AUheading level="1" size="xl">
+        Edit live opportunity
+      </AUheading>
+      <p className={styles.fontSizeMd}>
+        If you&apos;re having issues making the changes you need, <a href="/contact-us">contact us</a>.
+      </p>
+    </div>
   </div>
 )
 
