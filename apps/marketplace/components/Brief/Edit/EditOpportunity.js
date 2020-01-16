90c2fa3139ad5ea3f61c9b0f3e02df3165fd7237
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-01-17 10:35:04 +1100

    [EditOpportunity] Add check box for user to confirm they understand the edit process

--- apps/marketplace/components/Brief/Edit/EditOpportunity.js
+++ apps/marketplace/components/Brief/Edit/EditOpportunity.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 import AUbutton from '@gov.au/buttons/lib/js/react.js'
+import { AUcheckbox } from '@gov.au/control-input/lib/js/react.js'
 import AUheading from '@gov.au/headings/lib/js/react.js'
+
 import { rootPath } from 'marketplace/routes'
 import EditOpportunityTable from './EditOpportunityTable'
 
@@ -22,6 +24,20 @@ const EditOpportunity = props => {
       </div>
       <div className="row">
         <EditOpportunityTable brief={brief} edits={edits} isOpenToAll={isOpenToAll} location={location} />
+      </div>
+      <div className="row">
+        <AUcheckbox
+          className={`${styles.marginTop2}`}
+          id="understandsEditProcess"
+          label={
+            isOpenToAll
+              ? 'I understand if I submit these changes, sellers will be able to view previous versions of my updates.'
+              : 'I understand if I submit these changes, invited sellers will be notified and can view previous versions of my updates.'
+          }
+          name="understandsEditProcess"
+          onChange={() => {}}
+          onClick={() => {}}
+        />
       </div>
       <div className={`row ${styles.marginTop2}`}>
         <AUbutton>Submit changes</AUbutton>
