6dcb9f67b5bae2131f19eab1c64bade4819a268a
Reshma Abraham <reshmamabraham@gmail.com>
2020-01-16 13:57:51 +1100

    placing candidate includes candidate pool

--- apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentHybridPlacingCandidatesStage.js
+++ apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentHybridPlacingCandidatesStage.js
@@ -3,48 +3,30 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { actions, Form } from 'react-redux-form'
 import formProps from 'shared/form/formPropsSelector'
-import AUheading from '@gov.au/headings/lib/js/react.js'
 import RadioList from 'shared/form/RadioList'
 import ErrorAlert from 'marketplace/components/Alerts/ErrorAlert'
+import Textfield from 'shared/form/Textfield'
+import AUheadings from '@gov.au/headings/lib/js/react.js'
 
-export class SellerAssessmentHybridPlacingCandidatesStage extends Component {
-  constructor(props) {
-    super(props)
-  }
 
-  render() {
-    const categories = [
-      {
-        value: '',
-        text: 'Select category'
-      }
-    ]
-    this.props.domains.map(domain => {
-      categories.push({
-        value: domain.id,
-        text: domain.name
-      })
-      return true
-    })
-
-    return (
+const SellerAssessmentHybridPlacingCandidatesStage = props => (
       <Form
-        model={this.props.model}
+        model={props.model}
         validators={{
           '': {
             requiredChoice: formValues => formValues.placingCandidates
           }
         }}
-        onSubmit={this.props.onSubmit}
-        onSubmitFailed={this.props.onSubmitFailed}
+        onSubmit={props.onSubmit}
+        onSubmitFailed={props.onSubmitFailed}
         validateOn="submit"
       >
-        <AUheading level="1" size="xl">
+        <AUheadings level="1" size="xl">
           Placing candidates
-        </AUheading>
+        </AUheadings>
         <p> Your business will be placing candidates for INSERT category roles by submitting</p>
         <ErrorAlert
-          model={this.props.model}
+          model={props.model}
           messages={{
             requiredChoice: 'You must select who can respond'
           }}
@@ -54,7 +36,7 @@ export class SellerAssessmentHybridPlacingCandidatesStage extends Component {
             id="placingCandidates"
             label=""
             name="placingCandidates"
-            model={`${this.props.model}.placingCandidates`}
+            model={`${props.model}.placingCandidates`}
             options={[
               {
                 label: 'contractors you organised through recruitment activities',
@@ -72,12 +54,39 @@ export class SellerAssessmentHybridPlacingCandidatesStage extends Component {
             messages={{}}
           />
         </div>
-        {this.props.formButtons}
+
+        <div> 
+        <AUheadings level="1" size="xl">
+      Candidate Pool
+    </AUheadings>
+
+    <ErrorAlert
+      model={props.model}
+      messages={{
+        validWholeNumber: 'The size of your candidate database must be a whole number (e.g. 1200)'
+      }}
+    />
+    <Textfield
+      model={`${props.model}.database_size`}
+      label="What is the size of your candidate database?"
+      name="database_size"
+      id="database_size"
+      htmlFor="database_size"
+      defaultValue={props[props.model].database_size}
+    />
+    <Textfield
+      model={`${props.model}.placed_candidates`}
+      label={`How many candidates have you placed in ${props.meta.domain.name} roles in the last 12 months?`}
+      name="placed_candidates"
+      id="placed_candidates"
+      htmlFor="placed_candidates"
+      defaultValue={props[props.model].placed_candidates}
+    />
+        </div>
+        {props.formButtons}
       </Form>
     )
-  }
-}
-
+  
 SellerAssessmentHybridPlacingCandidatesStage.defaultProps = {
   onSubmit: () => {},
   onSubmitFailed: () => {}
@@ -86,6 +95,7 @@ SellerAssessmentHybridPlacingCandidatesStage.defaultProps = {
 SellerAssessmentHybridPlacingCandidatesStage.propTypes = {
   model: PropTypes.string.isRequired,
   formButtons: PropTypes.node.isRequired,
+  meta: PropTypes.object.isRequired,
   onSubmit: PropTypes.func,
   onSubmitFailed: PropTypes.func
 }
@@ -98,66 +108,3 @@ const mapStateToProps = (state, props) => ({
 export default connect(
   mapStateToProps
 )(SellerAssessmentHybridPlacingCandidatesStage)
-
-// export const done = formValues =>
-//   formValues.placingCandidatesRadio
-
-// const SellerAssessmentCandidatePool = props => (
-//   <Form
-//     model={props.model}
-//     onSubmit={props.onSubmit}
-//     onSubmitFailed={props.onSubmitFailed}
-//     validateOn="submit"
-//   >
-//     <AUheadings level="1" size="xl">
-//       Placing candidates
-//     </AUheadings>
-//     <p>
-//     Your business will be placing candidates for  {props.meta.domain.name} roles by submitting:
-//     </p>
-  
-//   <AUradio
-//     model={`${props.model}.placingCandidatesRadio`}
-//     label="contractors you organised through recruitment activies" 
-//     name="radio-ex" 
-//     id="radio-recrutierOnly-block" 
-//     block defaultChecked 
-//     />
-
-//   <AUradio
-//     model={`${props.model}.placingCandidatesRadio`}
-//     label="your own consultants" 
-//     name="radio-ex" 
-//     id="radio-consultantOnly-block" 
-//     block
-//   />
-
-//   <AUradio
-//     model={`${props.model}.placingCandidatesRadio`}
-//     label="both contractors and consultants" 
-//     name="radio-ex" 
-//     id="radio-Hybrid-block" 
-//     block 
-//   />
-//     {props.formButtons}
-//   </Form>
-// )
-
-// SellerAssessmentCandidatePool.defaultProps = {
-//   onSubmit: () => {},
-//   onSubmitFailed: () => {}
-// }
-
-// SellerAssessmentCandidatePool.propTypes = {
-//   model: PropTypes.string.isRequired,
-//   formButtons: PropTypes.node.isRequired,
-//   meta: PropTypes.object.isRequired,
-//   onSubmit: PropTypes.func,
-//   onSubmitFailed: PropTypes.func
-// }
-
-// const mapStateToProps = (state, props) => ({
-//   ...formProps(state, props.model)
-// })
-
-// export default connect(mapStateToProps)(SellerAssessmentCandidatePool)
