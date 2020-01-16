53bc9d95b1595e53caceb133a1d5649948f855d0
Reshma Abraham <reshmamabraham@gmail.com>
2020-01-16 15:24:05 +1100

    content and styling changes made for intro page

--- apps/marketplace/components/SellerAssessment/Recruiter/SellerAssessmentIntroductionStage.js
+++ apps/marketplace/components/SellerAssessment/Recruiter/SellerAssessmentIntroductionStage.js
@@ -9,10 +9,12 @@ const SellerAssessmentIntroductionStage = props => (
       {props.meta.domain.name} assessment
     </AUheadings>
     <p>
-    Before you can respond to opportunities in this category, you must be
-    <br/> 
-    <a href={`https://marketplace1.zendesk.com/hc/en-gb/articles/115011292847-Request-an-assessment-for-categories`}> assessed by the Digital Marketplace in offering value for money. </a>
-    The information you submit will only be visible to the assessment team.
+    Before you can respond to opportunities in this category, you must be assessed by the Digital Marketplace in offering value for money.
+    Details of your request will only be visible to the assessment team.
+    <br/>
+    <br/>
+      Read more on how to 
+    <a href={`https://marketplace1.zendesk.com/hc/en-gb/articles/115011292847-Request-an-assessment-for-categories`}> request an assessment for categories</a>
     </p>
 
     <p>
@@ -22,32 +24,7 @@ const SellerAssessmentIntroductionStage = props => (
       <strong>Please note:</strong> The time we take to assess your criteria is dependent on volume, seasonal peaks,
       complex cases and incomplete submissions. We will endeavour to assess your criteria in a reasonable timeframe.
     </p>
-    {/* <AUheadings level="2" size="lg">
-      You will be asked for
-    </AUheadings>
-    <ul>
-      <li>
-        your maximum daily rate for your services in {props.meta.domain.name}. Use{' '}
-        <a
-          href="https://www.sfia-online.org/en/framework/sfia-7/busskills/level-5"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          SFIA level 5
-        </a>{' '}
-        and the{' '}
-        <a
-          href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000556476"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          panel categories and rates
-        </a>{' '}
-        as a guide.
-      </li>
-      <li>which criteria you are requesting assessment in.</li>
-      <li>evidence of your expertise in the criteria.</li>
-    </ul> */}
+   
     {props.formButtons}
   </Form>
 )
