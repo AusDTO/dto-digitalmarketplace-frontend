377a8768b4deeaef2bef9af38ee1d1413c4c8836
Reshma Abraham <reshmamabraham@gmail.com>
2020-01-20 13:38:03 +1100

    just for now ... javascript is being weird with the error messages

--- apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentHybridPlacingCandidatesStage.js
+++ apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentHybridPlacingCandidatesStage.js
@@ -13,6 +13,8 @@ export const greaterThanZero = formValues => parseInt(formValues.database_size, 
 
 export const validWholeNumber = formValues => formValues.database_size && /^[0-9]+$/.test(formValues.database_size)
 
+export const validWholeNumberPlacedCandidates = formValues => formValues.placed_candidates && /^[0-9]+$/.test(formValues.placed_candidates)
+
 export const done = formValues => {
   if (props[props.model].placingCandidates === 'recruitment' || props[props.model].placingCandidates === 'hybrid') {
     formValues.placingCandidates &&
@@ -28,8 +30,10 @@ const SellerAssessmentHybridPlacingCandidatesStage = props => (
     model={props.model}
     validators={{
       '': {
-        requiredChoice: formValues => formValues.placingCandidates ==='consultants' || formValues.placingCandidates ==='hybrid' || formValues.placingCandidates ==='recruitment'
-        // greaterThanZero: formValues => formValues.database_size
+        requiredChoice: formValues => formValues.placingCandidates ==='consultants' || formValues.placingCandidates ==='hybrid' || formValues.placingCandidates ==='recruitment',
+        greaterThanZero: formValues => formValues.database_size,
+        validWholeNumber: formValues => formValues.database_size,
+        validWholeNumberPlacedCandidates: formValues => formValues.placed_candidates
       }
     }}
     onSubmit={props.onSubmit}
@@ -43,8 +47,10 @@ const SellerAssessmentHybridPlacingCandidatesStage = props => (
       model={props.model}
       messages={{
         //it works only once.... to be investigated
-        requiredChoice: 'You must select who can respond'
-        // greaterThanZero: 'The size of your candidate database must be greater than zero'
+        requiredChoice: 'You must select who can respond',
+        greaterThanZero: 'The size of your candidate database must be greater than zero',
+        validWholeNumber: 'dfdgdf', 
+        validWholeNumberPlacedCandidates: 'dsfsdfsdfsdf',
       }}
     />
     <p> Your business will be placing candidates for {props.meta.domain.name} category roles by submitting</p>
