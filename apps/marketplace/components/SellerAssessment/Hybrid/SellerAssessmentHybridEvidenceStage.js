e3c6d60d96cf4934bcd484d9a1b1db737b3588df
Reshma Abraham <reshmamabraham@gmail.com>
2020-01-24 14:15:57 +1100

    commenting out the validators till I figure it out why domain is undefined + prettier

--- apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentHybridEvidenceStage.js
+++ apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentHybridEvidenceStage.js
@@ -83,8 +83,6 @@ export const requiredCandidateFullName = (formValues, meta) =>
   formValues.evidence &&
   Object.keys(formValues.evidence).length > 0 &&
   (formValues.criteria.length === 0 ||
-    // {this.props[this.props.model].criteria.map((criteriaId, index) => (
-    //   {isRecruiterCriteria(criteriaId, this.props.meta.domain.criteria) && (
     formValues.criteria.every(
       criteriaId =>
         formValues.evidence[criteriaId] &&
@@ -418,13 +416,12 @@ class SellerAssessmentHybridEvidenceStage extends Component {
                 {!isRecruiterCriteria(criteriaId, this.props.meta.domain.criteria) && (
                   <React.Fragment>
                     {/* //making it child */}
-                    <div className={styles.child}>
-                      <p className={styles.criteriaText}>
-                        {getCriteriaName(criteriaId, this.props.meta.domain.criteria)}
-                      </p>
-                      {index !== 0 && (
-                        <p>
-                          {/* <CheckboxDetailsField
+                    <p className={styles.criteriaText}>
+                      {getCriteriaName(criteriaId, this.props.meta.domain.criteria)}
+                    </p>
+                    {index !== 0 && (
+                      <p>
+                        {/* <CheckboxDetailsField
                             model={`${this.props.model}.evidence[${criteriaId}].sameAsFirst`}
                             id={`client_check_${criteriaId}`}
                             name={`client_check_${criteriaId}`}
@@ -435,168 +432,167 @@ class SellerAssessmentHybridEvidenceStage extends Component {
                             validators={{}}
                             messages={{}}
                           /> */}
-                        </p>
-                      )}
-                      <Textfield
-                        model={`${this.props.model}.evidence[${criteriaId}].client`}
-                        defaultValue={this.props[this.props.model].evidence[criteriaId].client}
-                        // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                        label="Client"
-                        name={`client_${criteriaId}`}
-                        id={`client_${criteriaId}`}
-                        htmlFor={`client_${criteriaId}`}
-                        maxLength={100}
-                        validators={{
-                          required
-                        }}
-                        onChange={e => {
-                          if (index === 0) {
-                            this.updateAllOtherCriteriaFromFirst('client', e.target.value)
-                          }
-                          return true
-                        }}
-                      />
-                      <Textfield
-                        model={`${this.props.model}.evidence[${criteriaId}].refereeName`}
-                        defaultValue={this.props[this.props.model].evidence[criteriaId].refereeName}
-                        // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                        label="Referee's full name"
-                        description="We may contact your referee to confirm your involvement in the project."
-                        name={`referee_name_${criteriaId}`}
-                        id={`referee_name_${criteriaId}`}
-                        htmlFor={`referee_name_${criteriaId}`}
-                        maxLength={100}
-                        validators={{
-                          required
-                        }}
-                        onChange={e => {
-                          if (index === 0) {
-                            this.updateAllOtherCriteriaFromFirst('refereeName', e.target.value)
-                          }
-                          return true
-                        }}
-                      />
-                      <Textfield
-                        model={`${this.props.model}.evidence[${criteriaId}].refereeNumber`}
-                        // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                        defaultValue={this.props[this.props.model].evidence[criteriaId].refereeNumber}
-                        label="Referee's phone number"
-                        description="Please include the area code for landlines."
-                        name={`referee_number_${criteriaId}`}
-                        id={`referee_number_${criteriaId}`}
-                        htmlFor={`referee_number_${criteriaId}`}
-                        maxLength={100}
-                        validators={{
-                          required,
-                          validPhoneNumber
-                        }}
-                        onChange={e => {
-                          if (index === 0) {
-                            this.updateAllOtherCriteriaFromFirst('refereeNumber', e.target.value)
-                          }
-                          return true
-                        }}
-                      />
+                      </p>
+                    )}
+                    <Textfield
+                      model={`${this.props.model}.evidence[${criteriaId}].client`}
+                      defaultValue={this.props[this.props.model].evidence[criteriaId].client}
+                      // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
+                      label="Client"
+                      name={`client_${criteriaId}`}
+                      id={`client_${criteriaId}`}
+                      htmlFor={`client_${criteriaId}`}
+                      maxLength={100}
+                      validators={{
+                        required
+                      }}
+                      onChange={e => {
+                        if (index === 0) {
+                          this.updateAllOtherCriteriaFromFirst('client', e.target.value)
+                        }
+                        return true
+                      }}
+                    />
+                    <Textfield
+                      model={`${this.props.model}.evidence[${criteriaId}].refereeName`}
+                      defaultValue={this.props[this.props.model].evidence[criteriaId].refereeName}
+                      // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
+                      label="Referee's full name"
+                      description="We may contact your referee to confirm your involvement in the project."
+                      name={`referee_name_${criteriaId}`}
+                      id={`referee_name_${criteriaId}`}
+                      htmlFor={`referee_name_${criteriaId}`}
+                      maxLength={100}
+                      validators={{
+                        required
+                      }}
+                      onChange={e => {
+                        if (index === 0) {
+                          this.updateAllOtherCriteriaFromFirst('refereeName', e.target.value)
+                        }
+                        return true
+                      }}
+                    />
+                    <Textfield
+                      model={`${this.props.model}.evidence[${criteriaId}].refereeNumber`}
+                      // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
+                      defaultValue={this.props[this.props.model].evidence[criteriaId].refereeNumber}
+                      label="Referee's phone number"
+                      description="Please include the area code for landlines."
+                      name={`referee_number_${criteriaId}`}
+                      id={`referee_number_${criteriaId}`}
+                      htmlFor={`referee_number_${criteriaId}`}
+                      maxLength={100}
+                      validators={{
+                        required,
+                        validPhoneNumber
+                      }}
+                      onChange={e => {
+                        if (index === 0) {
+                          this.updateAllOtherCriteriaFromFirst('refereeNumber', e.target.value)
+                        }
+                        return true
+                      }}
+                    />
 
-                      <div className={styles.evidenceDates}>
-                        <div>
-                          <strong>Start of project</strong>
-                          <Control.select
-                            model={`${this.props.model}.evidence[${criteriaId}].startDate`}
-                            // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                            id={`startDate_${criteriaId}`}
-                            name={`startDate_${criteriaId}`}
-                            mapProps={{
-                              className: 'au-select'
-                            }}
-                            onChange={e => {
-                              if (index === 0) {
-                                this.updateAllOtherCriteriaFromFirst('startDate', e.target.value)
-                              }
-                              return true
-                            }}
-                          >
-                            <option value="" key="0">
-                              Select start date
+                    <div className={styles.evidenceDates}>
+                      <div>
+                        <strong>Start of project</strong>
+                        <Control.select
+                          model={`${this.props.model}.evidence[${criteriaId}].startDate`}
+                          // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
+                          id={`startDate_${criteriaId}`}
+                          name={`startDate_${criteriaId}`}
+                          mapProps={{
+                            className: 'au-select'
+                          }}
+                          onChange={e => {
+                            if (index === 0) {
+                              this.updateAllOtherCriteriaFromFirst('startDate', e.target.value)
+                            }
+                            return true
+                          }}
+                        >
+                          <option value="" key="0">
+                            Select start date
+                          </option>
+                          {getYears().map(year => (
+                            <option value={year} key={year}>
+                              {year}
                             </option>
-                            {getYears().map(year => (
-                              <option value={year} key={year}>
-                                {year}
-                              </option>
-                            ))}
-                          </Control.select>
-                        </div>
-                        <div>
-                          <strong>End of project</strong>
-                          <Control.select
-                            model={`${this.props.model}.evidence[${criteriaId}].endDate`}
-                            // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                            id={`endDate_${criteriaId}`}
-                            name={`endDate_${criteriaId}`}
-                            mapProps={{
-                              className: 'au-select'
-                            }}
-                            onChange={e => {
-                              if (index === 0) {
-                                this.updateAllOtherCriteriaFromFirst('endDate', e.target.value)
-                              }
-                              return true
-                            }}
-                          >
-                            <option value="" key="0">
-                              Select end date
-                            </option>
-                            {getYears().map(year => (
-                              <option value={year} key={year}>
-                                {year}
-                              </option>
-                            ))}
-                            <option value="ongoing" key={getYears().length + 1}>
-                              ongoing
-                            </option>
-                          </Control.select>
-                        </div>
+                          ))}
+                        </Control.select>
                       </div>
-                      <Textarea
-                        model={`${this.props.model}.evidence[${criteriaId}].background`}
-                        // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                        label="Background"
-                        description="Describe the background of the project you worked on."
-                        name={`background_${criteriaId}`}
-                        id={`background_${criteriaId}`}
-                        htmlFor={`background_${criteriaId}`}
-                        validators={{
-                          required
-                        }}
-                        onCustomChange={value => {
-                          if (index === 0) {
-                            this.updateAllOtherCriteriaFromFirst('background', value)
-                          }
-                          return true
-                        }}
-                      />
-                      <p>Your evidence for the criteria should:</p>
-                      <ul>
-                        <li>Explain the activities you were specifically responsible for, what you did, and why.</li>
-                        <li>Avoid ambiguity e.g. &apos;we have extensive experience in...&apos;.</li>
-                        <li>Describe the result or outcome of your activities.</li>
-                      </ul>
-                      <Textarea
-                        key={criteriaId}
-                        model={`${this.props.model}.evidence[${criteriaId}].response`}
-                        label={`Evidence for '${getCriteriaName(criteriaId, this.props.meta.domain.criteria)}'`}
-                        name={`criteria_${criteriaId}`}
-                        id={`criteria_${criteriaId}`}
-                        htmlFor={`criteria_${criteriaId}`}
-                        controlProps={{ minimum: minimumWordRequirement, rows: '10' }}
-                        validators={{
-                          required
-                        }}
-                        messages={{
-                          minimumWords: `Your criteria response has not yet reached the ${minimumWordRequirement} word minimum requirement`
-                        }}
-                      />
+                      <div>
+                        <strong>End of project</strong>
+                        <Control.select
+                          model={`${this.props.model}.evidence[${criteriaId}].endDate`}
+                          // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
+                          id={`endDate_${criteriaId}`}
+                          name={`endDate_${criteriaId}`}
+                          mapProps={{
+                            className: 'au-select'
+                          }}
+                          onChange={e => {
+                            if (index === 0) {
+                              this.updateAllOtherCriteriaFromFirst('endDate', e.target.value)
+                            }
+                            return true
+                          }}
+                        >
+                          <option value="" key="0">
+                            Select end date
+                          </option>
+                          {getYears().map(year => (
+                            <option value={year} key={year}>
+                              {year}
+                            </option>
+                          ))}
+                          <option value="ongoing" key={getYears().length + 1}>
+                            ongoing
+                          </option>
+                        </Control.select>
+                      </div>
                     </div>
+                    <Textarea
+                      model={`${this.props.model}.evidence[${criteriaId}].background`}
+                      // disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
+                      label="Background"
+                      description="Describe the background of the project you worked on."
+                      name={`background_${criteriaId}`}
+                      id={`background_${criteriaId}`}
+                      htmlFor={`background_${criteriaId}`}
+                      validators={{
+                        required
+                      }}
+                      onCustomChange={value => {
+                        if (index === 0) {
+                          this.updateAllOtherCriteriaFromFirst('background', value)
+                        }
+                        return true
+                      }}
+                    />
+                    <p>Your evidence for the criteria should:</p>
+                    <ul>
+                      <li>Explain the activities you were specifically responsible for, what you did, and why.</li>
+                      <li>Avoid ambiguity e.g. &apos;we have extensive experience in...&apos;.</li>
+                      <li>Describe the result or outcome of your activities.</li>
+                    </ul>
+                    <Textarea
+                      key={criteriaId}
+                      model={`${this.props.model}.evidence[${criteriaId}].response`}
+                      label={`Evidence for '${getCriteriaName(criteriaId, this.props.meta.domain.criteria)}'`}
+                      name={`criteria_${criteriaId}`}
+                      id={`criteria_${criteriaId}`}
+                      htmlFor={`criteria_${criteriaId}`}
+                      controlProps={{ minimum: minimumWordRequirement, rows: '10' }}
+                      validators={{
+                        required
+                      }}
+                      messages={{
+                        minimumWords: `Your criteria response has not yet reached the ${minimumWordRequirement} word minimum requirement`
+                      }}
+                    />
                   </React.Fragment>
                 )}
               </div>
