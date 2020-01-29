f2018c065ea22f964ce3b8e934c10deabfdc4762
Reshma Abraham <reshmamabraham@gmail.com>
2020-01-29 11:22:07 +1100

    adding this for now - seller rate changes

--- apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentMaximumRate.js
+++ apps/marketplace/components/SellerAssessment/Hybrid/SellerAssessmentMaximumRate.js
@@ -7,6 +7,7 @@ import formProps from 'shared/form/formPropsSelector'
 import ErrorAlert from 'marketplace/components/Alerts/ErrorAlert'
 import AUheadings from '@gov.au/headings/lib/js/react.js'
 import styles from '../Hybrid/SellerAssessmentMaximumRate.scss'
+import style from '../../Brief/BriefSpecialistResponseForm2.scss'
 
 const maxDailyRateLimit = 99999
 
@@ -20,9 +21,9 @@ export const validWholeNumberMarkup = formValues => formValues.markup && /^[0-9]
 
 const sellerRateChange = (maxDailyRate, markup) => {
   let totalMax = 0
-  const fixMaxDailyRate = parseInt(maxDailyRate, 10)
-  const fixMarkUp = parseInt(markup, 10)
-  totalMax = parseFloat(fixMaxDailyRate * (fixMarkUp / 100) + fixMaxDailyRate)
+  let fixMaxDailyRate = parseInt(maxDailyRate, 10)
+  let fixMarkUp = parseInt(markup, 10)
+  totalMax = fixMaxDailyRate * (fixMarkUp / 100) + fixMaxDailyRate
   return totalMax
 }
 
@@ -37,8 +38,16 @@ export const done = formValues =>
     greaterThanZeroMarkup(formValues) &&
     validWholeNumberMarkup(formValues) &&
     formValues.totalMaximumRate)
-
+  
+  
 const SellerAssessmentRateStage = props => (
+
+  //   const SellerAssessmentRateStage: {
+  //     ({  onRateChange2 }: 
+  //       {
+  //       onRateChange2: any;
+  //     })
+  // }
   <Form
     model={props.model}
     validators={{
@@ -73,31 +82,17 @@ const SellerAssessmentRateStage = props => (
         />
 
         <div className={styles.maxDailyRate} />
-        <h5> Maximum daily rate (excluding mark-up)</h5>
-        <p>
-          This rate must be based on a person who demonstrates skills equivalnet to
-          <a
-            href="https://www.sfia-online.org/en/framework/sfia-7/busskills/level-5"
-            target="_blank"
-            rel="external noopener noreferrer"
-          >
-            SFIA level 5
-          </a>
-        </p>
         <Textfield
           model={`${props.model}.maxDailyRate`}
           prefix={`$`}
           postfix={'including GST'}
-          // label="Maximum daily rate (excluding mark-up)"
-          // description={`This rate must be based on a person who demonstrates skills equivalnet to`}
-
-          // href="https://www.sfia-online.org/en/framework/sfia-7/busskills/level-5"
-          // target="_blank"
-          // rel="external noopener noreferrer"
+          label="Maximum daily rate (excluding mark-up)"
+          description={`This rate must be based on a person who demonstrates skills equivalnet to  `}
           name="maxDailyRate"
           id="maxDailyRate"
           htmlFor="maxDailyRate"
           defaultValue={props[props.model].maxDailyRate}
+          onChange={data => props.onRateChange2('totalMaximumRate', data.target.value, props[props.model].markup)}
           // onChange={sellerRateChange(maxDailyRate, markup)}
         />
         <Textfield
@@ -109,25 +104,22 @@ const SellerAssessmentRateStage = props => (
           id="markup"
           htmlFor="markup"
           defaultValue={props[props.model].markup}
-          // validators={{
-          //   required
-          // }}
-          // messages={{
-          //   required: 'Maximum mark-up is required'
-          // }}
+          onChange={data => props.onRateChange2('totalMaximumRate', props[props.model].maxDailyRate, data.target.value)}
         />
+       
+
+        <span>{sellerRateChange(props[props.model].maxDailyRate,props[props.model].markup)} </span>
         <Textfield
           model={`${props.model}.totalMaximumRate`}
-          label="Total maximum rate"
-          description={`The threshold for ${props.meta.domain.name} is $${props.meta.domain.priceMaximum}. If your total maximum rate is above this threshold, you will be asked to meet more criteria to prove you offer value for money.`}
           name="totalMaximumRate"
           id="totalMaximumRate"
-          htmlFor="totalMaximumRate"
-          defaultValue={
-            sellerRateChange(props[props.model].maxDailyRate, props[props.model].markup) ||
-            props[props.model].totalMaximumRate
-          }
+          defaultValue={props[props.model].totalMaximumRate}
+          htmlFor="dayRate"
+          label="Total Maximum Rate"
+          description={`The threshold for ${props.meta.domain.name} is $${props.meta.domain.priceMaximum}. If your total maximum rate is above this threshold, you will be asked to meet more criteria to prove you offer value for money.`}
+          className={style.readOnly}
           readOnly
+          // prefix={'$'}
         />
       </div>
     )}
@@ -162,7 +154,8 @@ const SellerAssessmentRateStage = props => (
 
 SellerAssessmentRateStage.defaultProps = {
   onSubmit: () => {},
-  onSubmitFailed: () => {}
+  onSubmitFailed: () => {},
+  onRateChange2: () => null
 }
 
 SellerAssessmentRateStage.propTypes = {
@@ -170,7 +163,8 @@ SellerAssessmentRateStage.propTypes = {
   formButtons: PropTypes.node.isRequired,
   meta: PropTypes.object.isRequired,
   onSubmit: PropTypes.func,
-  onSubmitFailed: PropTypes.func
+  onSubmitFailed: PropTypes.func,
+  onRateChange2: PropTypes.func
 }
 
 const mapStateToProps = (state, props) => ({
