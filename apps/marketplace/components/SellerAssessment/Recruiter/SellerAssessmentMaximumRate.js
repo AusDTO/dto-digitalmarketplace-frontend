f8ece38b5151697fb015fca19d5fde02c98c2335
Reshma Abraham <reshmamabraham@gmail.com>
2020-01-29 11:50:51 +1100

    fixing recruiter rate so it  changes in the form

--- apps/marketplace/components/SellerAssessment/Recruiter/SellerAssessmentMaximumRate.js
+++ apps/marketplace/components/SellerAssessment/Recruiter/SellerAssessmentMaximumRate.js
@@ -6,37 +6,34 @@ import Textfield from 'shared/form/Textfield'
 import formProps from 'shared/form/formPropsSelector'
 import ErrorAlert from 'marketplace/components/Alerts/ErrorAlert'
 import AUheadings from '@gov.au/headings/lib/js/react.js'
+import styles from '../Hybrid/SellerAssessmentMaximumRate.scss'
 
 const maxDailyRateLimit = 99999
 
 export const greaterThanZero = formValues => parseInt(formValues.maxDailyRate, 10) > 0
+export const greaterThanZeroMarkup = formValues => parseInt(formValues.markup, 10) > 0
 
 export const lessThanLimit = formValues => parseInt(formValues.maxDailyRate, 10) < maxDailyRateLimit
 
 export const validWholeNumber = formValues => formValues.maxDailyRate && /^[0-9]+$/.test(formValues.maxDailyRate)
+export const validWholeNumberMarkup = formValues => formValues.markup && /^[0-9]+$/.test(formValues.markup)
 
-// const isRecruiterCriteria = (criteriaId, criteria) => {
-//   let recruiter = false
-//   criteria.find(c => {
-//     if (c.id === parseInt(criteriaId, 10)) {
-//       recruiter = c.is_recruiter_criteria
-//       return true
-//     }
-//     return false
-//   })
-//   return recruiter
-// }
-
-// export const onRateChange = (field, value) => {
-//   let withGst = parseFloat(value * 1.1).toFixed(2)
-//   if (isNaN(withGst)) {
-//     withGst = ''
-//   }
-//   this.props.changeModel(`${this.props.model}.${field}`, `${withGst}`)
-// }
+const sellerRateChange = (maxDailyRate, markup) => {
+  let totalMax = 0
+  const fixMaxDailyRate = parseInt(maxDailyRate, 10)
+  const fixMarkUp = parseInt(markup, 10)
+  totalMax = fixMaxDailyRate * (fixMarkUp / 100) + fixMaxDailyRate
+  return totalMax
+}
 
 export const done = formValues =>
-  formValues.maxDailyRate && greaterThanZero(formValues) && lessThanLimit(formValues) && validWholeNumber(formValues)
+  formValues.maxDailyRate &&
+  greaterThanZero(formValues) &&
+  lessThanLimit(formValues) &&
+  validWholeNumber(formValues) &&
+  formValues.markup &&
+  greaterThanZeroMarkup(formValues) &&
+  validWholeNumberMarkup(formValues)
 
 const SellerAssessmentRateStage = props => (
   <Form
@@ -45,7 +42,9 @@ const SellerAssessmentRateStage = props => (
       '': {
         greaterThanZero,
         lessThanLimit,
-        validWholeNumber
+        validWholeNumber,
+        greaterThanZeroMarkup,
+        validWholeNumberMarkup
       }
     }}
     onSubmit={props.onSubmit}
@@ -56,71 +55,53 @@ const SellerAssessmentRateStage = props => (
       Maximum rate
     </AUheadings>
 
-    <ErrorAlert
-      model={props.model}
-      messages={{
-        greaterThanZero: 'The maximum daily rate must be greater than zero',
-        lessThanLimit: `The maximum daily rate must be lower than $${maxDailyRateLimit}`,
-        validWholeNumber: 'The maximum daily rate must be a whole number (e.g. 1200)'
-      }}
-    />
-    <Textfield
-      model={`${props.model}.maxDailyRate`}
-      prefix={`$`}
-      postfix={'including GST'}
-      label="Maximum daily rate (excluding mark-up)"
-      description={`This rate must be based on a person who demonstrates skills equivalnet to  `}
-      name="maxDailyRate"
-      id="maxDailyRate"
-      htmlFor="maxDailyRate"
-      defaultValue={props[props.model].maxDailyRate}
-      // validators={{
-      //   required
-      // }}
-      // messages={{
-      //   required: 'Maximum Daily Rate is required'
-      // }}
-    />
-    <Textfield
-      model={`${props.model}.markup`}
-      postfix={'%'}
-      label="Maximum mark-up"
-      description={`The percentage of on-costs to the day rate, including: your commission, workers compensation, payroll tax`}
-      name="markup"
-      id="markup"
-      htmlFor="markup"
-      defaultValue={props[props.model].markup}
-      // onChange={data => onRateChange('totalMaximumRate', data.target.value)}
-      // validators={{
-      //   required
-      // }}
-      // messages={{
-      //   required: 'Maximum mark-up is required'
-      // }}
-    />
-    <Textfield
-      model={`${props.model}.totalMaximumRate`}
-      label="Total maximum rate"
-      description={`The threshold for ${props.meta.domain.name} is $${props.meta.domain.priceMaximum}. If your total maximum rate is above this threshold, you will be asked to meet more criteria to prove you offer value for money.`}
-      name="totalMaximumRate"
-      id="totalMaximumRate"
-      htmlFor="totalMaximumRate"
-      defaultValue={Math.round(
-        parseInt(props[props.model].maxDailyRate, 10) * (parseInt(props[props.model].markup, 10) / 100) +
-          parseInt(props[props.model].maxDailyRate, 10)
-      )}
-      disabled
-      // need to add syles to this
-      // className={styles.readOnly}
-      // readOnly
-    />
+    <div>
+      <ErrorAlert
+        model={props.model}
+        messages={{
+          greaterThanZero: 'The maximum daily rate must be greater than zero',
+          lessThanLimit: `The maximum daily rate must be lower than $${maxDailyRateLimit}`,
+          validWholeNumber: 'The maximum daily rate must be a whole number (e.g. 1200)',
+          greaterThanZeroMarkup: 'The maximum mark-up must be greater than zero percent',
+          validWholeNumberMarkup: 'The maximum mark-up must be a whole number (e.g. 15)'
+        }}
+      />
 
+      <div className={styles.maxDailyRate} />
+      <Textfield
+        model={`${props.model}.maxDailyRate`}
+        prefix={`$`}
+        postfix={'including GST'}
+        label="Maximum daily rate (excluding mark-up)"
+        description={`This rate must be based on a person who demonstrates skills equivalnet to  `}
+        name="maxDailyRate"
+        id="maxDailyRate"
+        htmlFor="maxDailyRate"
+        defaultValue={props[props.model].maxDailyRate}
+      />
+      <Textfield
+        model={`${props.model}.markup`}
+        postfix={'%'}
+        label="Maximum mark-up"
+        description={`The percentage of on-costs to the day rate, including: your commission, workers compensation, payroll tax`}
+        name="markup"
+        id="markup"
+        htmlFor="markup"
+        defaultValue={props[props.model].markup}
+      />
+
+      <h5>Total Maximum Rate</h5>
+      <p>
+        The threshold for {props.meta.domain.name} is ${props.meta.domain.priceMaximum}. If your total maximum rate is
+        above this threshold, you will be asked to meet more criteria to prove you offer value for money.
+      </p>
+      <span>${sellerRateChange(props[props.model].maxDailyRate, props[props.model].markup)} </span>
+    </div>
     {props.formButtons}
   </Form>
 )
 
 SellerAssessmentRateStage.defaultProps = {
-  // onRateChange: () => null,
   onSubmit: () => {},
   onSubmitFailed: () => {}
 }
@@ -129,7 +110,6 @@ SellerAssessmentRateStage.propTypes = {
   model: PropTypes.string.isRequired,
   formButtons: PropTypes.node.isRequired,
   meta: PropTypes.object.isRequired,
-  // onRateChange: PropTypes.func,
   onSubmit: PropTypes.func,
   onSubmitFailed: PropTypes.func
 }
