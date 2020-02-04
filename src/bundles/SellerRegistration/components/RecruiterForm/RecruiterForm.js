e736adb1f3a4ad65ff89f30d158968950c9cf3b5
Nathan Davison <nathan.davison@digital.gov.au>
2020-02-04 12:35:24 +1100

    Changes the hire licence validation so number and expiry are linked for each state and removing a value from both returns the fields back to optional

--- src/bundles/SellerRegistration/components/RecruiterForm/RecruiterForm.js
+++ src/bundles/SellerRegistration/components/RecruiterForm/RecruiterForm.js
@@ -39,20 +39,6 @@ class RecruiterForm extends BaseForm {
         this.setState({loaded: true})
     }
 
-    validExpiryDate(v, state) {
-        const { model } = this.props;
-        
-        if (this.state.recruiter === 'no') {
-            return true
-        }
-        if (v) {
-            if (this.props[model].labourHire[state].licenceNumber) {
-                return validDate(v)
-            }
-        }
-        return true;
-    }
-
     onChangeState(e) {
         const { model, updateProperty } = this.props;
 
@@ -70,59 +56,53 @@ class RecruiterForm extends BaseForm {
         }
     }
 
-    requiredLicenceNumber(v, state) {
-        const { model, setValid } = this.props
-        const valid = (
-            this.props[model].recruiter !== 'no' &&
-            this.props[model].labourHire &&
-            this.props[model].labourHire[state] &&
-            this.props[model].labourHire[state].licenceNumber
-            ? true : false
+    validLicenceNumber(formValues, state) {
+        return (
+            formValues.labourHire &&
+            formValues.labourHire[state] &&
+            (
+                !formValues.labourHire[state].expiry ||
+                formValues.labourHire[state].expiry &&
+                formValues.labourHire[state].licenceNumber
+            )
         )
-        if (!v) {
-            if (valid) {
-                if (this.state.loaded) {
-                    setValid(`${model}.labourHire.${state}.licenceNumber`, {
-                        required: false
-                    })
-                }
-            }
-            return true
-        } else {
-            if (this.state.loaded) {
-                setValid(`${model}.labourHire.${state}.licenceNumber`, {
-                    required: valid
-                })
-            }
-        }
-        return true
     }
-    requiredExpiry(v, state) {
-        const { model, setValid } = this.props
-        const valid = (
-            this.props[model].recruiter !== 'no' &&
-            this.props[model].labourHire &&
-            this.props[model].labourHire[state] &&
-            this.props[model].labourHire[state].expiry
-            ? true : false
+
+    validLicenceExpiry(formValues, state) {
+        return (
+            formValues.labourHire &&
+            formValues.labourHire[state] &&
+            (
+                !formValues.labourHire[state].licenceNumber ||
+                formValues.labourHire[state].licenceNumber &&
+                formValues.labourHire[state].expiry &&
+                validDate(formValues.labourHire[state].expiry)
+            )
         )
-        if (!v) {
-            if (valid) {
-                if (this.state.loaded) {
-                    setValid(`${model}.labourHire.${state}.expiry`, {
-                        required: false
-                    })
-                }
-            }
+    }
+
+    generateLicenceNumberMessage(state) {
+        const messages = {}
+        const message = `Please enter your licence number for ${mapAustraliaState(state)}`
+        messages[`requiredLicenceNumber_${state}`] = message
+        return messages
+    }
+
+    generateLicenceExpiryMessage(state) {
+        const messages = {}
+        const message = `Please enter a valid expiry date for ${mapAustraliaState(state)} that is in the future`
+        messages[`requiredLicenceExpiry_${state}`] = message
+        return messages
+    }
+
+    generateLicenceMessages() {
+        let messages = {}
+        states.map(s => {
+            messages = { ...messages, ...this.generateLicenceNumberMessage(s) }
+            messages = { ...messages, ...this.generateLicenceExpiryMessage(s) }
             return true
-        } else {
-            if (this.state.loaded) {
-                setValid(`${model}.labourHire.${state}.expiry`, {
-                    required: valid
-                })
-            }
-        }
-        return true
+        })
+        return messages
     }
 
     render() {
@@ -154,6 +134,16 @@ class RecruiterForm extends BaseForm {
                           valid={form.valid}
                           component={SubmitForm}
                           onCustomSubmit={onSubmit}
+                          validators={{
+                              '': {
+                                  requiredLicenceNumber_qld: formValues => this.validLicenceNumber(formValues, 'qld'),
+                                  requiredLicenceExpiry_qld: formValues => this.validLicenceExpiry(formValues, 'qld'),
+                                  requiredLicenceNumber_sa: formValues => this.validLicenceNumber(formValues, 'sa'),
+                                  requiredLicenceExpiry_sa: formValues => this.validLicenceExpiry(formValues, 'sa'),
+                                  requiredLicenceNumber_vic: formValues => this.validLicenceNumber(formValues, 'vic'),
+                                  requiredLicenceExpiry_vic: formValues => this.validLicenceExpiry(formValues, 'vic')
+                              }
+                          }}
                     >
                         {csrf_token && (
                             <input type="hidden" name="csrf_token" id="csrf_token" value={csrf_token}/>
@@ -192,10 +182,14 @@ class RecruiterForm extends BaseForm {
                             {this.props[model].recruiter && this.props[model].recruiter !== 'no' && (
                                 <fieldset>
                                     <legend>
-                                        <h2 className="au-display-lg" tabIndex="-1">Labour hire licence</h2>
+                                        <h2 id="LabourHire" className="au-display-lg" tabIndex="-1">Labour hire licence</h2>
                                     </legend>
+                                    <StatefulError
+                                        model={model}
+                                        id="LabourHire"
+                                        messages={this.generateLicenceMessages()}
+                                    />
                                     <p>Some states operate under a mandated Labour Hire Licensing Act.  Please provide your licence details if you plan to submit roles for these states:</p>
-                                    
                                     {states.map(s => (
                                         <React.Fragment key={s}>
                                             <h3 className="au-display-md" tabIndex="-1">{mapAustraliaState(s)}</h3>
@@ -206,22 +200,6 @@ class RecruiterForm extends BaseForm {
                                                     Labour Hire Licensing Act for { mapAustraliaState(s) }
                                                 </a>
                                             </p>
-                                            <StatefulError
-                                                model={`${model}.labourHire.${s}.expiry`}
-                                                id={`${s}Expiry`}
-                                                messages={{
-                                                    validDate: `Expiry date is required for ${mapAustraliaState(s)} and must be in the future.`,
-                                                    required: `Please enter an expiry date for ${mapAustraliaState(s)}.`
-                                                }}
-                                            />
-                                            <br />
-                                            <StatefulError
-                                                model={`${model}.labourHire.${s}.licenceNumber`}
-                                                id={`${s}LicenceNumber`}
-                                                messages={{
-                                                    required: `Please enter your licence number for ${mapAustraliaState(s)}`
-                                                }}
-                                            />
                                             <Textfield
                                                 model={`${model}.labourHire.${s}.licenceNumber`}
                                                 name={`${s}LicenceNumber`}
@@ -229,9 +207,6 @@ class RecruiterForm extends BaseForm {
                                                 htmlFor={`${s}LicenceNumber`}
                                                 label="Licence number"
                                                 description=""
-                                                validators={{
-                                                    required: v => this.requiredExpiry(v, s)
-                                                }}
                                             />
                                             <Control
                                                 model={`${model}.labourHire.${s}.expiry`}
@@ -240,10 +215,6 @@ class RecruiterForm extends BaseForm {
                                                 id={`${s}Expiry`}
                                                 label="Expiry date"
                                                 updateOn="change"
-                                                validators={{
-                                                    validDate: v => this.validExpiryDate(v, s),
-                                                    required: v => this.requiredLicenceNumber(v, s)
-                                                }}
                                                 controlProps={{
                                                     id: `${s}Expiry`,
                                                     model: `${model}.labourHire.${s}.expiry`,
