b487d54c891ca51120422be25ac79fd0db9b89de
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-02-05 09:09:44 +1100

    [EditOpportunity] Remove max width from page alert paragraph

--- apps/marketplace/components/Brief/Edit/EditOpportunitySellers.js
+++ apps/marketplace/components/Brief/Edit/EditOpportunitySellers.js
@@ -166,11 +166,11 @@ class EditOpportunitySellers extends Component {
         </div>
         {showClosingDateWarning && (
           <div className={`row ${styles.marginTop1}`}>
-            <AUpageAlert as="warning">
+            <AUpageAlert as="warning" className={styles.pageAlert}>
               <AUheading level="2" size="lg">
                 Opportunity closing in {daysUntilOpportunityCloses} day{`${daysUntilOpportunityCloses > 1 ? 's' : ''}`}!
               </AUheading>
-              <p>
+              <p className={styles.noMaxWidth}>
                 We recommend you <Link to="/closing-date">extend the closing date</Link> to allow invited sellers you
                 added enough time to prepare and submit their responses.
               </p>
