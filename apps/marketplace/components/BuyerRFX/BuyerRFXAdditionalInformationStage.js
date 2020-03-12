e2b360ec1e987d2791939bfce72b854818f80a93
Reshma Abraham <reshmamabraham@gmail.com>
2020-03-12 15:26:34 +1100

    14 days instead of 7

--- apps/marketplace/components/BuyerRFX/BuyerRFXAdditionalInformationStage.js
+++ apps/marketplace/components/BuyerRFX/BuyerRFXAdditionalInformationStage.js
@@ -45,7 +45,7 @@ export class BuyerRFXAdditionalInformationStage extends Component {
 
   componentWillMount() {
     if (!this.props[this.props.model].closedAt) {
-      const date = addDays(new Date(), 7)
+      const date = addDays(new Date(), 14)
       this.props.setDate(format(date, 'YYYY-MM-DD'))
     }
   }
