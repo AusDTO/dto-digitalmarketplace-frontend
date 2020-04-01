7a54a5a277aeb6ee708230c2aa595a63bc457387
Reshma Abraham <reshmamabraham@gmail.com>
2020-04-02 07:54:52 +1100

    changes made

--- apps/marketplace/pages/SellerAssessmentViewPage.js
+++ apps/marketplace/pages/SellerAssessmentViewPage.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { actions } from 'react-redux-form'
-import { Redirect } from 'react-router-dom'
-import formProps from 'shared/form/formPropsSelector'
-import SellerAssessmentStages from 'marketplace/components/SellerAssessment/SellerAssessmentStages'
-import { rootPath } from 'marketplace/routes'
-import { loadDomainData, loadEvidenceData, saveEvidence } from 'marketplace/actions/supplierActions'
-import SellerAssessmentView from 'marketplace/components/SellerAssessment/SellerAssessmentView'
+import { loadDomainData, loadEvidenceData } from 'marketplace/actions/supplierActions'
 import LoadingIndicatorFullPage from 'shared/LoadingIndicatorFullPage/LoadingIndicatorFullPage'
-import { SellerAssessmentFormReducer, SellerAssessmentEvidenceReducer } from 'marketplace/reducers'
+import SellerAssessmentView from 'marketplace/components/SellerAssessment/SellerAssessmentView'
 
-const model = 'SellerAssessmentForm'
-
-export class SellerAssessmentViewPage extends Component {
+class SellerAssessmentViewPage extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
-      loading: false,
-      flowIsDone: false
+      loading: false
     }
-
-    this.saveEvidence = this.saveEvidence.bind(this)
-    this.handleStageMount = this.handleStageMount.bind(this)
   }
 
   componentDidMount() {
@@ -45,95 +32,27 @@ export class SellerAssessmentViewPage extends Component {
       loading: true
     })
     return this.props.loadInitialData(this.props.match.params.evidenceId).then(response => {
-      // only accept data defined in the form reducer
-      const data = { ...SellerAssessmentFormReducer }
-      if (response.data && !response.error) {
-        Object.keys(response.data).map(property => {
-          if (Object.keys(SellerAssessmentFormReducer).includes(property)) {
-            data[property] = response.data[property]
-          }
-          return true
-        })
-
-        // pre-populate the evidence model with the selected criteria if it doesn't yet exist
-        if (data.criteria && data.criteria.length > 0 && data.evidence) {
-          data.criteria.map(criteriaId => {
-            if (!(criteriaId in data.evidence)) {
-              data.evidence[criteriaId] = { ...SellerAssessmentEvidenceReducer }
-            }
-            return true
-          })
-        }
-
-        this.props.changeFormModel(data)
-      } else {
-        this.props.setError('Failed to load the evidence - invalid id')
-      }
-
       this.setState({
         loading: false
       })
-
-      return data
+      return response.data
     })
-  }
-
-  saveEvidence(publish = false) {
-    if (publish) {
-      this.setState({
-        loading: true
-      })
-    }
-    const data = { ...this.props[model] }
-    data.publish = publish
-    return this.props.saveEvidence(this.props.match.params.evidenceId, data).then(response => {
-      if (response.status === 200 && publish) {
-        this.setState({
-          flowIsDone: true,
-          loading: false
-        })
-      }
-    })
-  }
-
-  handleStageMount() {
-    this.props.resetFormValidity()
   }
 
   render() {
-    const evidenceId = this.props.match.params.evidenceId
-
     if (this.state.loading) {
       return <LoadingIndicatorFullPage />
     }
-
-    if (this.state.flowIsDone) {
-      return <Redirect to={`${rootPath}/seller-assessment/${evidenceId}/completed`} push />
-    }
-
-    return (
-      <SellerAssessmentView
-        model={model}
-        meta={{ domain: this.props.domain, evidence: this.props.evidence }}
-        onStageMount={this.handleStageMount}
-        basename={`${rootPath}/seller-assessment/${evidenceId}`}
-        stages={SellerAssessmentStages}
-        saveModel={this.saveEvidence}
-      />
-    )
+    return <SellerAssessmentView meta={{ domain: this.props.domain }} evidence={this.props.evidence} />
   }
 }
 
 const mapStateToProps = state => ({
-  ...formProps(state, model),
   domain: state.domain.domain,
   evidence: state.evidence
 })
 
 const mapDispatchToProps = dispatch => ({
-  changeFormModel: data => dispatch(actions.merge(model, data)),
-  resetFormValidity: () => dispatch(actions.resetValidity(model)),
-  saveEvidence: (evidenceId, data) => dispatch(saveEvidence(evidenceId, data)),
   loadInitialData: evidenceId => dispatch(loadEvidenceData(evidenceId)),
   loadDomainData: domainId => dispatch(loadDomainData(domainId))
 })
