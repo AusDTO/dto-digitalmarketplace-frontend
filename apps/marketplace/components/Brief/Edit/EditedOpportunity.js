4c855463e90534af2bb9e54c71ce2cd0d6db43d2
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-05-05 09:48:12 +1000

    Update paragraph text on edited opportunity page alert

--- apps/marketplace/components/Brief/Edit/EditedOpportunity.js
+++ apps/marketplace/components/Brief/Edit/EditedOpportunity.js
@@ -29,7 +29,7 @@ const EditedOpportunity = props => {
             </strong>
           </h1>
           <div className={styles.marginTop2}>
-            <p>You can view all invited sellers by selecting the opportunity on your dashboard.</p>
+            <p>You can view invited sellers by selecting the opportunity on your dashboard.</p>
             <div className={styles.marginTop1}>
               <AUbutton link={`${rootPath}/digital-marketplace/opportunities/${brief.id}`}>View opportunity</AUbutton>
             </div>
