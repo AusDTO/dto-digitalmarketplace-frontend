20b71a0aa0a89abc1b4ffc5f7053d6143ef86e28
Reshma Abraham <reshmamabraham@gmail.com>
2020-06-19 16:21:04 +1000

    added casestudies page

---
+++ apps/marketplace/pages/SellerAssessmentCaseStudiesPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import LoadingIndicatorFullPage from 'shared/LoadingIndicatorFullPage/LoadingIndicatorFullPage'
+import SellerAssessmentView from 'marketplace/components/SellerAssessment/SellerAssessmentView'
+import { loadDomainEvidenceData } from '../actions/supplierActions'
+import SellerAssessmentCaseStudies from '../components/SellerAssessment/SellerAssessmentCaseStudies'
+
+class SellerAssessmentCaseStudiesPage extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        loading: false
+      }
+    }
+  
+  
+    render() {
+      if (this.state.loading) {
+        return <LoadingIndicatorFullPage />
+      }
+    return <SellerAssessmentCaseStudies></SellerAssessmentCaseStudies>
+    }
+  }
+  
+  
+  export default (SellerAssessmentCaseStudiesPage)
