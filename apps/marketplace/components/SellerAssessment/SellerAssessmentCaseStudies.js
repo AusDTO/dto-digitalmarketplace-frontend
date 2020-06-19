c8c7a06f096cbfb4fe3a24b0e46d79c1a5fb9c9e
Reshma Abraham <reshmamabraham@gmail.com>
2020-06-19 16:21:52 +1000

    added case studies  - note will have to change file location and name of this to be purely case studies

---
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentCaseStudies.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import AUheading from '@gov.au/headings/lib/js/react.js'
+import AUdirectionLink from '@gov.au/direction-links/lib/js/react.js'
+import { rootPath } from 'marketplace/routes'
+import styles from './SellerAssessmentReviewStage.scss'
+import main from '../../main.scss'
+import SellerAssessmentCaseStudiesPage from '../../pages/SellerAssessmentCaseStudiesPage'
+
+const SellerAssessmentCaseStudies = props => (
+  <div>
+    <AUdirectionLink link={`${rootPath}/seller-dashboard/categories`} text="back to dashboard" direction="left" />
+
+    <AUheading level="1" size="xl">
+      Case Studies
+    </AUheading>
+  </div>
+)
+
+export default SellerAssessmentCaseStudies
