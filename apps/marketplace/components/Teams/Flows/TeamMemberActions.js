e79d2bd955315c1510c65b6548f2b315061b9139
tobiasbrown <2645932+tobiasbrown@users.noreply.github.com>
2020-07-01 15:30:30 +1000

    Move link to remove sellers when editing on smaller screens (#958)
    * Update selected item styles to allow for different behaviour on smaller screens
    
    * Fix float that overflows for selected team member actions

--- apps/marketplace/components/Teams/Flows/TeamMemberActions.js
+++ apps/marketplace/components/Teams/Flows/TeamMemberActions.js
@@ -3,35 +3,36 @@ import React from 'react'
 import AUlinklist from '@gov.au/link-list/lib/js/react.js'
 
 import commonStyles from './TeamStages.scss'
-import itemSelectStyles from '../../ItemSelect/SelectedItems.scss'
 
 const TeamMemberActions = props => {
   const { handleConvertToTeamLead, handleRemoveTeamMember, id } = props
 
   return (
-    <AUlinklist
-      className={itemSelectStyles.selectedItemActions}
-      inline
-      items={[
-        {
-          link: '#change-to-lead',
-          onClick: e => {
-            e.preventDefault()
-            handleConvertToTeamLead(id)
+    <div className={commonStyles.selectedItemActionsContainer}>
+      <AUlinklist
+        className={`${commonStyles.selectedItemActions}`}
+        inline
+        items={[
+          {
+            link: '#change-to-lead',
+            onClick: e => {
+              e.preventDefault()
+              handleConvertToTeamLead(id)
+            },
+            text: 'Change to lead'
           },
-          text: 'Change to lead'
-        },
-        {
-          className: commonStyles.removeLink,
-          link: '#remove',
-          onClick: e => {
-            e.preventDefault()
-            handleRemoveTeamMember(id)
-          },
-          text: 'Remove'
-        }
-      ]}
-    />
+          {
+            className: commonStyles.removeLink,
+            link: '#remove',
+            onClick: e => {
+              e.preventDefault()
+              handleRemoveTeamMember(id)
+            },
+            text: 'Remove'
+          }
+        ]}
+      />
+    </div>
   )
 }
 
