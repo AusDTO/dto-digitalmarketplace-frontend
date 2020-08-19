6c0442685197130e4d31084f7560d4a695fcf7c5
Reshma Abraham <reshmamabraham@gmail.com>
2020-08-19 23:28:28 +1000

    wip

--- apps/marketplace/components/SellerAssessment/SellerAssessmentCaseStudies.js
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentCaseStudies.js
@@ -8,56 +8,58 @@ const SellerAssessmentCaseStudies = props => (
   <div>
     <AUdirectionLink link={`${rootPath}/seller-dashboard/categories`} text="back to dashboard" direction="left" />
     <AUheading level="1" size="xl">
-      {props.caseStudies.domainName} case study
+      {/* {Object.keys(props.caseStudies).map(key => {}} */}
+      case study
     </AUheading>
     <div className={styles.spacer} />
 
-    {props.caseStudies.caseStudiesId &&
-      props.caseStudies.caseStudiesId.map((id, index) => (
-        <React.Fragment key={id}>
+    {props.caseStudies &&
+      props.caseStudies.map(value => (
+        <React.Fragment key={value.cs_id}>
           <AUheading level="2" size="lg">
-            {props.caseStudies.data[id].title}
+            {value.data.title}
           </AUheading>
+
           <AUheading level="2" size="md">
             Client
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].client}</p>
+          <p className={styles.reviewText}>{value.data.client}</p>
 
           <AUheading level="2" size="md">
             Referee details
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].referee_name}</p>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].referee_email}</p>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].referee_position}</p>
+          <p className={styles.reviewText}>{value.data.referee_name}</p>
+          <p className={styles.reviewText}>{value.data.referee_email}</p>
+          <p className={styles.reviewText}>{value.data.referee_position}</p>
 
           <AUheading level="2" size="md">
             Project date
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].timeframe}</p>
+          <p className={styles.reviewText}>{value.data.timeframe}</p>
 
           <AUheading level="2" size="md">
             Responsible for
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].roles}</p>
+          <p className={styles.reviewText}>{value.data.roles}</p>
 
           <AUheading level="2" size="md">
             Challenge
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].opportunity}</p>
+          <p className={styles.reviewText}>{value.data.opportunity}</p>
 
           <AUheading level="2" size="md">
             Approach
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].approach}</p>
+          <p className={styles.reviewText}>{value.data.approach}</p>
 
           <AUheading level="2" size="md">
             Outcomes and benefits
           </AUheading>
-          <p className={styles.reviewText}>{props.caseStudies.data[id].outcome}</p>
-          {index !== props.caseStudies.caseStudiesId.length - 1 && <div className={styles.spacer} />}
+          <p className={styles.reviewText}>{value.data.outcome}</p> 
         </React.Fragment>
       ))}
   </div>
+
 )
 
 export default SellerAssessmentCaseStudies
