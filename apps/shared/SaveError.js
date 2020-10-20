721765dedf2ef5889b502d5608bfb285bf2853e4
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-10-20 15:20:26 +1100

    Replace @digital.gov.au with @dta.gov.au

--- apps/shared/SaveError.js
+++ apps/shared/SaveError.js
@@ -9,7 +9,7 @@ const SaveError = ({ error }) => (
       <div className="callout--warning" aria-describedby="validation-masthead-heading" tabIndex="-1" role="alert">
         <h4 id="validation-masthead-heading">We couldn't save your information.</h4>
         Check your internet connection and if there's still a problem email{' '}
-        <a href="mailto:marketplace@digital.gov.au">marketplace@digital.gov.au</a>
+        <a href="mailto:marketplace@dta.gov.au">marketplace@dta.gov.au</a>
       </div>
     )}
   </div>
