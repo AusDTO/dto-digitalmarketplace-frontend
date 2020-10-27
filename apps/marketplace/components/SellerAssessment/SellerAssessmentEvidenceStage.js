912357f2a87a73c06dfce948267a63fc85996306
tobiasbrown <2645932+tobiasbrown@users.noreply.github.com>
2020-10-28 10:09:19 +1100

    Update referee label and description  (#966)
    * Update description used for referee's name
    
    * Update label used for referee's name

--- apps/marketplace/components/SellerAssessment/SellerAssessmentEvidenceStage.js
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentEvidenceStage.js
@@ -331,8 +331,8 @@ class SellerAssessmentEvidenceStage extends Component {
                   model={`${this.props.model}.evidence[${criteriaId}].refereeName`}
                   defaultValue={this.props[this.props.model].evidence[criteriaId].refereeName}
                   disabled={index !== 0 && this.isCriteriaDetailsDisabled(criteriaId)}
-                  label="Referee's full name"
-                  description="We may contact your referee to confirm your involvement in the project."
+                  label="Client referee's full name"
+                  description="The referee must be an employee of the client who can verify your involvement in the project."
                   name={`referee_name_${criteriaId}`}
                   id={`referee_name_${criteriaId}`}
                   htmlFor={`referee_name_${criteriaId}`}
