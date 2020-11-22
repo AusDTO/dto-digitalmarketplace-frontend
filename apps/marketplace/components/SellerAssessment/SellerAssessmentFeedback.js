a201e8112c6695e6ef34fe2a8e90dc2d363728b1
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-11-23 09:23:34 +1100

    Destructure seller assessment feedback props

--- apps/marketplace/components/SellerAssessment/SellerAssessmentFeedback.js
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentFeedback.js
@@ -32,124 +32,128 @@ const renderCriteriaFeedback = (criteriaId, criteria) => {
 
 const allCriteriaPassed = criteria => Object.keys(criteria).every(id => !criteria[id].has_feedback)
 
-const SellerAssessmentFeedback = props => (
-  <div>
-    <AUdirectionLink link={`${rootPath}/seller-dashboard`} text="back to dashboard" direction="left" />
-    <AUheading level="1" size="xl">
-      The assessment was unsuccessful.
-    </AUheading>
-    <AUheading level="2" size="lg">
-      How the decision was reached
-    </AUheading>
-    {Object.keys(props.feedback.criteria).length > 0 && (
-      <React.Fragment>
-        <ul className={styles.feedbackList}>
-          {props.feedback.vfm === false ? (
-            <li>
-              <span className={styles.feedbackFlex}>
-                <span className={styles.iconBlock}>
-                  <Cross colour="#FF0000" className={styles.icon} />
+const SellerAssessmentFeedback = props => {
+  const { feedback } = props
+
+  return (
+    <div>
+      <AUdirectionLink link={`${rootPath}/seller-dashboard`} text="back to dashboard" direction="left" />
+      <AUheading level="1" size="xl">
+        The assessment was unsuccessful.
+      </AUheading>
+      <AUheading level="2" size="lg">
+        How the decision was reached
+      </AUheading>
+      {Object.keys(feedback.criteria).length > 0 && (
+        <React.Fragment>
+          <ul className={styles.feedbackList}>
+            {feedback.vfm === false ? (
+              <li>
+                <span className={styles.feedbackFlex}>
+                  <span className={styles.iconBlock}>
+                    <Cross colour="#FF0000" className={styles.icon} />
+                  </span>
+                  <span className={styles.iconBlock}>
+                    You have been assessed as not offering value for money. Your maximum daily rate is considered too high
+                    for {feedback.domainName}.
+                  </span>
                 </span>
-                <span className={styles.iconBlock}>
-                  You have been assessed as not offering value for money. Your maximum daily rate is considered too high
-                  for {props.feedback.domainName}.
-                </span>
-              </span>
-            </li>
-          ) : (
-            <React.Fragment>
-              {Object.keys(props.feedback.criteria).map(criteriaId => (
-                <li key={criteriaId}>
-                  {!props.feedback.criteria[criteriaId].has_feedback && (
-                    <span className={styles.feedbackFlex}>
-                      <span className={styles.iconBlock}>
-                        <Tick colour="#17788D" className={styles.icon} />
-                      </span>
-                      <span className={styles.iconBlock}>
-                        Evidence demonstrates &quot;{props.feedback.criteria[criteriaId].name}&quot;
-                      </span>
-                    </span>
-                  )}
-                </li>
-              ))}
-              {Object.keys(props.feedback.criteria).map(criteriaId => (
-                <li key={criteriaId}>
-                  {props.feedback.criteria[criteriaId].has_feedback && (
-                    <React.Fragment>
+              </li>
+            ) : (
+              <React.Fragment>
+                {Object.keys(feedback.criteria).map(criteriaId => (
+                  <li key={criteriaId}>
+                    {!feedback.criteria[criteriaId].has_feedback && (
                       <span className={styles.feedbackFlex}>
                         <span className={styles.iconBlock}>
-                          <Cross colour="#FF0000" className={styles.icon} />
+                          <Tick colour="#17788D" className={styles.icon} />
                         </span>
                         <span className={styles.iconBlock}>
-                          Evidence does not demonstrate &quot;{props.feedback.criteria[criteriaId].name}&quot;
+                          Evidence demonstrates &quot;{feedback.criteria[criteriaId].name}&quot;
                         </span>
                       </span>
-                      {renderCriteriaFeedback(criteriaId, props.feedback.criteria)}
-                    </React.Fragment>
-                  )}
-                </li>
-              ))}
-            </React.Fragment>
-          )}
-        </ul>
-        <AUheading level="2" size="lg">
-          Next steps
-        </AUheading>
-        {props.feedback.vfm === false && !props.feedback.currentEvidenceId && (
-          <p>
-            To be approved in {props.feedback.domainName}, you may need to reconsider your maximum daily rate and{' '}
-            <a href={`${rootPath}/seller-assessment/create/${props.feedback.domainId}`}>resubmit for review</a>. You can
-            read our{' '}
-            <a
-              href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000556476-Panel-categories-and-rates"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              guide on pricing thresholds
-            </a>{' '}
-            for each category.
-          </p>
-        )}
-        {props.feedback.vfm === false && props.feedback.currentEvidenceId && (
-          <p>
-            To be approved in {props.feedback.domainName}, you may need to reconsider your maximum daily rate and{' '}
-            <a href={`${rootPath}/seller-assessment/${props.feedback.currentEvidenceId}/introduction`}>
-              resubmit for review
-            </a>
-            . You can read our{' '}
-            <a
-              href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000556476-Panel-categories-and-rates"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              guide on pricing thresholds
-            </a>{' '}
-            for each category.
-          </p>
-        )}
-        {!allCriteriaPassed(props.feedback.criteria) &&
-          props.feedback.vfm !== false &&
-          !props.feedback.currentEvidenceId && (
+                    )}
+                  </li>
+                ))}
+                {Object.keys(feedback.criteria).map(criteriaId => (
+                  <li key={criteriaId}>
+                    {feedback.criteria[criteriaId].has_feedback && (
+                      <React.Fragment>
+                        <span className={styles.feedbackFlex}>
+                          <span className={styles.iconBlock}>
+                            <Cross colour="#FF0000" className={styles.icon} />
+                          </span>
+                          <span className={styles.iconBlock}>
+                            Evidence does not demonstrate &quot;{feedback.criteria[criteriaId].name}&quot;
+                          </span>
+                        </span>
+                        {renderCriteriaFeedback(criteriaId, feedback.criteria)}
+                      </React.Fragment>
+                    )}
+                  </li>
+                ))}
+              </React.Fragment>
+            )}
+          </ul>
+          <AUheading level="2" size="lg">
+            Next steps
+          </AUheading>
+          {feedback.vfm === false && !feedback.currentEvidenceId && (
             <p>
-              You can incorporate the assessment team&apos;s feedback and{' '}
-              <a href={`${rootPath}/seller-assessment/create/${props.feedback.domainId}`}>resubmit your request</a>.
+              To be approved in {feedback.domainName}, you may need to reconsider your maximum daily rate and{' '}
+              <a href={`${rootPath}/seller-assessment/create/${feedback.domainId}`}>resubmit for review</a>. You can
+              read our{' '}
+              <a
+                href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000556476-Panel-categories-and-rates"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                guide on pricing thresholds
+              </a>{' '}
+              for each category.
             </p>
           )}
-        {!allCriteriaPassed(props.feedback.criteria) &&
-          props.feedback.vfm !== false &&
-          props.feedback.currentEvidenceId && (
+          {feedback.vfm === false && feedback.currentEvidenceId && (
             <p>
-              You can incorporate the assessment team&apos;s feedback and{' '}
-              <a href={`${rootPath}/seller-assessment/${props.feedback.currentEvidenceId}/introduction`}>
-                resubmit your request
+              To be approved in {feedback.domainName}, you may need to reconsider your maximum daily rate and{' '}
+              <a href={`${rootPath}/seller-assessment/${feedback.currentEvidenceId}/introduction`}>
+                resubmit for review
               </a>
-              .
+              . You can read our{' '}
+              <a
+                href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000556476-Panel-categories-and-rates"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                guide on pricing thresholds
+              </a>{' '}
+              for each category.
             </p>
           )}
-      </React.Fragment>
-    )}
-  </div>
-)
+          {!allCriteriaPassed(feedback.criteria) &&
+            feedback.vfm !== false &&
+            !feedback.currentEvidenceId && (
+              <p>
+                You can incorporate the assessment team&apos;s feedback and{' '}
+                <a href={`${rootPath}/seller-assessment/create/${feedback.domainId}`}>resubmit your request</a>.
+              </p>
+            )}
+          {!allCriteriaPassed(feedback.criteria) &&
+            feedback.vfm !== false &&
+            feedback.currentEvidenceId && (
+              <p>
+                You can incorporate the assessment team&apos;s feedback and{' '}
+                <a href={`${rootPath}/seller-assessment/${feedback.currentEvidenceId}/introduction`}>
+                  resubmit your request
+                </a>
+                .
+              </p>
+            )}
+        </React.Fragment>
+      )}
+    </div>
+  )
+}
 
 SellerAssessmentFeedback.defaultProps = {}
 
