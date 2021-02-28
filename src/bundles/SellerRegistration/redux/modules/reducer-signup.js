0efd9ff79c8a0d536061be6f68de60f1d2519d6f
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-01 09:36:00 +1100

    adding states into reducersignup

--- src/bundles/SellerRegistration/redux/modules/reducer-signup.js
+++ src/bundles/SellerRegistration/redux/modules/reducer-signup.js
@@ -114,7 +114,7 @@ export default combineReducers({
     },
     recruiterForm: {
       recruiter: '',
-      labourHire: {},
+      labourHire: {qld: {expiry: '', licenceNumber: ''}, vic: {expiry: '', licenceNumber: ''}, act: {expiry: '', licenceNumber: ''}},
       understandsAssessmentProcess: false
     },
     candidatesForm: {
