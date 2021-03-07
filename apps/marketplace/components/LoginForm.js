3c55696219f63ed7fbf0102a33c5b2ed197ab982
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-08 09:35:03 +1100

    adding content

--- apps/marketplace/components/LoginForm.js
+++ apps/marketplace/components/LoginForm.js
@@ -32,7 +32,9 @@ const LoginForm = props => {
             />
             <header className="page-heading page-heading-without-breadcrumb">
               <h1 className="au-display-xl">Sign in to the Marketplace</h1>
-              <h2 className="au-display-m">New to the Marketplace? Create your account</h2>
+              <p>
+  New to the Marketplace? <a href="/2/signup">Create your account</a>.
+</p>
             </header>
             <Form model={model} id="login" onSubmit={data => handleSubmit(data)}>
               <Textfield
