e4014429f16c98a956587b81e87006de0081f202
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-09 12:56:55 +1100

    adding login success and login error props for login form

--- apps/marketplace/components/LoginForm.js
+++ apps/marketplace/components/LoginForm.js
@@ -2,14 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Form } from 'react-redux-form'
 import DocumentTitle from 'react-document-title'
-
+import Layout from 'shared/Layout'
 import ErrorBox from 'shared/form/ErrorBox'
 import Textfield from 'shared/form/Textfield'
 import { required, validEmail, passwordLength } from '../components/validators'
 import LoadingButton from './LoadingButton/LoadingButton'
 
+const getErrorMessageFromServerCode = code => {
+  let message = ''
+
+  switch (code) {
+    case 409:
+      message = (
+        <React.Fragment>
+          <p>409 message</p>
+        </React.Fragment>
+      )
+      break
+    default:
+      message = <p>Default messages</p>
+      break
+  }
+
+  return message
+}
+
 const LoginForm = props => {
-  const { model, submitClicked, handleSubmit, currentlySending } = props
+  const { model, submitClicked, handleSubmit, currentlySending, loginErrorCode, loginSuccess } = props
 
   let hasFocused = false
   const setFocus = e => {
@@ -24,48 +43,58 @@ const LoginForm = props => {
       <DocumentTitle title="Login - Digital Marketplace">
         <div className="col-sm-push-2 col-sm-8 col-xs-12">
           <article role="main">
-            <ErrorBox
-              title="There was a problem signing in"
-              model={model}
-              submitClicked={submitClicked}
-              setFocus={setFocus}
-            />
-            <header className="page-heading page-heading-without-breadcrumb">
-              <h1 className="au-display-xl">Sign in to the Marketplace</h1>
-            </header>
-            <Form model={model} id="login" onSubmit={data => handleSubmit(data)}>
-              <Textfield
-                model={`${model}.emailAddress`}
-                name="emailAddress"
-                id="emailAddress"
-                htmlFor="emailAddress"
-                label="Email"
-                type="email"
-                validators={{ required, validEmail }}
-                messages={{
-                  required: 'Your email is required',
-                  validEmail: 'A validly formatted email is required.'
-                }}
-              />
-              <Textfield
-                model={`${model}.password`}
-                name="password"
-                id="password"
-                htmlFor="password"
-                label="Password"
-                type="password"
-                description="Your password should be at least 10 characters"
-                validators={{ passwordLength }}
-                messages={{
-                  passwordLength: 'Your password should be at least 10 characters'
-                }}
-              />
-              {currentlySending ? (
-                <LoadingButton />
-              ) : (
-                <input className="au-btn" type="submit" value="Sign in" onClick={submitClicked} />
-              )}
-            </Form>
+            {!loginSuccess && (
+              <Layout>
+                <ErrorBox
+                  title="There was a problem signing in"
+                  model={model}
+                  submitClicked={submitClicked}
+                  setFocus={setFocus}
+                />
+                {loginErrorCode && (
+                  <AUpageAlert as="error">
+                    <h4 className="au-display-md">There was a problem with login</h4>
+                    {getErrorMessageFromServerCode(loginErrorCode)}
+                  </AUpageAlert>
+                )}
+                <header className="page-heading page-heading-without-breadcrumb">
+                  <h1 className="au-display-xl">Sign in to the Marketplace</h1>
+                </header>
+                <Form model={model} id="login" onSubmit={data => handleSubmit(data)}>
+                  <Textfield
+                    model={`${model}.emailAddress`}
+                    name="emailAddress"
+                    id="emailAddress"
+                    htmlFor="emailAddress"
+                    label="Email"
+                    type="email"
+                    validators={{ required, validEmail }}
+                    messages={{
+                      required: 'Your email is required',
+                      validEmail: 'A validly formatted email is required.'
+                    }}
+                  />
+                  <Textfield
+                    model={`${model}.password`}
+                    name="password"
+                    id="password"
+                    htmlFor="password"
+                    label="Password"
+                    type="password"
+                    description="Your password should be at least 10 characters"
+                    validators={{ passwordLength }}
+                    messages={{
+                      passwordLength: 'Your password should be at least 10 characters'
+                    }}
+                  />
+                  {currentlySending ? (
+                    <LoadingButton />
+                  ) : (
+                    <input className="au-btn" type="submit" value="Sign in" onClick={submitClicked} />
+                  )}
+                </Form>
+              </Layout>
+            )}
           </article>
         </div>
       </DocumentTitle>
@@ -76,14 +105,18 @@ const LoginForm = props => {
 LoginForm.defaultProps = {
   submitClicked: null,
   handleSubmit: null,
-  currentlySending: false
+  currentlySending: false,
+  loginErrorCode: null,
+  loginSuccess: null
 }
 
 LoginForm.propTypes = {
   model: PropTypes.string.isRequired,
   submitClicked: PropTypes.func,
   handleSubmit: PropTypes.func,
-  currentlySending: PropTypes.bool
+  currentlySending: PropTypes.bool,
+  loginSuccess: PropTypes.bool,
+  loginErrorCode: PropTypes.number
 }
 
 export default LoginForm
