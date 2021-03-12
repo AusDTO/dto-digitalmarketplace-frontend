288fbc5820b211b5b7d437037509255ccd642112
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-12 16:35:39 +1100

    updating login

--- apps/marketplace/components/Header/UnauthenticatedHeaderActions.js
+++ apps/marketplace/components/Header/UnauthenticatedHeaderActions.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {rootPath} from '../../routes'
 
 const UnauthenticatedHeaderActions = () => (
   <ul data-reactroot="" id="main-navigation" className="au-marketplace-header-inline-links">
@@ -8,7 +9,7 @@ const UnauthenticatedHeaderActions = () => (
       </a>
     </li>
     <li>
-      <a href="/login" className="au-btn au-btn--dark">
+      <a href={`${rootPath}/login`} className="au-btn au-btn--dark">
         Log in
       </a>
     </li>
