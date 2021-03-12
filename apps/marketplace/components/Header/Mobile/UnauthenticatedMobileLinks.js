22a3986122b9feaeb6213fdd7d7fc212e0a4fdc9
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-12 16:33:40 +1100

    updating login for moblie

--- apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
+++ apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {rootPath} from '../../../routes'
 
 import CommonMobileLinks from './CommonMobileLinks'
 
 const UnauthenticatedMobileLinks = () => (
   <React.Fragment>
     <div className="au-marketplace-header_mobile-link">
-      <a href="/login">Sign in</a>
+      <a href={`${rootPath}/login`}>Sign in</a>
     </div>
     <div className="au-marketplace-header_mobile-link">
       <a href="/2/signup">Sign up</a>
