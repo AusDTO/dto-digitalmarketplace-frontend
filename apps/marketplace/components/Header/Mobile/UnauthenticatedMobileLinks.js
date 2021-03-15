b58a56049975eace02dbb59ba407683a2e41ebb4
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-15 13:59:59 +1100

    removing unused imports

--- apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
+++ apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { rootPath } from '../../../routes'
+import { rootPath } from '../../../routes'
 
 import CommonMobileLinks from './CommonMobileLinks'
 
@@ -7,7 +7,7 @@ const UnauthenticatedMobileLinks = () => (
   <React.Fragment>
     <div className="au-marketplace-header_mobile-link">
       <a href="/2/login">Sign in</a>
-      {/* <a href={`${rootPath}/login`}>Sign in</a> */}
+      <a href={`${rootPath}/login`}>Sign in</a>
     </div>
     <div className="au-marketplace-header_mobile-link">
       <a href="/2/signup">Sign up</a>
