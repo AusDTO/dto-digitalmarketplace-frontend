106483b14eed2f6d943ef1e2e011977e0208463d
Reshma-M-A <reshmamabraham@gmail.com>
2021-03-15 12:04:24 +1100

    wip

--- apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
+++ apps/marketplace/components/Header/Mobile/UnauthenticatedMobileLinks.js
@@ -6,7 +6,8 @@ import CommonMobileLinks from './CommonMobileLinks'
 const UnauthenticatedMobileLinks = () => (
   <React.Fragment>
     <div className="au-marketplace-header_mobile-link">
-      <a href={`${rootPath}/login`}>Sign in</a>
+      <a href='/2/login'>Sign in</a>
+      {/* <a href={`${rootPath}/login`}>Sign in</a> */}
     </div>
     <div className="au-marketplace-header_mobile-link">
       <a href="/2/signup">Sign up</a>
