57b54fe42544213c7f78cc60b1f4c7243774dd0b
Revit Hathaisatpong <hrevit@gmail.com>
2021-04-13 11:56:27 +1000

    Modified BuyerSpecialistResponseFormatsStage.js

--- apps/marketplace/components/BuyerSpecialist/BuyerSpecialistResponseFormatsStage.js
+++ apps/marketplace/components/BuyerSpecialist/BuyerSpecialistResponseFormatsStage.js
@@ -250,16 +250,21 @@ const BuyerSpecialistResponseFormatsStage = props => (
         </a>
       }
     />
-    <Textfield
-      model={`${props.model}.budgetRange`}
-      label="Additional budget information (optional) Remember to request that sellers provide an offer of discount(s) and to seek better rates of longer-term contracts."
-      name="budgetRange"
-      id="budgetRange"
-      htmlFor="budgetRange"
-      defaultValue={props[props.model].budgetRange}
-      maxLength={100}
-      validators={{}}
-    />
+    
+    <Textarea
+        model={`${props.model}.budgetRange`}
+        label="Additional budget information (optional)"
+        description="Remember to request that sellers provide an offer of discount(s) and to seek better rates of longer-term contracts."
+        name="budgetRange"
+        id="budgetRange"
+        htmlFor="budgetRange"
+        defaultValue={props[props.model].budgetRange}
+        controlProps={{ limit: 50 }}
+        validators={{}}
+        messages={{
+          limitWords: 'Additional budget has exceeded the 50 word limit'
+        }}
+      />
     <RadioList
       id="securityClearance"
       label="What are the security clearance requirements?"
