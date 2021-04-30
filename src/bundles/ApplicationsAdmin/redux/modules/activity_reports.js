2aec0052c46caafe09241a97cad191cd9b648e36
Revit Hathaisatpong <hrevit@gmail.com>
2021-04-30 15:42:17 +1000

    saved current work

--- src/bundles/ApplicationsAdmin/redux/modules/activity_reports.js
+++ src/bundles/ApplicationsAdmin/redux/modules/activity_reports.js
@@ -7,14 +7,8 @@ export default function reducer(state = {}, action = {}) {
   }
   
   export const downloadReports = data => {
-    console.log('eeeeeeeeeee')
-    console.log(data)
-    console.log('qqqqq')
     return (dispatch, getState, api) => {
       const state = getState();
-      console.log('stateeeee')
-      console.log(state.meta)
-      console.log(data)
       return api(state.meta.url_download_reports, {
         method: 'POST',
         body: {'report_type': data}
