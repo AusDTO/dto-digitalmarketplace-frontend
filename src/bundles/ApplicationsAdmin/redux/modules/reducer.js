da3fe91e9e0ea4f14a9f7bed84b24d7f4a4c8a4c
Revit Hathaisatpong <hrevit@gmail.com>
2021-05-05 09:04:52 +1000

    removed activity_reports.js

--- src/bundles/ApplicationsAdmin/redux/modules/reducer.js
+++ src/bundles/ApplicationsAdmin/redux/modules/reducer.js
@@ -7,7 +7,6 @@ import assessments from './assessments';
 import brief from './brief';
 import users from './users';
 import evidence from './evidence'
-import downloadReports from './activity_reports'
 
 export default combineReducers({
   agency,
@@ -17,7 +16,6 @@ export default combineReducers({
   evidence,
   brief,
   users,
-  downloadReports,
   form_options: (state = {}) => state,
   meta: (state = {}) => state,
   options: (state = {}) => state,
