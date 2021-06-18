8866d0023b9f776fbb7bdae2504ad86a19ca127f
Revit Hathaisatpong <hrevit@gmail.com>
2021-06-18 11:19:04 +1000

    saved work

---
+++ src/bundles/ApplicationsAdmin/components/ActivityReports/ActivityReports.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import AUbutton from '@gov.au/buttons/lib/js/react.js'
+import AUheading from '@gov.au/headings/lib/js/react.js'
+  
+export class ActivityReports extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      reportType: '',
+      acceptEnabled: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const url = `/export_reports_application/${this.state.reportType}`
+    window.location.href = url
+    
+  }
+
+  handleChange(event) {
+    this.setState({
+      reportType: event.target.value,
+      acceptEnabled: true
+    })
+  }
+
+  render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+        <AUheading size="xl" level="1">
+          Activity reports
+        </AUheading>
+        <p>Download opportunity report data for your agency or a current list of approved sellers.</p>
+        <input type="radio" id="allSellersCategoryMaxDailyRates" name="reportType" value="allSellersCategoryMaxDailyRates" onChange={this.handleChange}/>
+        <label htmlFor="allSellersCategoryMaxDailyRates">All Sellers Category Max Daily Rates</label>
+        <input type="radio" id="allSellersApprovedInCategory" name="reportType" value="allSellersApprovedInCategory" onChange={this.handleChange}/>
+        <label htmlFor="allSellersApprovedInCategory">All Sellers Approved in a Category</label>
+        <AUbutton type="submit" disabled={!this.state.acceptEnabled}>
+          Download reports
+        </AUbutton>
+        </form>
+      )
+  }
+}
+
+
+export default ActivityReports
+
+
+
