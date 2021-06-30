dd7e9eda13f0b84e90eb0e689eaa66e14d4d16a0
Revit Hathaisatpong <hrevit@gmail.com>
2021-06-30 15:21:37 +1000

    fixed eslint issue

--- apps/marketplace/actions/adminDownloadReportActions.js
+++ apps/marketplace/actions/adminDownloadReportActions.js
@@ -1,12 +1,17 @@
+import { SENDING_REQUEST } from '../constants/constants'
+import dmapi from '../services/apiClient'
+
+export const sendingRequest = sending => ({ type: SENDING_REQUEST, sending })
+
 export const downloadReports = (reportType, data) => (dispatch, getState) => {
-    dispatch(sendingRequest(true))
-    return dmapi({
-      url: `/downloadReports/${reportType}`,
-      method: 'PATCH',
-      headers: {
-        'X-CSRFToken': getState().app.csrfToken,
-        'Content-Type': 'application/json'
-      },
-      data: JSON.stringify(data)
-    })
-  }
+  dispatch(sendingRequest(true))
+  return dmapi({
+    url: `/downloadReports/${reportType}`,
+    method: 'PATCH',
+    headers: {
+      'X-CSRFToken': getState().app.csrfToken,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify(data)
+  })
+}
