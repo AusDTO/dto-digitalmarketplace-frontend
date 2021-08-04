c2ef2141fbc3c806777019f7d9db29f53d206e2b
Revit Hathaisatpong <hrevit@gmail.com>
2021-08-04 13:35:53 +1000

    removed blank line

--- src/bundles/ApplicationsAdmin/ActivityReportsAdminWidget.js
+++ src/bundles/ApplicationsAdmin/ActivityReportsAdminWidget.js
@@ -2,11 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { Route, Switch, IndexRoute} from 'react-router-dom'
 import RegisterComponent from '../../RegisterComponent'
-
 import createStore from './redux/create'
-
 import ActivityReports from './components/ActivityReports'
-
 
 
 const ActivityReportsAdminWidget = (props) => {
