07472a0497516cabab378a87bb8e55ce2857b617
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2021-08-30 14:36:36 +1000

    Fix indentation in ActivityReports render function

--- src/bundles/ApplicationsAdmin/components/ActivityReports/ActivityReports.js
+++ src/bundles/ApplicationsAdmin/components/ActivityReports/ActivityReports.js
@@ -49,21 +49,21 @@ export class ActivityReports extends React.Component {
   }
 
   render() {
-      return (
-        <form method="GET">
-          <AUheading size="xl" level="1">
+    return (
+      <form method="GET">
+        <AUheading size="xl" level="1">
           Activity reports
-          </AUheading>
-          <input type="radio" id="allSellersCategoryMaxDailyRates" name="reportType" value="maxDailyRates" onChange={this.handleChange} checked/>
-          <label htmlFor="allSellersCategoryMaxDailyRates">All sellers category max daily rates</label>
-          <input type="radio" id="allSellersApprovedInCategory" name="reportType" value="approvedInCategory" onChange={this.handleChange}/>
-          <label htmlFor="allSellersApprovedInCategory">All sellers approved in a category</label>
-          <AUbutton  onClick={this.handleSubmit}>
+        </AUheading>
+        <input type="radio" id="allSellersCategoryMaxDailyRates" name="reportType" value="maxDailyRates" onChange={this.handleChange} checked/>
+        <label htmlFor="allSellersCategoryMaxDailyRates">All sellers category max daily rates</label>
+        <input type="radio" id="allSellersApprovedInCategory" name="reportType" value="approvedInCategory" onChange={this.handleChange}/>
+        <label htmlFor="allSellersApprovedInCategory">All sellers approved in a category</label>
+        <AUbutton  onClick={this.handleSubmit}>
           Download reports
-          </AUbutton>
-        
-        </form>
-      )
+        </AUbutton>
+      
+      </form>
+    )
   }
 }
 
