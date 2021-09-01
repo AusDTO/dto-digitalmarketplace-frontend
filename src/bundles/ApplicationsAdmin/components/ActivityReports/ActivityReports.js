b4be23c9c30bde7791876853222126362ec7ff69
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2021-09-01 15:11:09 +1000

    Remove old submit event handler

--- src/bundles/ApplicationsAdmin/components/ActivityReports/ActivityReports.js
+++ src/bundles/ApplicationsAdmin/components/ActivityReports/ActivityReports.js
@@ -18,27 +18,6 @@ export class ActivityReports extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleSubmit_old(event) {
-    //event.preventDefault();
-    //const url = `./download_activity_reports/${this.state.reportType}`
-    //window.location.href = url
-    //this.props.history.push('./activity_reports')
-    event.preventDefault();
-    const url = `/download_activity_reports/${this.state.reportType}`
-    console.log('ddd111')
-    console.log(this.state.reportType)
-    var my = window.location.href
-    console.log(my)
-    var len = my.length;
-    console.log(len)
-    var rootbase = my.trim().substr(0,my.length-17)
-    var fullurl = rootbase + url
-    window.location.href = rootbase + url
-
-    this.props.history.push('./activity_reports')
-    //this.props.history.push(fullurl)
-  }
-
   handleSubmit(event) {
     event.preventDefault();
     const url = `/download_activity_reports/${this.state.reportType}`
