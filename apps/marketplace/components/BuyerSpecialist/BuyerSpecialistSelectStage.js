0a7a295749111ed12616f035deb468e7894db216
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-02-08 15:14:12 +1100

    HM-4374: Reorder components on Who Can Respond stage (#1013)
    * Reposition components on specialist select stage
    
    * Remove unused functions which reset openTo field
    
    * Add missing default props for seller select component
    
    * Format seller select with prettier
    
    * Add whitespace below alert under openTo field
    
    Co-authored-by: tobiasbrown <2645932+tobiasbrown@users.noreply.github.com>
    
    * Remove unnecessary div and correct code indentation
    
    * Fix prettier error
    
    Co-authored-by: tobiasbrown <2645932+tobiasbrown@users.noreply.github.com>

--- apps/marketplace/components/BuyerSpecialist/BuyerSpecialistSelectStage.js
+++ apps/marketplace/components/BuyerSpecialist/BuyerSpecialistSelectStage.js
@@ -13,10 +13,10 @@ import { countLabourHireSellers } from 'marketplace/actions/supplierActions'
 
 import mainStyles from '../../main.scss'
 
-const requiredCategory = v => v.sellerCategory
-const requiredChoice = v => !v.sellerCategory || v.openTo
+const requiredChoice = v => v.openTo
 const atLeastOneSeller = v =>
   !v.openTo || v.openTo === 'all' || (v.openTo === 'selected' && v.sellers && Object.keys(v.sellers).length > 0)
+const requiredCategory = v => v.sellerCategory
 
 export const done = v => requiredCategory(v) && requiredChoice(v) && atLeastOneSeller(v)
 
@@ -58,8 +58,6 @@ export class BuyerSpecialistSelectStage extends Component {
   handleSellerCategorySelect(category) {
     if (category !== this.props[this.props.model].sellerCategory) {
       this.props.updateSelectedSellerCategory(category)
-      this.props.resetOpenTo()
-      this.props.resetSelectedSellers()
     }
   }
 
@@ -89,9 +87,9 @@ export class BuyerSpecialistSelectStage extends Component {
         model={this.props.model}
         validators={{
           '': {
-            requiredCategory,
             requiredChoice,
-            atLeastOneSeller
+            atLeastOneSeller,
+            requiredCategory
           }
         }}
         onSubmit={this.props.onSubmit}
@@ -109,83 +107,80 @@ export class BuyerSpecialistSelectStage extends Component {
             atLeastOneSeller: 'You must add at least one seller'
           }}
         />
+        <div>
+          <RadioList
+            id="openTo"
+            label="Accept responses from:"
+            name="openTo"
+            model={`${this.props.model}.openTo`}
+            options={[
+              {
+                label: 'Any seller that provides ICT Labour Hire',
+                value: 'all'
+              },
+              {
+                label: 'Specific ICT Labour Hire sellers',
+                value: 'selected'
+              }
+            ]}
+            messages={{}}
+          />
+        </div>
+        {this.props[this.props.model].openTo === 'all' && (
+          <AUpageAlert as="warning" className={mainStyles.marginBottom3}>
+            <AUheading level="2" size="lg">
+              You are about to invite{' '}
+              {this.state.totalLabourHireSellers >= 1 ? this.state.totalLabourHireSellers : 'all'} seller
+              {this.state.totalLabourHireSellers === 1 ? '' : 's'}.
+            </AUheading>
+            <div className={mainStyles.marginTop1}>
+              <p>
+                You will need to reply to all seller questions, evaluate all quotes and debrief all unsuccessful sellers
+                (where requested).
+              </p>
+              <p>
+                You should only invite all sellers if there is a business need for this approach. If you have questions,
+                please{' '}
+                <a href="/contact-us" rel="noopener noreferrer" target="_blank">
+                  contact us
+                </a>
+                .
+              </p>
+            </div>
+          </AUpageAlert>
+        )}
+        {this.props[this.props.model].openTo === 'selected' && (
+          <React.Fragment>
+            <SellerSelect
+              briefId={this.props[this.props.model].id}
+              label="Seller name"
+              showSelected={false}
+              showSearchButton={false}
+              categories={categories}
+              onSellerSelect={this.handleSellerSelect}
+              onSellerCategorySelect={this.handleSellerCategorySelect}
+              showCategorySelect={false}
+              notFoundMessage="Seller is not on the Digital Marketplace"
+              selectedCategory="labourHire"
+              showSellerCatalogueLink
+            />
+            <br />
+            <SelectedSellersControl
+              id="selected-sellers"
+              model={`${this.props.model}.sellers`}
+              onRemoveClick={sellerCode => this.removeSeller(sellerCode)}
+            />
+          </React.Fragment>
+        )}
         <PanelCategorySelect
           id="select-seller"
           categories={categories}
           onChange={e => this.handleSellerCategorySelect(e.target.value)}
           selectedCategory={this.props[this.props.model].sellerCategory}
           label="Category"
+          description="Please select the category that best suits your opportunity. This is for reporting purposes only and will not limit who can respond."
+          helpLabel="View category descriptions"
         />
-        {this.props[this.props.model].sellerCategory && (
-          <div>
-            <div>
-              <RadioList
-                id="openTo"
-                label="Accept responses from:"
-                name="openTo"
-                model={`${this.props.model}.openTo`}
-                options={[
-                  {
-                    label: 'Any seller that provides ICT Labour Hire',
-                    value: 'all'
-                  },
-                  {
-                    label: 'Specific ICT Labour Hire sellers',
-                    value: 'selected'
-                  }
-                ]}
-                messages={{}}
-                onChange={() => this.props.resetSelectedSellers()}
-              />
-            </div>
-            {this.props[this.props.model].openTo === 'all' && (
-              <AUpageAlert as="warning">
-                <AUheading level="2" size="lg">
-                  You are about to invite{' '}
-                  {this.state.totalLabourHireSellers >= 1 ? this.state.totalLabourHireSellers : 'all'} seller
-                  {this.state.totalLabourHireSellers === 1 ? '' : 's'}.
-                </AUheading>
-                <div className={mainStyles.marginTop1}>
-                  <p>
-                    You will need to reply to all seller questions, evaluate all quotes and debrief all unsuccessful
-                    sellers (where requested).
-                  </p>
-                  <p>
-                    You should only invite all sellers if there is a business need for this approach. If you have
-                    questions, please{' '}
-                    <a href="/contact-us" rel="noopener noreferrer" target="_blank">
-                      contact us
-                    </a>
-                    .
-                  </p>
-                </div>
-              </AUpageAlert>
-            )}
-            {this.props[this.props.model].openTo === 'selected' && (
-              <React.Fragment>
-                <SellerSelect
-                  briefId={this.props[this.props.model].id}
-                  label="Seller name"
-                  showSelected={false}
-                  showSearchButton={false}
-                  categories={categories}
-                  onSellerSelect={this.handleSellerSelect}
-                  onSellerCategorySelect={this.handleSellerCategorySelect}
-                  showCategorySelect={false}
-                  notFoundMessage="Seller is not on the Digital Marketplace"
-                  selectedCategory="labourHire"
-                  showSellerCatalogueLink
-                />
-                <br />
-                <SelectedSellersControl
-                  id="selected-sellers"
-                  model={`${this.props.model}.sellers`}
-                  onRemoveClick={sellerCode => this.removeSeller(sellerCode)}
-                />
-              </React.Fragment>
-            )}
-          </div>
-        )}
         {this.props.formButtons}
       </Form>
     )
@@ -211,9 +206,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
   countSellers: () => dispatch(countLabourHireSellers()),
-  resetSelectedSellers: () => dispatch(actions.change(`${props.model}.sellers`, {})),
   updateSelectedSellers: sellers => dispatch(actions.change(`${props.model}.sellers`, sellers)),
-  resetOpenTo: () => dispatch(actions.change(`${props.model}.openTo`, '')),
   updateSelectedSellerCategory: category => dispatch(actions.change(`${props.model}.sellerCategory`, category))
 })
 
