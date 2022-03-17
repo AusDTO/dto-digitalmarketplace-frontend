63b3f2819e77ff443fe1af303e5ffd913b223576
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-03-17 16:49:27 +1100

    Restrict closing dates when creating an ATM opportunity

--- apps/marketplace/components/BuyerATM/BuyerATMAdditionalInformationStage.js
+++ apps/marketplace/components/BuyerATM/BuyerATMAdditionalInformationStage.js
@@ -4,19 +4,26 @@ import { connect } from 'react-redux'
 import { actions, Form } from 'react-redux-form'
 import formProps from 'shared/form/formPropsSelector'
 import Textfield from 'shared/form/Textfield'
+import AUpageAlert from '@gov.au/page-alerts/lib/js/react'
 import AUheading from '@gov.au/headings/lib/js/react.js'
 import format from 'date-fns/format'
 import addDays from 'date-fns/add_days'
+import isAfter from 'date-fns/is_after'
 import {
   required,
   validPhoneNumber,
   validDate,
   dateIs2DaysInFuture,
-  dateIsBefore
+  dateIsBefore,
+  dateIsOutsideBlackout
 } from 'marketplace/components/validators'
 import ErrorAlert from 'marketplace/components/Alerts/ErrorAlert'
 import DateControl from 'marketplace/components/BuyerBriefFlow/DateControl'
+import mainStyles from 'marketplace/main.scss'
 import styles from '../BuyerSpecialist/BuyerSpecialistAdditionalInformationStage.scss'
+
+let blackoutStartDate = null
+let blackoutEndDate = null
 
 const requiredContactNumber = v => required(v.contactNumber)
 const contactNumberFormat = v => validPhoneNumber(v.contactNumber)
@@ -24,6 +31,8 @@ const requiredClosedAt = v => required(v.closedAt)
 const closedAtIsValid = v => validDate(v.closedAt)
 const closedAtIs2DaysInFuture = v => !closedAtIsValid(v) || dateIs2DaysInFuture(v.closedAt)
 const closedAtIsBefore = v => !closedAtIsValid(v) || dateIsBefore(v.closedAt, addDays(new Date(), 366))
+const closedAtIsOutsideBlackout = v =>
+  !closedAtIsValid(v) || dateIsOutsideBlackout(v.closedAt, blackoutStartDate, blackoutEndDate)
 
 export const done = v =>
   requiredContactNumber(v) &&
@@ -31,7 +40,8 @@ export const done = v =>
   requiredClosedAt(v) &&
   closedAtIsValid(v) &&
   closedAtIs2DaysInFuture(v) &&
-  closedAtIsBefore(v)
+  closedAtIsBefore(v) &&
+  closedAtIsOutsideBlackout(v)
 
 export class BuyerATMAdditionalInformationStage extends Component {
   constructor(props) {
@@ -41,6 +51,8 @@ export class BuyerATMAdditionalInformationStage extends Component {
       const date = addDays(new Date(), 14)
       this.props.setDate(format(date, 'YYYY-MM-DD'))
     }
+    blackoutStartDate = this.props.blackoutPeriod.startDate
+    blackoutEndDate = this.props.blackoutPeriod.endDate
   }
 
   handleDateChange(date) {
@@ -48,7 +60,13 @@ export class BuyerATMAdditionalInformationStage extends Component {
   }
 
   render() {
-    const { model } = this.props
+    const { model, blackoutPeriod } = this.props
+    const isBlackoutPeriod = blackoutPeriod.startDate && blackoutPeriod.endDate
+    let closingTime = '6pm'
+    if (isBlackoutPeriod && isAfter(new Date(this.props[this.props.model].closedAt), blackoutPeriod.startDate)) {
+      closingTime = '11:59pm'
+    }
+
     return (
       <Form
         className={styles.additionalInformationContainer}
@@ -60,7 +78,8 @@ export class BuyerATMAdditionalInformationStage extends Component {
             requiredClosedAt,
             closedAtIsValid,
             closedAtIs2DaysInFuture,
-            closedAtIsBefore
+            closedAtIsBefore,
+            closedAtIsOutsideBlackout
           }
         }}
         onSubmit={this.props.onSubmit}
@@ -78,7 +97,10 @@ export class BuyerATMAdditionalInformationStage extends Component {
             closedAtIsValid: 'You must enter a valid closing date',
             closedAtIs2DaysInFuture: 'You must enter a closing date at least 2 days from now',
             requiredClosedAt: 'You must enter the closing date for this opportunity',
-            closedAtIsBefore: 'You must enter a closing date no more than one year from now'
+            closedAtIsBefore: 'You must enter a closing date no more than one year from now',
+            closedAtIsOutsideBlackout: `You must enter a closing date before 
+              ${format(blackoutPeriod.startDate, 'D MMMM YYYY')} or after 
+              ${format(blackoutPeriod.endDate, 'D MMMM YYYY')}`
           }}
         />
 
@@ -107,13 +129,25 @@ export class BuyerATMAdditionalInformationStage extends Component {
           maxLength={100}
           validators={{}}
         />
+        {isBlackoutPeriod && (
+          <AUpageAlert as="warning" className={`${mainStyles.marginTop3} ${mainStyles.marginBottom1}`}>
+            <p>
+              The closing date must be <b>before {format(blackoutPeriod.startDate, 'D MMMM YYYY')}</b> or{' '}
+              <b>after {format(blackoutPeriod.endDate, 'D MMMM YYYY')}</b> due to{' '}
+              <a href="/api/2/r/buyict" target="_blank">
+                Digital Marketplace moving to BuyICT
+              </a>
+              .
+            </p>
+          </AUpageAlert>
+        )}
         <DateControl
           id="closedAt"
           model={`${model}.closedAt`}
           onDateChange={this.handleDateChange}
           defaultValue={this.props[model].closedAt}
           label="Closing date for opportunity"
-          description="We recommend publishing for at least 2 weeks to allow interested sellers to respond. Responses will be available after 6pm Canberra time on this date."
+          description={`This date must be at least 2 days after you publish this request. Responses will be available after ${closingTime} Canberra time.`}
         />
         {this.props.formButtons}
       </Form>
@@ -123,18 +157,24 @@ export class BuyerATMAdditionalInformationStage extends Component {
 
 BuyerATMAdditionalInformationStage.defaultProps = {
   onSubmit: () => {},
-  onSubmitFailed: () => {}
+  onSubmitFailed: () => {},
+  blackoutPeriod: {
+    startDate: null,
+    endDate: null
+  }
 }
 
 BuyerATMAdditionalInformationStage.propTypes = {
   model: PropTypes.string.isRequired,
   formButtons: PropTypes.node.isRequired,
   onSubmit: PropTypes.func,
-  onSubmitFailed: PropTypes.func
+  onSubmitFailed: PropTypes.func,
+  blackoutPeriod: PropTypes.object
 }
 
 const mapStateToProps = (state, props) => ({
-  ...formProps(state, props.model)
+  ...formProps(state, props.model),
+  blackoutPeriod: state.brief.blackoutPeriod
 })
 
 const mapDispatchToProps = (dispatch, props) => ({
