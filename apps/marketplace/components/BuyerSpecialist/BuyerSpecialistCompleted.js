104d827877b7156cba759ac4ae6a464bbd111ab2
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-03-25 17:41:44 +1100

    Refactor code for Complete opportunity pages

--- apps/marketplace/components/BuyerSpecialist/BuyerSpecialistCompleted.js
+++ apps/marketplace/components/BuyerSpecialist/BuyerSpecialistCompleted.js
@@ -1,81 +1,69 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import AUpageAlert from '@gov.au/page-alerts/lib/js/react.js'
 import AUheading from '@gov.au/headings/lib/js/react.js'
 import format from 'date-fns/format'
-import isAfter from 'date-fns/is_after'
+import { getLockoutStatus } from 'marketplace/components/helpers'
 import { rootPath } from 'marketplace/routes'
 import Feedback from 'marketplace/components/Feedback/Feedback'
 import styles from './BuyerSpecialistCompleted.scss'
 
-export class BuyerSpecialistCompleted extends Component {
-  constructor(props) {
-    super()
-    this.props = props
-  }
-
-  render() {
-    const { lockoutPeriod } = this.props
-    const isLockoutPeriod = lockoutPeriod.startDate && lockoutPeriod.endDate
-    let closingTime = '6pm'
-    if (isLockoutPeriod && isAfter(new Date(this.props.closingDate), lockoutPeriod.startDate)) {
-      closingTime = '11:55pm'
-    }
-
-    return (
-      <div>
-        <AUpageAlert as="success">
-          <AUheading level="1" size="md">
-            Your opportunity is now live.
-          </AUheading>
-          <p>
-            It will be open until {format(this.props.closingDate, 'dddd D MMMM YYYY')} at {closingTime} Canberra time.
-          </p>
-        </AUpageAlert>
-        <AUheading level="2" size="lg">
-          What happens next?
+const BuyerSpecialistCompleted = props => {
+  const { lockoutPeriod, closingDate } = props
+  const { closingTime } = getLockoutStatus(lockoutPeriod, closingDate)
+  return (
+    <div>
+      <AUpageAlert as="success">
+        <AUheading level="1" size="md">
+          Your opportunity is now live.
         </AUheading>
-        <ul>
-          <li>
-            While your opportunity is live, you&apos;ll need to{' '}
-            <a
-              href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000583195#while-open"
-              rel="noopener noreferrer"
-            >
-              answer seller questions
-            </a>
-            .
-          </li>
-          <li>
-            We will send an email to <strong>{this.props.contactEmail}</strong> when the opportunity closes, so you can
-            download responses.
-          </li>
-        </ul>
         <p>
-          If you need help at any time, <a href="/contact-us">contact us</a>.
+          It will be open until {format(props.closingDate, 'dddd D MMMM YYYY')} at {closingTime} Canberra time.
         </p>
-        <p className={styles.buttons}>
-          <a href={`${rootPath}/brief/${this.props.briefId}/overview/atm`} className="au-btn au-btn--secondary">
-            Return to overview
+      </AUpageAlert>
+      <AUheading level="2" size="lg">
+        What happens next?
+      </AUheading>
+      <ul>
+        <li>
+          While your opportunity is live, you&apos;ll need to{' '}
+          <a
+            href="https://marketplace1.zendesk.com/hc/en-gb/articles/360000583195#while-open"
+            rel="noopener noreferrer"
+          >
+            answer seller questions
           </a>
-          <a href={`${rootPath}/digital-marketplace/opportunities/${this.props.briefId}`} className="au-btn">
-            View live opportunity
-          </a>
-        </p>
-        <br />
-        <br />
-        <br />
-        <Feedback
-          app={this.props.app}
-          handleSubmit={this.props.handleSubmit}
-          difficultyQuestion="How easy or difficult was it for you to publish this opportunity?"
-          commentQuestion="How would you improve publishing an opportunity?"
-          objectAction="published"
-        />
-      </div>
-    )
-  }
+          .
+        </li>
+        <li>
+          We will send an email to <strong>{props.contactEmail}</strong> when the opportunity closes, so you can
+          download responses.
+        </li>
+      </ul>
+      <p>
+        If you need help at any time, <a href="/contact-us">contact us</a>.
+      </p>
+      <p className={styles.buttons}>
+        <a href={`${rootPath}/brief/${props.briefId}/overview/atm`} className="au-btn au-btn--secondary">
+          Return to overview
+        </a>
+        <a href={`${rootPath}/digital-marketplace/opportunities/${props.briefId}`} className="au-btn">
+          View live opportunity
+        </a>
+      </p>
+      <br />
+      <br />
+      <br />
+      <Feedback
+        app={props.app}
+        handleSubmit={props.handleSubmit}
+        difficultyQuestion="How easy or difficult was it for you to publish this opportunity?"
+        commentQuestion="How would you improve publishing an opportunity?"
+        objectAction="published"
+      />
+    </div>
+  )
 }
 
 BuyerSpecialistCompleted.defaultProps = {
