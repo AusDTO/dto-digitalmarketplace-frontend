e8fec49b3ae099aa1188131f6ab15c3a49c8650a
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-03-28 15:58:57 +1100

    Change class components to function components for review pages

--- apps/marketplace/components/BuyerSpecialist/BuyerSpecialistReviewStage.js
+++ apps/marketplace/components/BuyerSpecialist/BuyerSpecialistReviewStage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -11,81 +11,72 @@ import ErrorAlert from 'marketplace/components/Alerts/ErrorAlert'
 import BuyerSpecialistStages from './BuyerSpecialistStages'
 import styles from './BuyerSpecialistReviewStage.scss'
 
-export class BuyerSpecialistReviewStage extends Component {
-  constructor(props) {
-    super()
-    this.props = props
-  }
+const BuyerSpecialistReviewStage = props => {
+  const { lockoutPeriod, model } = props
+  const { closingTime } = getLockoutStatus(lockoutPeriod, props[model].closedAt)
 
-  render() {
-    const { lockoutPeriod, model } = this.props
-    const { closingTime } = getLockoutStatus(lockoutPeriod, this.props[model].closedAt)
-
-    return (
-      <Form model={this.props.model} onSubmit={this.props.onSubmit} validators={{}}>
-        <div>
-          <AUheading level="1" size="xl">
-            Review and publish
-          </AUheading>
-          <ErrorAlert model={this.props.model} messages={{}} />
-          <AUheading level="2" size="sm">
-            If you publish today, you must be aware of the following dates:
-          </AUheading>
-          <div className={styles.milestones}>
-            <div className="row">
-              <div className="col-xs-12 col-sm-4">Today</div>
-              <div className="col-xs-12 col-sm-8">
-                Invited sellers can ask questions about your requirements and apply for the opportunity.
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-xs-12 col-sm-4">
-                {format(getBriefLastQuestionDate(new Date(this.props[this.props.model].closedAt)), 'dddd D MMMM YYYY')}
-              </div>
-              <div className="col-xs-12 col-sm-8">The last day sellers can ask questions.</div>
-            </div>
-            <div className="row">
-              <div className="col-xs-12 col-sm-4">
-                {format(this.props[this.props.model].closedAt, 'dddd D MMMM YYYY')}
-              </div>
-              <div className="col-xs-12 col-sm-8">
-                The last day you can publish answers to all sellers&apos; questions.
-                <br />
-                The last day sellers can apply.
-                <br />
-                The opportunity will close at {closingTime} Canberra time.
-              </div>
+  return (
+    <Form model={model} onSubmit={props.onSubmit} validators={{}}>
+      <div>
+        <AUheading level="1" size="xl">
+          Review and publish
+        </AUheading>
+        <ErrorAlert model={model} messages={{}} />
+        <AUheading level="2" size="sm">
+          If you publish today, you must be aware of the following dates:
+        </AUheading>
+        <div className={styles.milestones}>
+          <div className="row">
+            <div className="col-xs-12 col-sm-4">Today</div>
+            <div className="col-xs-12 col-sm-8">
+              Invited sellers can ask questions about your requirements and apply for the opportunity.
             </div>
           </div>
-          {this.props.stagesTodo.length > 0 ? (
-            <div>
-              <AUheading level="2" size="sm">
-                Before you publish your opportunity, you need to complete information in:
-              </AUheading>
-              <ul>
-                {this.props.stagesTodo.map(stage => (
-                  <li key={stage}>
-                    <Link to={stage}>{BuyerSpecialistStages.find(s => s.slug === stage).title || ''}</Link>
-                  </li>
-                ))}
-              </ul>
+          <div className="row">
+            <div className="col-xs-12 col-sm-4">
+              {format(getBriefLastQuestionDate(new Date(props[model].closedAt)), 'dddd D MMMM YYYY')}
             </div>
-          ) : (
-            <div>
-              <AUheading level="2" size="lg">
-                Once you press publish
-              </AUheading>
-              <ul>
-                <li>A summary of this request will be visible on the Digital Marketplace.</li>
-                <li>Only invited sellers and other buyers will be able to view attached documents.</li>
-              </ul>
-              {this.props.formButtons}
+            <div className="col-xs-12 col-sm-8">The last day sellers can ask questions.</div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 col-sm-4">{format(props[model].closedAt, 'dddd D MMMM YYYY')}</div>
+            <div className="col-xs-12 col-sm-8">
+              The last day you can publish answers to all sellers&apos; questions.
+              <br />
+              The last day sellers can apply.
+              <br />
+              The opportunity will close at {closingTime} Canberra time.
             </div>
-          )}
+          </div>
         </div>
-      </Form>
-    )
-  }
+        {props.stagesTodo.length > 0 ? (
+          <div>
+            <AUheading level="2" size="sm">
+              Before you publish your opportunity, you need to complete information in:
+            </AUheading>
+            <ul>
+              {props.stagesTodo.map(stage => (
+                <li key={stage}>
+                  <Link to={stage}>{BuyerSpecialistStages.find(s => s.slug === stage).title || ''}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div>
+            <AUheading level="2" size="lg">
+              Once you press publish
+            </AUheading>
+            <ul>
+              <li>A summary of this request will be visible on the Digital Marketplace.</li>
+              <li>Only invited sellers and other buyers will be able to view attached documents.</li>
+            </ul>
+            {props.formButtons}
+          </div>
+        )}
+      </div>
+    </Form>
+  )
 }
 
 BuyerSpecialistReviewStage.defaultProps = {
--- apps/marketplace/components/BuyerTraining/BuyerTrainingReviewStage.js
+++ apps/marketplace/components/BuyerTraining/BuyerTrainingReviewStage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -10,84 +10,72 @@ import format from 'date-fns/format'
 import BuyerTrainingStages from './BuyerTrainingStages'
 import styles from './BuyerTrainingReviewStage.scss'
 
-export class BuyerTrainingReviewStage extends Component {
-  constructor(props) {
-    super()
-    this.props = props
-  }
+const BuyerTrainingReviewStage = props => {
+  const { lockoutPeriod, model } = props
+  const { closingTime } = getLockoutStatus(lockoutPeriod, props[model].closedAt)
 
-  render() {
-    const { lockoutPeriod, model } = this.props
-    const { closingTime } = getLockoutStatus(lockoutPeriod, this.props[model].closedAt)
-
-    return (
-      <Form model={this.props.model} onSubmit={this.props.onSubmit}>
-        {this.props.stagesTodo.length > 0 ? (
-          <div>
-            <AUheading level="1" size="xl">
-              Review and publish
-            </AUheading>
-            <p>Before you publish your opportunity, you need to complete information in:</p>
-            <ul>
-              {this.props.stagesTodo.map(stage => (
-                <li key={stage}>
-                  <Link to={stage}>{BuyerTrainingStages.find(s => s.slug === stage).title || ''}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <div>
-            <AUheading level="1" size="xl">
-              Review and publish
-            </AUheading>
-            <p>If you publish today, you must be aware of the following dates:</p>
-            <div className={styles.milestones}>
-              <div className="row">
-                <div className="col-xs-12 col-sm-4">Today</div>
-                <div className="col-xs-12 col-sm-8">
-                  Invited sellers can ask questions about your requirements and apply for the opportunity.
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-xs-12 col-sm-4">
-                  {format(
-                    getBriefLastQuestionDate(new Date(this.props[this.props.model].closedAt)),
-                    'dddd D MMMM YYYY'
-                  )}
-                </div>
-                <div className="col-xs-12 col-sm-8">The last day sellers can ask questions.</div>
-              </div>
-              <div className="row">
-                <div className="col-xs-12 col-sm-4">
-                  {format(this.props[this.props.model].closedAt, 'dddd D MMMM YYYY')}
-                </div>
-                <div className="col-xs-12 col-sm-8">
-                  The last day you can publish answers to all sellers&apos; questions.
-                  <br />
-                  The last day sellers can apply.
-                  <br />
-                  The opportunity will close at {closingTime} Canberra time.
-                </div>
+  return (
+    <Form model={model} onSubmit={props.onSubmit}>
+      {props.stagesTodo.length > 0 ? (
+        <div>
+          <AUheading level="1" size="xl">
+            Review and publish
+          </AUheading>
+          <p>Before you publish your opportunity, you need to complete information in:</p>
+          <ul>
+            {props.stagesTodo.map(stage => (
+              <li key={stage}>
+                <Link to={stage}>{BuyerTrainingStages.find(s => s.slug === stage).title || ''}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <AUheading level="1" size="xl">
+            Review and publish
+          </AUheading>
+          <p>If you publish today, you must be aware of the following dates:</p>
+          <div className={styles.milestones}>
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">Today</div>
+              <div className="col-xs-12 col-sm-8">
+                Invited sellers can ask questions about your requirements and apply for the opportunity.
               </div>
             </div>
-            <AUheading level="2" size="lg">
-              Once you press publish
-            </AUheading>
-            <ul>
-              <li>A summary of this request will be visible on the Digital Marketplace.</li>
-              <li>Only invited sellers and other buyers will be able to view attached documents.</li>
-              {this.props[this.props.model].industryBriefing && (
-                <li>Only invited sellers will be able to view industry briefing details you provide.</li>
-              )}
-              <li>An email will be sent to the sellers&apos; business contacts inviting them to view and respond.</li>
-            </ul>
-            {this.props.formButtons}
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">
+                {format(getBriefLastQuestionDate(new Date(props[model].closedAt)), 'dddd D MMMM YYYY')}
+              </div>
+              <div className="col-xs-12 col-sm-8">The last day sellers can ask questions.</div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">{format(props[model].closedAt, 'dddd D MMMM YYYY')}</div>
+              <div className="col-xs-12 col-sm-8">
+                The last day you can publish answers to all sellers&apos; questions.
+                <br />
+                The last day sellers can apply.
+                <br />
+                The opportunity will close at {closingTime} Canberra time.
+              </div>
+            </div>
           </div>
-        )}
-      </Form>
-    )
-  }
+          <AUheading level="2" size="lg">
+            Once you press publish
+          </AUheading>
+          <ul>
+            <li>A summary of this request will be visible on the Digital Marketplace.</li>
+            <li>Only invited sellers and other buyers will be able to view attached documents.</li>
+            {props[model].industryBriefing && (
+              <li>Only invited sellers will be able to view industry briefing details you provide.</li>
+            )}
+            <li>An email will be sent to the sellers&apos; business contacts inviting them to view and respond.</li>
+          </ul>
+          {props.formButtons}
+        </div>
+      )}
+    </Form>
+  )
 }
 
 BuyerTrainingReviewStage.defaultProps = {
@@ -104,7 +92,7 @@ BuyerTrainingReviewStage.propTypes = {
   formButtons: PropTypes.node.isRequired,
   stagesTodo: PropTypes.array,
   onSubmit: PropTypes.func,
-  lockoutPeriod: PropTypes.object
+  lockoutPeriod: PropTypes.objects
 }
 
 const mapStateToProps = (state, props) => ({
