b3f7ae8f9be6ae4513efa7867a0abfb90efe3a59
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-04-13 13:26:40 +1000

    Update content for review page lockout  notice

--- apps/marketplace/components/BuyerBriefFlow/LockoutReviewNotice.js
+++ apps/marketplace/components/BuyerBriefFlow/LockoutReviewNotice.js
@@ -1,40 +1,76 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import format from 'date-fns/format'
-
+import AUheadings from '@gov.au/headings/lib/js/react.js'
 import styles from './LockoutReviewNotice.scss'
 
 const LockoutReviewNotice = props => {
   const { startDate, endDate } = props
   return (
-    <div className={`row ${styles.highlightRow}`}>
-      <div className="col-xs-12 col-sm-4">
-        <strong>
-          {format(startDate, 'dddd D MMMM YYYY')} – <br />
-          {format(endDate, 'dddd D MMMM YYYY')}
-        </strong>
+    <Fragment>
+      <div className={`row ${styles.highlightRow}`}>
+        <div className="col-xs-12 col-sm-4">
+          <strong>
+            {format(startDate, 'dddd D MMMM YYYY')} – <br />
+            {format(endDate, 'dddd D MMMM YYYY')}
+          </strong>
+        </div>
+        <div className="col-xs-12 col-sm-8">
+          <AUheadings level="2" size="sm">
+            Digital Marketplace is unavailable during its move to{' '}
+            <a href="/api/2/r/dmpbuyict" target="_blank" rel="noopener noreferrer">
+              BuyICT
+            </a>
+          </AUheadings>
+          <ul>
+            <li>
+              You <strong>won&apos;t be able to</strong> access your account to:
+              <ul>
+                <li>edit or withdraw your opportunity</li>
+                <li>respond to seller questions.</li>
+              </ul>
+            </li>
+            <li>
+              Sellers <strong>won&apos;t be able to</strong>:
+              <ul>
+                <li>submit questions</li>
+                <li>respond to your opportunity.</li>
+              </ul>
+            </li>
+          </ul>
+          <p>An overview of your opportunity can still be viewed by logged-out users.</p>
+        </div>
       </div>
-      <div className="col-xs-12 col-sm-8">
-        <strong>
-          Digital Marketplace is unavailable while it is{' '}
-          <a href="/" target="_blank">
-            moving to BuyICT
-          </a>
-          :
-        </strong>
-        <ul>
-          <li>
-            You <u>won&apos;t</u> be able to access your account and:
-            <ul>
-              <li>edit your opportunity</li>
-              <li>respond to seller questions</li>
-            </ul>
-          </li>
-          <li>Sellers can&apos;t submit questions or responses.</li>
-          <li>Sellers can only view a summary of the opportunity.</li>
-        </ul>
+      <div className={`row ${styles.highlightRow}`}>
+        <div className="col-xs-12 col-sm-4">
+          <strong>{format(endDate, 'dddd D MMMM YYYY')}</strong>
+        </div>
+        <div className="col-xs-12 col-sm-8">
+          <AUheadings level="2" size="sm">
+            Digital Marketplace goes live on{' '}
+            <a href="https://www.buyict.gov.au/" target="_blank" rel="noopener noreferrer">
+              BuyICT
+            </a>
+          </AUheadings>
+          <ul>
+            <li>
+              You can access your account on BuyICT and:
+              <ul>
+                <li>edit your opportunity</li>
+                <li>respond to seller questions.</li>
+              </ul>
+            </li>
+            <li>
+              Sellers can:
+              <ul>
+                <li>submit questions</li>
+                <li>respond to your opportunity.</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
