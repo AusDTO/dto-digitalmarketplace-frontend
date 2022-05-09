49dba97dc3865b80599120b07fbe0f5f6323cc4c
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-05-09 10:34:54 +1000

    Remove unused import

--- apps/marketplace/components/SellerAssessment/SellerAssessmentView.js
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentView.js
@@ -4,7 +4,6 @@ import AUheading from '@gov.au/headings/lib/js/react.js'
 import AUdirectionLink from '@gov.au/direction-links/lib/js/react.js'
 import { rootPath } from 'marketplace/routes'
 import styles from './SellerAssessmentReviewStage.scss'
-import main from '../../main.scss'
 
 const SellerAssessmentView = props => (
   <div>
