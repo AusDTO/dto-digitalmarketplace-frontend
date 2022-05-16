ce21ab53217e292b015eb577ee21768b1b21169e
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-05-16 11:46:20 +1000

    Update lockout page content

--- apps/marketplace/pages/LockoutPage.js
+++ apps/marketplace/pages/LockoutPage.js
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { logout } from 'marketplace/actions/appActions'
 import AUpageAlert from '@gov.au/page-alerts/lib/js/react'
+import AUheading from '@gov.au/headings/lib/js/react.js'
 import style from '../../marketplace/main.scss'
 
 const LockoutPageComponent = props => {
@@ -14,19 +15,32 @@ const LockoutPageComponent = props => {
     <div className="row">
       <div className="col-sm-push-2 col-sm-8 col-xs-12">
         <article role="main">
-          <h1>Sign up</h1>
+          <AUheading level="1" size="xl">
+            Digital Marketplace is on the move to BuyICT
+          </AUheading>
           <AUpageAlert as="error" className={`${style.marginTop2} ${style.marginBottom2}`}>
             <div>
               <p>
-                Digital Marketplace is closed while it is moving to{` `}
+                Digital Marketplace is closed while it&apos;s moving to{` `}
                 <a href="/api/2/r/buyict">BuyICT</a>.
               </p>
             </div>
           </AUpageAlert>
+          <p>
+            Sign ups and account access will be available on{' '}
+            <a href="https://www.buyict.gov.au/" rel="external">
+              BuyICT
+            </a>{' '}
+            <strong>from Monday 30 May 2022</strong>.
+          </p>
         </article>
       </div>
     </div>
   )
+}
+
+LockoutPageComponent.defaultProps = {
+  loggedIn: false
 }
 
 LockoutPageComponent.propTypes = {
