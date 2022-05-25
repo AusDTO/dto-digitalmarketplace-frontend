e3ba33863fe954af8f969b24909bfb1f9348db84
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-05-25 12:20:52 +1000

    Update banner text

--- apps/marketplace/components/Header/Header.js
+++ apps/marketplace/components/Header/Header.js
@@ -35,7 +35,7 @@ export class Header extends Component {
             <div className="wrapper">
               <div className="row">
                 <span className="maintenance-message">
-                  Digital Marketplace is closed while it&apos;s moving to BuyICT. <a href="/api/2/r/buyict">Find out more</a>.
+                  Digital Marketplace is now part of <a href="https://www.buyict.gov.au">BuyICT</a>.
                 </span>
               </div>
             </div>
